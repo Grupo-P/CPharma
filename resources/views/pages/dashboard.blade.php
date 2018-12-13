58c24eae7eb3784ee78fc5d28ef1721d2c0af446
Sergio Cova <sergio.cova1@gmail.com>
2018-12-13 18:44:42 -0400

    Vigesimo Tercer Commit
    Dashboard listo

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,24 +6,71 @@
 @endsection
 
 @section('content')
+
+	<?php
+		$usuarios = DB::table('users')->count();
+		$empresas = DB::table('empresas')->count();
+		$proveedores = DB::table('proveedors')->count();		
+	?>
+
 	<h1 class="h5 text-info">
 		<i class="fas fa-chart-pie"></i>
 		Dashboard
 	</h1>
 
 	<hr class="row align-items-start col-12"> 
+	<div class="card-deck">
 
-	 <div class="card border-success mb-3" style="width: 14rem;">	  	
-	  	<div class="card-body text-left bg-success">
-	    	<h2 class="card-title">
-	    		<span class="card-text text-white">26</span>
-	    	</h2>
-	    	<p class="card-text text-white">Empresas registradas!</p>
-	  	</div>
-	  	<div class="card-footer bg-transparent border-success text-right">
-	  		<a href="/empresa/" class="btn btn-outline-success btn-sm">Visualizar</a>
-	  	</div>
-	</div>
+		<!-- Usuario -->
+		<div class="card border-danger mb-3" style="width: 14rem;">	  	
+	  		<div class="card-body text-left bg-danger">
+	    		<h2 class="card-title">
+		    		<span class="card-text text-white">
+		    			<?php
+							echo ''.$usuarios;
+						?>
+		    		</span>
+	    		</h2>
+	    		<p class="card-text text-white">Usuarios registrados!</p>
+	  		</div>
+		  	<div class="card-footer bg-transparent border-danger text-right">
+		  		<a href="#" class="btn btn-outline-danger btn-sm">Visualizar</a>
+		  	</div>
+		</div>
+
+		<!-- Empresas -->
+	 	<div class="card border-success mb-3" style="width: 14rem;">	  	
+	  		<div class="card-body text-left bg-success">
+	    		<h2 class="card-title">
+		    		<span class="card-text text-white">
+		    			<?php
+							echo ''.$empresas;
+						?>
+		    		</span>
+	    		</h2>
+	    		<p class="card-text text-white">Empresas registradas!</p>
+	  		</div>
+		  	<div class="card-footer bg-transparent border-success text-right">
+		  		<a href="/empresa/" class="btn btn-outline-success btn-sm">Visualizar</a>
+		  	</div>
+		</div>
+	
+		<!-- Provedores -->
+		<div class="card border-info mb-3" style="width: 14rem;">	  	
+	  		<div class="card-body text-left bg-info">
+	    		<h2 class="card-title">
+		    		<span class="card-text text-white">
+		    			<?php
+							echo ''.$proveedores;
+						?>
+		    		</span>
+	    		</h2>
+	    		<p class="card-text text-white">Proveedores registrados!</p>
+	  		</div>
+		  	<div class="card-footer bg-transparent border-info text-right">
+		  		<a href="/proveedor/" class="btn btn-outline-info btn-sm">Visualizar</a>
+		  	</div>
+		</div>
 
 	</div>
    
