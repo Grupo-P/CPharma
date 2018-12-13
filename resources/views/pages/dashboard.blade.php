fd55665340e09852a3278e63e06486d5e6c1a7ef
Sergio Cova <sergio.cova1@gmail.com>
2018-12-13 18:26:03 -0400

    Vigesimo Segundo Commit
    Vista de dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,8 +6,25 @@
 @endsection
 
 @section('content')
-<h1 class="h5 text-info">
-<i class="fas fa-chart-pie"></i>
-Dashboard</h1>
-          
+	<h1 class="h5 text-info">
+		<i class="fas fa-chart-pie"></i>
+		Dashboard
+	</h1>
+
+	<hr class="row align-items-start col-12"> 
+
+	 <div class="card border-success mb-3" style="width: 14rem;">	  	
+	  	<div class="card-body text-left bg-success">
+	    	<h2 class="card-title">
+	    		<span class="card-text text-white">26</span>
+	    	</h2>
+	    	<p class="card-text text-white">Empresas registradas!</p>
+	  	</div>
+	  	<div class="card-footer bg-transparent border-success text-right">
+	  		<a href="/empresa/" class="btn btn-outline-success btn-sm">Visualizar</a>
+	  	</div>
+	</div>
+
+	</div>
+   
 @endsection
