5aaa0efdc1dba94c4d9b31cb913c7b9f3a8170c5
Sergio Cova <sergio.cova1@gmail.com>
2019-01-11 16:43:25 -0400

    Vigesimo Quinto Commit
    Base de datos conectada al server real

--- app/functions/config.php
+++ app/functions/config.php
@@ -3,5 +3,11 @@
 define ("serverDB" , "SERGIO-PC\CSERVER");	
 define ("userDB" , "sa");
 define ("passDB" , "soporte123");
-define ("nameDB" , "SMART");				 
+define ("nameDB" , "SMART");	
+/*
+define ("userDB" , "compouser1");
+define ("serverDB" , "10.100.0.2\SMARTPHARMA,1450");
+define ("passDB" , "c0mp0us3r");
+define ("nameDB" , "SMART");	
+*/		 
 ?>
