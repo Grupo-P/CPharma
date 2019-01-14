6827d62cb41c2c0a978b95ff5fb8372d51cf4f53
Sergio Cova <sergio.cova1@gmail.com>
2019-01-14 17:05:14 -0400

    Trigesimo Cuarto Commit
    cambios en sql server y querys

--- app/functions/Functions.php
+++ app/functions/Functions.php
@@ -1,6 +1,5 @@
 <?php
 //Conectar DB
-
 function conectarDB(){
 	$connectionInfo = array(
 		"Database"=>nameDB,
@@ -17,6 +16,39 @@ function conectarDB(){
 	     die( print_r( sqlsrv_errors(), true));
 	}
 	return $conn;
+}
+
+function GenPersona(){
+	$conn = conectarDB();
+
+	$sql = "SELECT Id, Nombre FROM GenPersona where TipoPersona = '2' ORDER BY 'Id' ASC";
+	$result = sqlsrv_query($conn,$sql);
+
+	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+      echo $row['Id'].", ".$row['Nombre']."<br />";
+  	}
+}
+
+function ComProveedor(){
+	$conn = conectarDB();
+
+	$sql = "SELECT Id, GenPersonaId, CodigoProveedor FROM ComProveedor ORDER BY 'Id' ASC";
+	$result = sqlsrv_query($conn,$sql);
+
+	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+      echo $row['Id'].", ".$row['GenPersonaId'].", ".$row['CodigoProveedor']."<br />";
+  	}
+}
+
+function ComFactura(){
+	$conn = conectarDB();
+
+	$sql = "SELECT Id, ComProveedorId, CONVERT(VARCHAR, FechaRegistro, 20) AS FechaRegistro FROM ComFactura ORDER BY 'Id' ASC";
+	$result = sqlsrv_query($conn,$sql);
+
+	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+      echo $row['Id'].", ".$row['ComProveedorId'].", ".$row['FechaRegistro']."<br />";
+  	}
 }
 
 ?>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,20 +3,5 @@
 	include(app_path().'\functions\config.php');
 	include(app_path().'\functions\Functions.php');
 
-	$conn = conectarDB();
-
-	$sql = "SELECT Nombre, Apellido, IdentificacionFiscal, Telefono, DireccionCorta, TipoPersona FROM GenPersona where TipoPersona = '1'";
-	$result = sqlsrv_query($conn,$sql);
-
-	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
-      echo $row['Nombre'].", ".$row['Apellido'].", ".$row['IdentificacionFiscal'].", ".$row['Telefono'].", ".$row['DireccionCorta'].", ".$row['TipoPersona']."<br />";
-
-
-/*
-      $sql = "SELECT CodigoArticulo, Descripcion, M_CostoUnitarioBruto FROM InvArticulo where Auditoria_FechaActualizacion > '2018-01-01 00:00:00.0000000' ORDER BY M_CostoUnitarioBruto ASC";
-	$result = sqlsrv_query($conn,$sql);
-
-	while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
-      echo $row['CodigoArticulo'].", ".$row['Descripcion'].", ".$row['M_CostoUnitarioBruto']."<br />";*/
-}
+	Example2();
 ?>
