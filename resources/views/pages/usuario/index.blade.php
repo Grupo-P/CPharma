03d744c7d9d36543baf90cabdec6207c84cf0779
Sergio Cova <sergio.cova1@gmail.com>
2019-01-14 10:04:36 -0400

    Vigesimo Septiomo Commit
    Controlador de usuario

---
+++ resources/views/pages/usuario/index.blade.php
@@ -0,0 +1,182 @@
+<!-- Correccion Orotografica: Parcial -->
+@extends('layouts.model')
+
+@section('title')
+    Empresa
+@endsection
+
+@section('content')
+
+	<!-- Modal Guardar -->
+	@if (session('Saved'))
+		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		  <div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Saved') }}</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        <h4 class="h6">Usuario almacenado con exito</h4>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	@endif
+	
+	<!-- Modal Editar -->
+	@if (session('Updated'))
+		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		  <div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Updated') }}</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        <h4 class="h6">Usuario modificado con exito</h4>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	@endif
+
+	<!-- Modal Eliminar -->
+	@if (session('Deleted'))
+		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		  <div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Deleted') }}</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        <h4 class="h6">Usuario actualizado con exito</h4>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>	
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	@endif
+
+	<h1 class="h5 text-info">
+		<i class="fas fa-User"></i>
+		Usuario
+	</h1>
+	<hr class="row align-items-start col-12">
+	
+	<div>
+		<a href="{{ url('/usuario/create') }}" role="button" class="btn btn-outline-info btn-sm" 
+		style="display: inline; text-align: left;">
+      		<i class="fa fa-plus"></i>
+      		Agregar		      		
+      	</a>
+	</div>
+	
+	<br>
+
+	<table class="table table-striped table-borderless col-12">
+	  	<thead class="thead-dark">
+		    <tr>
+		      	<th scope="col">#</th>
+		      	<th scope="col">Nombre</th>
+		      	<th scope="col">Email</th>
+		      	<th scope="col">Rol</th>
+		      	{{-- <th scope="col">Estatus</th>--}}
+		      	<th scope="col">Acciones</th> 
+		    </tr>
+	  	</thead>
+	  	<tbody>
+		@foreach($usuarios as $usuario)
+		    <tr>
+		      <th>{{$usuario->id}}</th>
+		      <td>{{$usuario->name}}</td>
+		      <td>{{$usuario->email}}</td>
+		      <td>{{$usuario->role}}</td> 
+		      {{-- <td>{{$empresa->estatus}}</td> --}}
+		      
+		    <!-- Inicio Validacion de ROLES -->
+		      <td>
+				
+				<?php
+				if(Auth::user()->role == 'MASTER'){
+				?>
+
+					<?php
+					if($usuario->estatus == 'ACTIVO'){
+					?>
+						<a href="/usuario/{{$usuario->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+			      			<i class="far fa-eye"></i>			      		
+			      		</a>
+
+			      		<a href="/usuario/{{$usuario->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+			      			<i class="fas fa-edit"></i>			      		
+				      	</a>
+				 					  
+				      	<form action="/usuario/{{$usuario->id}}" method="POST" style="display: inline;">
+						    @method('DELETE')
+						    @csrf					    
+						    <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Desincorporar"><i class="fa fa-reply"></i></button>
+						</form>
+					<?php
+					}
+					else if($usuario->estatus == 'INACTIVO'){
+					?>		
+			      	<form action="/usuario/{{$usuario->id}}" method="POST" style="display: inline;">
+					    @method('DELETE')
+					    @csrf					    
+					    <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Reincorporar"><i class="fa fa-share"></i></button>
+					</form>
+					<?php
+					}					
+					?>
+				<?php	
+				} else if(Auth::user()->role == 'SUPERVISOR'){
+				?>
+					<a href="/usuario/{{$usuario->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+		      			<i class="far fa-eye"></i>			      		
+		      		</a>
+
+		      		<a href="/usuario/{{$usuario->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+		      			<i class="fas fa-edit"></i>
+	      			</a>
+				<?php
+				} else if(Auth::user()->role == 'USUARIO'){
+				?>
+					<a href="/usuario/{{$usuario->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+		      			<i class="far fa-eye"></i>			      		
+		      		</a>		
+				<?php
+				}
+				?>
+										
+		      </td>
+		    <!-- Fin Validacion de ROLES -->
+
+		    </tr>
+		@endforeach
+		</tbody>
+	</table>
+
+	<script>
+		$(document).ready(function(){
+		    $('[data-toggle="tooltip"]').tooltip();   
+		});
+		$('#exampleModalCenter').modal('show')
+	</script>
+
+@endsection
