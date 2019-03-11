848a292f7f02e0787521251762c65c9f66e7c4f8
Sergio Cova <sergio.cova1@gmail.com>
2019-03-11 11:12:53 -0400

    ARMADO DEL SCRIPT PARA REPORTE 7

--- app/functions/QueryPrueba.php
+++ app/functions/QueryPrueba.php
@@ -1,52 +1,28 @@
 <?php
 /*Productos por proveedor
---LIMPIEZA DE TABLAS
-IF OBJECT_ID ('TablaTemp', 'U') IS NOT NULL
-	DROP TABLE TablaTemp;
-IF OBJECT_ID ('TablaTemp1', 'U') IS NOT NULL
-	DROP TABLE TablaTemp1;
-IF OBJECT_ID ('TablaTemp2', 'U') IS NOT NULL
-	DROP TABLE TablaTemp2;
-IF OBJECT_ID ('TablaTemp3', 'U') IS NOT NULL
-	DROP TABLE TablaTemp3;
-
---FACTURAS DONDE APARECE EL PROVEEDOR
-SELECT
-ComFactura.Id,
-ComFactura.ComProveedorId
-INTO TablaTemp
-FROM ComFactura
-WHERE ComFactura.ComProveedorId = 4
-ORDER BY ComFactura.Id ASC
-
---DETALLES DE FACTURAS DONDE APARECE EL PROVEEDOR
+******* SCRIPT 1 *****
 SELECT 
-ComFacturaDetalle.InvArticuloId,
-ComFacturaDetalle.ComFacturaId
-INTO TablaTemp1
-FROM ComFacturaDetalle
-INNER JOIN TablaTemp ON TablaTemp.Id = ComFacturaDetalle.ComFacturaId
-ORDER BY ComFacturaDetalle.ComFacturaId ASC
+GenPersona.Nombre,
+ComProveedor.Id
+FROM ComProveedor
+INNER JOIN GenPersona ON ComProveedor.GenPersonaId=GenPersona.Id
+INNER JOIN ComFactura ON ComFactura.ComProveedorId=ComProveedor.Id
+GROUP by ComProveedor.Id, GenPersona.Nombre
+ORDER BY ComProveedor.Id ASC
 
---ARTICULOS QUE APARECEN EN LOS DETALLES DE LAS FACTURAS
-SELECT
-InvArticulo.Id,
-InvArticulo.Descripcion 
-INTO TablaTemp2
-FROM InvArticulo
-INNER JOIN TablaTemp1 ON TablaTemp1.InvArticuloId = InvArticulo.Id
-ORDER BY InvArticulo.Descripcion ASC
-
-SELECT * FROM TablaTemp
-SELECT * FROM TablaTemp1
-SELECT * FROM TablaTemp2
- */
+SELECT 
+GenPersona.Nombre,
+ComProveedor.Id
+FROM ComProveedor
+INNER JOIN GenPersona ON ComProveedor.GenPersonaId=GenPersona.Id
+INNER JOIN ComFactura ON ComFactura.ComProveedorId=ComProveedor.Id 
+GROUP by ComProveedor.Id, GenPersona.Nombre
+ORDER BY GenPersona.Nombre ASC
 
 
 
 
-/*
---LIMPIEZA DE TABLAS
+******* SCRIPT 2 *****
 IF OBJECT_ID ('TablaTemp', 'U') IS NOT NULL
 	DROP TABLE TablaTemp;
 IF OBJECT_ID ('TablaTemp1', 'U') IS NOT NULL
@@ -60,81 +36,64 @@ IF OBJECT_ID ('TablaTemp4', 'U') IS NOT NULL
 IF OBJECT_ID ('TablaTemp5', 'U') IS NOT NULL
 	DROP TABLE TablaTemp5;
 
---FACTURAS DONDE APARECE EL PROVEEDOR
+--FACTURAS DONDE APARECE EL PROVEEDOR **OPTIMIZADO**
 SELECT
-ComFactura.Id,
-ComFactura.ComProveedorId
+ComFactura.Id AS FacturaId,
+ComFactura.ComProveedorId AS FacturaProveedorId
 INTO TablaTemp
 FROM ComFactura
-WHERE ComFactura.ComProveedorId = 20
-ORDER BY ComFactura.Id ASC
+WHERE ComFactura.ComProveedorId = 33
+ORDER BY FacturaId ASC
 
---DETALLES DE FACTURAS DONDE APARECE EL PROVEEDOR
+SELECT * FROM TablaTemp
+
+--DETALLES DE FACTURAS DONDE APARECE EL PROVEEDOR **OPTIMIZADO**
 SELECT 
-ComFacturaDetalle.InvArticuloId,
-ComFacturaDetalle.ComFacturaId
+ComFacturaDetalle.ComFacturaId AS FacturaIdDetalle,
+ComFacturaDetalle.InvArticuloId AS ArtiuloId
 INTO TablaTemp1
 FROM ComFacturaDetalle
-INNER JOIN TablaTemp ON TablaTemp.Id = ComFacturaDetalle.ComFacturaId
-ORDER BY ComFacturaDetalle.InvArticuloId ASC
+INNER JOIN TablaTemp ON TablaTemp.FacturaId = ComFacturaDetalle.ComFacturaId
+ORDER BY FacturaIdDetalle ASC
 
---ARTICULOS QUE APARECEN EN LOS DETALLES DE LAS FACTURAS
+SELECT * FROM TablaTemp1
+
+--ARTICULOS QUE APARECEN EN LOS DETALLES DE LAS FACTURAS 21123 13913 28933 2736
 SELECT
-InvArticulo.Id,
+InvArticulo.Id AS ArticuloIdPro,
 InvArticulo.Descripcion 
 INTO TablaTemp2
 FROM InvArticulo
-INNER JOIN TablaTemp1 ON TablaTemp1.InvArticuloId = InvArticulo.Id
+INNER JOIN TablaTemp1 ON TablaTemp1.ArtiuloId = InvArticulo.Id
+GROUP BY InvArticulo.Id,InvArticulo.Descripcion
 ORDER BY InvArticulo.Descripcion ASC
 
---SELECT * FROM TablaTemp
---SELECT * FROM TablaTemp1
 SELECT * FROM TablaTemp2
 
---REGRESION PARA PRODUCTO UNICO
+
+
+******* SCRIPT 3 *****
+IF OBJECT_ID ('TablaTemp', 'U') IS NOT NULL
+	DROP TABLE TablaTemp;
+IF OBJECT_ID ('TablaTemp1', 'U') IS NOT NULL
+	DROP TABLE TablaTemp1;
+
 SELECT
-TablaTemp2.Id AS IdArt,
-TablaTemp2.Descripcion,
-ComFacturaDetalle.ComFacturaId,
-ComFacturaDetalle.InvArticuloId
-INTO Tablatemp3
+ComFacturaDetalle.ComFacturaId AS FacturaId
+INTO TablaTemp
 FROM ComFacturaDetalle
-INNER JOIN TablaTemp2 ON TablaTemp2.Id = ComFacturaDetalle.InvArticuloId
+WHERE ComFacturaDetalle.InvArticuloId = 8840
+
+SELECT * FROM TablaTemp
 
 SELECT
-TablaTemp3.IdArt,
-TablaTemp3.Descripcion,
-ComFactura.Id,
 ComFactura.ComProveedorId
-INTO TablaTemp4
-FROM ComFactura
-INNER JOIN Tablatemp3 ON Tablatemp3.ComFacturaId = ComFactura.Id
-GROUP BY TablaTemp3.IdArt,TablaTemp3.Descripcion,ComFactura.Id,ComFactura.ComProveedorId
-ORDER BY ComFactura.Id ASC
+INTO TablaTemp1
+FROM TablaTemp 
+INNER JOIN ComFactura ON ComFactura.Id = TablaTemp.FacturaId
+WHERE ComFactura.ComProveedorId <> 33
+GROUP BY ComFactura.ComProveedorId
 
-SELECT
-TablaTemp3.IdArt,
-TablaTemp3.Descripcion,
-ComFactura.Id,
-ComFactura.ComProveedorId
-INTO TablaTemp5
-FROM ComFactura
-INNER JOIN Tablatemp3 ON Tablatemp3.ComFacturaId = ComFactura.Id
-WHERE ComFactura.ComProveedorId = 20
-GROUP BY TablaTemp3.IdArt,TablaTemp3.Descripcion,ComFactura.Id,ComFactura.ComProveedorId
-ORDER BY ComFactura.Id ASC
-
-
---SELECT * FROM TablaTemp3
---SELECT * FROM TablaTemp4
---SELECT * FROM TablaTemp5
-
-SELECT
-TablaTemp5.IdArt,
-TablaTemp5.Descripcion,
-TablaTemp5.Id,
-TablaTemp5.ComProveedorId
-FROM TablaTemp5 INNER JOIN TablaTemp4 ON TablaTemp4.IdArt = TablaTemp5.IdArt
-GROUP BY TablaTemp5.IdArt,TablaTemp5.Descripcion,TablaTemp5.Id,TablaTemp5.ComProveedorId
+SELECT * FROM TablaTemp1
  */
 ?>
