735979c0abb920cc73a7f410aa4d2c6332c82dc6
Sergio Cova <sergio.cova1@gmail.com>
2019-03-25 19:59:33 -0400

    Copyright

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,19 @@
 <!-- Copyright -->
+<style>
+    a{
+      text-decoration: none;
+      color: #5cb85c;
+    }
+
+    a:hover {      
+      text-decoration: none;
+      color: #5cb85c;
+    }
+</style>
+
 <div class="p-3 mb-2 bg-white text-success footer-copyright text-center py-3">
-	<span>Cpharma v.1.0 © 2018 Copyright Grupo P</span>
+	<span>CPharma v.1.0 © 2019 Copyright Grupo P</span>
+	<br>
+	<a href="https://www.instagram.com/sergiocovab/" target="blank">Developed by Sergio Cova</a>
 </div>
 <!-- Copyright -->
