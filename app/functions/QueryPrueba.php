cc3e38da868e3e725b0ab3196ee57c54f211908a
Sergio Cova <sergio.cova1@gmail.com>
2019-03-27 15:09:14 -0400

    Commit QueryPrueba

--- app/functions/QueryPrueba.php
+++ app/functions/QueryPrueba.php
@@ -91,4 +91,130 @@ GROUP BY ComFactura.ComProveedorId
 
 SELECT * FROM TablaTemp1
  */
+
+/* Reporte 6 prueba 27/03/2019
+
+IF OBJECT_ID ('TablaTemp', 'U') IS NOT NULL
+	DROP TABLE TablaTemp;
+IF OBJECT_ID ('TablaTemp1', 'U') IS NOT NULL
+	DROP TABLE TablaTemp1;
+IF OBJECT_ID ('TablaTemp2', 'U') IS NOT NULL
+	DROP TABLE TablaTemp2;
+IF OBJECT_ID ('TablaTemp3', 'U') IS NOT NULL
+	DROP TABLE TablaTemp3;
+IF OBJECT_ID ('TablaTemp4', 'U') IS NOT NULL
+	DROP TABLE TablaTemp4;
+IF OBJECT_ID ('TablaTemp5', 'U') IS NOT NULL
+	DROP TABLE TablaTemp5;
+IF OBJECT_ID ('TablaTemp6', 'U') IS NOT NULL
+	DROP TABLE TablaTemp6;
+
+SELECT 
+InvArticulo.Id, 
+InvArticulo.CodigoArticulo,
+InvArticulo.Descripcion,
+InvArticulo.FinConceptoImptoIdCompra AS ConceptoImpuesto
+INTO TablaTemp
+FROM InvArticulo
+WHERE 
+InvArticulo.Descripcion LIKE '%torondoy%'
+ORDER BY InvArticulo.Descripcion ASC
+	
+SELECT
+TablaTemp.Id,
+TablaTemp.CodigoArticulo,
+TablaTemp.Descripcion, 
+COUNT(*) AS VecesFacturadoCliente, 
+SUM(VenFacturaDetalle.Cantidad) AS UnidadesVendidasCliente 
+INTO TablaTemp1
+FROM VenFacturaDetalle
+INNER JOIN TablaTemp ON TablaTemp.Id = VenFacturaDetalle.InvArticuloId
+INNER JOIN VenFactura ON VenFactura.Id = VenFacturaDetalle.VenFacturaId
+WHERE
+(VenFactura.FechaDocumento > '2018-10-09' AND VenFactura.FechaDocumento < '2019-03-27')
+GROUP BY TablaTemp.Id,TablaTemp.CodigoArticulo,TablaTemp.Descripcion
+ORDER BY TablaTemp.Descripcion ASC
+	
+SELECT
+TablaTemp.Id,
+TablaTemp.CodigoArticulo,
+TablaTemp.Descripcion,
+COUNT(*) AS VecesDevueltaCliente,
+SUM(VenDevolucionDetalle.Cantidad) AS UnidadesDevueltaCliente
+INTO TablaTemp2
+FROM VenDevolucionDetalle
+INNER JOIN TablaTemp ON TablaTemp.Id = VenDevolucionDetalle.InvArticuloId
+INNER JOIN VenDevolucion ON VenDevolucion.Id = VenDevolucionDetalle.VenDevolucionId
+WHERE
+(VenDevolucion.FechaDocumento > '2018-10-09' AND VenDevolucion.FechaDocumento < '2019-03-27')
+GROUP BY TablaTemp.Id,TablaTemp.CodigoArticulo,TablaTemp.Descripcion
+ORDER BY TablaTemp.Descripcion ASC
+	
+SELECT
+TablaTemp.Id,
+TablaTemp.CodigoArticulo,
+TablaTemp.Descripcion, 
+COUNT(*) AS VecesFacturadaProveedor,
+SUM(ComFacturaDetalle.CantidadFacturada) AS CantidadFacturadaProveedor
+INTO TablaTemp3
+FROM ComFactura
+INNER JOIN ComFacturaDetalle ON ComFacturaDetalle.ComFacturaId = ComFactura.Id
+INNER JOIN TablaTemp ON TablaTemp.Id = ComFacturaDetalle.InvArticuloId
+WHERE
+(ComFactura.FechaDocumento > '2018-10-09' AND ComFactura.FechaDocumento < '2019-03-27')
+GROUP BY TablaTemp.Id,TablaTemp.CodigoArticulo,TablaTemp.Descripcion
+ORDER BY TablaTemp.Descripcion ASC
+	
+SELECT
+TablaTemp.Id,
+TablaTemp.CodigoArticulo,
+TablaTemp.Descripcion,
+COUNT(*) AS VecesReclamoProveedor,
+SUM(ComReclamoDetalle.Cantidad) AS CantidadReclamoProveedor
+INTO TablaTemp4
+FROM ComReclamoDetalle
+INNER JOIN TablaTemp ON TablaTemp.Id =ComReclamoDetalle.InvArticuloId
+INNER JOIN ComReclamo ON ComReclamo.Id = ComReclamoDetalle.ComReclamoId
+WHERE
+(ComReclamo.FechaRegistro > '2018-10-09' AND ComReclamo.FechaRegistro < '2019-03-27')
+GROUP BY TablaTemp.Id,TablaTemp.CodigoArticulo,TablaTemp.Descripcion
+ORDER BY TablaTemp.Descripcion ASC
+	
+SELECT
+TablaTemp.Id, 
+TablaTemp.CodigoArticulo, 
+TablaTemp.Descripcion,  
+SUM(InvLoteAlmacen.Existencia) AS Existencia
+INTO TablaTemp5
+FROM TablaTemp
+INNER JOIN InvArticulo ON InvArticulo.CodigoArticulo = TablaTemp.CodigoArticulo
+INNER JOIN InvLoteAlmacen ON InvLoteAlmacen.InvArticuloId = InvArticulo.Id
+WHERE (InvLoteAlmacen.InvAlmacenId = 1 or InvLoteAlmacen.InvAlmacenId = 2)
+GROUP BY TablaTemp.Id,TablaTemp.CodigoArticulo, TablaTemp.Descripcion
+	
+SELECT 
+TablaTemp.Id,
+TablaTemp.CodigoArticulo,
+TablaTemp.Descripcion,
+TablaTemp.ConceptoImpuesto,
+TablaTemp5.Existencia,
+TablaTemp1.VecesFacturadoCliente,
+TablaTemp1.UnidadesVendidasCliente,
+TablaTemp2.VecesDevueltaCliente,
+TablaTemp2.UnidadesDevueltaCliente,
+TablaTemp3.VecesFacturadaProveedor,
+TablaTemp3.CantidadFacturadaProveedor,
+TablaTemp4.VecesReclamoProveedor,
+TablaTemp4.CantidadReclamoProveedor
+INTO TablaTemp6
+FROM TablaTemp
+LEFT JOIN TablaTemp1 ON TablaTemp1.Id = TablaTemp.Id
+LEFT JOIN TablaTemp2 ON TablaTemp2.Id = TablaTemp.Id
+LEFT JOIN TablaTemp3 ON TablaTemp3.Id = TablaTemp.Id
+LEFT JOIN TablaTemp4 ON TablaTemp4.Id = TablaTemp.Id
+LEFT JOIN TablaTemp5 ON TablaTemp5.Id = TablaTemp.Id
+
+SELECT * FROM TablaTemp6
+
+ */
 ?>
