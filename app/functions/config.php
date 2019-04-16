499e94d309b809b8e3b57a7cf64be98c6713c999
Sergio Cova <sergio.cova1@gmail.com>
2019-04-16 16:19:25 -0400

    Commit Cadenas Conexion
    Cambio en las cadenas de conexion

--- app/functions/config.php
+++ app/functions/config.php
@@ -1,32 +1,44 @@
 <?php
-
 /*
-Constantes Globales
+	Constantes Globales
  */
 define("SigDolar","$");
 define("SigVe","Bs.S");
 define("Utilidad",0.77);
 define("Impuesto",1.16);
-
-//Cadena de conexion con la base de datos CPharma (desarrollo)
-
+/*
+	Cadena de conexion con la base de datos de CPharma
+*/
  define ("serverCP","localhost:3306");	
  define ("userCP","root");
  define ("passCP","");
  define ("nameCP","cpharma");
-
-//Cadena de conexion con el servidor local (desarrollo)
 /*
+	Cadena de conexion con la base de datos de Smartpharma FTN
+*/
+define ("serverFTN" , "10.100.0.2\SMARTPHARMA,1450");
+define ("userFTN" , "sa");
+define ("passFTN" , "Soporte123");
+define ("nameFTN" , "SMART");
+/*
+	Cadena de conexion con la base de datos de Smartpharma FLL
+*/
+define ("serverFLL" , "SERVER-FLL\SMARTPHARMA");
+define ("userFLL" , "sa");
+define ("passFLL" , "smartpharma123");
+define ("nameFLL" , "smartlago");
+/*
+	Cadena de conexion con la base de datos de Smartpharma FAU
+*/
+define ("serverFAU" , "");
+define ("userFAU" , "");
+define ("passFAU" , "");
+define ("nameFAU" , "");
+/*
+	Cadena de conexion con la base de datos del servidor de Desarrollo
+*/
  define ("serverDB" , "SERGIO-PC\CSERVER");	
  define ("userDB" , "admin");
  define ("passDB" , "soporte123");
  define ("nameDB" , "SMART");
-*/
-//Cadena de conexion con el servidor remoto (produccion)
-
-define ("serverDB" , "10.100.0.2\SMARTPHARMA,1450");
-define ("userDB" , "sa");
-define ("passDB" , "Soporte123");
-define ("nameDB" , "SMART");
-
 ?>
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -14,6 +14,6 @@
 <div class="p-3 mb-2 bg-white text-success footer-copyright text-center py-3">
 	<span>CPharma v.1.0 © 2019 Copyright Grupo P</span>
 	<br>
-	<a href="https://www.instagram.com/sergiocovab/" target="blank">Developed by Sergio Cova</a>
+	<a href="https://api.whatsapp.com/send?phone=584246254496&text=&source=&data=" target="blank">Developed by Sergio Cova</a>
 </div>
 <!-- Copyright -->
