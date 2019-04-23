d5335839bc02f9ff698b54cec8d60a8f5decf828
Sergio Cova <sergio.cova1@gmail.com>
2019-04-23 15:12:14 -0400

    Productos menos vendidos

--- app/functions/reportes.php
+++ app/functions/reportes.php
@@ -175,8 +175,8 @@ function ReporteHistoricoProducto($SedeConnection,$IdArticulo){
 				[$Top] Top de articulos a buscar
 				[$FInicial] Fecha inicial del rango donde se buscara
 				[$FFinal] Fecha final del rango donde se buscara
-	FUNCION: 
-	RETORNO: 
+	FUNCION: Arma una tabla con los productos mas vendidos
+	RETORNO: No aplica
  */
 function ReporteProductosMasVendidos($SedeConnection,$Top,$FInicial,$FFinal){
 
@@ -263,6 +263,104 @@ function ReporteProductosMasVendidos($SedeConnection,$Top,$FInicial,$FFinal){
 	
 	sqlsrv_close( $conn );
 }
+/************************************************************************************/
+/************************ REPORTE 3 PRODUCTOS MAS VENDIDOS **************************/
+/*
+	TITULO: ReporteProductosMenosVendidos
+	PARAMETROS: [$SedeConnection] Siglas de la sede para la conexion
+				[$Top] Top de articulos a buscar
+				[$FInicial] Fecha inicial del rango donde se buscara
+				[$FFinal] Fecha final del rango donde se buscara
+	FUNCION: Arma una tabla con los productos menos vendidos
+	RETORNO: No aplica
+ */
+function ReporteProductosMenosVendidos($SedeConnection,$Top,$FInicial,$FFinal){
+
+	$conn = ConectarSmartpharma('DB');
+
+	$FFinalImpresion= $FFinal;
+	$FFinal = date("Y-m-d",strtotime($FFinal."+ 1 days"));
+
+	$sql = QTablaTemp(5);
+	$sql1 = QUnidadesVendidasCliente($FInicial,$FFinal);
+	$sql2 = QUnidadesDevueltaCliente($FInicial,$FFinal);
+	$sql3 = QUnidadesCompradasProveedor($FInicial,$FFinal);
+	$sql4 = QUnidadesReclamoProveedor($FInicial,$FFinal);
+	$sql5 = QIntegracionProductosVendidos();
+	$sql6 = QProductoMenosVendido($Top);
+	
+	sqlsrv_query($conn,$sql);
+	sqlsrv_query($conn,$sql1);
+	sqlsrv_query($conn,$sql2);
+	sqlsrv_query($conn,$sql3);
+	sqlsrv_query($conn,$sql4);
+	sqlsrv_query($conn,$sql5);
+	$result = sqlsrv_query($conn,$sql6);
+
+	$RangoDias = RangoDias($FInicial,$FFinal);
+	
+	echo '
+	<div class="input-group md-form form-sm form-1 pl-0">
+	  <div class="input-group-prepend">
+	    <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+	        aria-hidden="true"></i></span>
+	  </div>
+	  <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()">
+	</div>
+	<br/>
+	';
+
+	echo'<h6 align="center">Periodo desde el '.$FInicial.' al '.$FFinalImpresion.' </h6>';
+
+	echo'
+	<table class="table table-striped table-bordered col-12 sortable" id="myTable">
+	  	<thead class="thead-dark">
+		    <tr>
+		    	<th scope="col">Codigo</th>
+		      	<th scope="col">Descripcion</th>
+		      	<th scope="col">Existencia</th>
+		      	<th scope="col">Veces Facturado</th>
+		      	<th scope="col">Unidades vendidas</th>
+		      	<th scope="col">Venta diaria</th>
+		      	<th scope="col">Dias restantes</th>
+		      	<th scope="col">Veces Comprado</th>
+		    </tr>
+	  	</thead>
+	  	<tbody>
+	';
+
+	while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+		$IdArticulo = $row["Id"];
+
+		$sql7 = QExistenciaArticulo($IdArticulo,0);
+		$result1 = sqlsrv_query($conn,$sql7);
+		$row1 = sqlsrv_fetch_array($result1,SQLSRV_FETCH_ASSOC);
+		$Existencia = $row1["Existencia"];
+
+		echo '<tr>';
+		echo '<td align="left">'.$row["CodigoArticulo"].'</td>';
+		echo '<td align="left">'.$row["Descripcion"].'</td>';
+		echo '<td align="center">'.intval($Existencia).'</td>';
+		echo '<td align="center">'.intval($row["TotalVecesVendidasCliente"]).'</td>';
+
+		$Venta = intval($row["TotalUnidadesVendidasCliente"]);
+		$VentaDiaria = VentaDiaria($Venta,$RangoDias);
+		$DiasRestantes = DiasRestantes($Existencia,$VentaDiaria);
+
+		echo '<td align="center">'.$Venta.'</td>';
+		echo '<td align="center">'.round($VentaDiaria,2).'</td>';
+		echo '<td align="center">'.round($DiasRestantes,2).'</td>';
+		echo '<td align="center">'.intval($row["TotalUnidadesCompradasProveedor"]).'</td>';
+		echo '</tr>';		
+  	}
+  	echo '
+  		</tbody>
+	</table>';
+	
+	sqlsrv_close( $conn );
+}
+
+
 
 /*
 	TITULO: 
--- resources/views/pages/reporte/reporte4.blade.php
+++ resources/views/pages/reporte/reporte4.blade.php
@@ -7,13 +7,15 @@
 @section('content')
 	<h1 class="h5 text-info">
 		<i class="fas fa-file-invoice"></i>
-		Articulos menos vendidos
+		Productos menos vendidos
 	</h1>
 	<hr class="row align-items-start col-12">
 
   <?php	
 	include(app_path().'\functions\config.php');
-	include(app_path().'\functions\Functions.php');
+  include(app_path().'\functions\querys.php');
+  include(app_path().'\functions\funciones.php');
+  include(app_path().'\functions\reportes.php');
 
   $ArtJson = "";
 	
@@ -21,13 +23,23 @@
 	{
     $InicioCarga = new DateTime("now");
 
-  	ReporteArticulosMenosVendidos($_GET['top'],$_GET['fechaInicio'],$_GET['fechaFin']);
+  	if (isset($_GET['SEDE'])){      
+      echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+    }
+    echo '<hr class="row align-items-start col-12">';
+
+    ReporteProductosMenosVendidos($_GET['SEDE'],$_GET['top'],$_GET['fechaInicio'],$_GET['fechaFin']);
 
     $FinCarga = new DateTime("now");
     $IntervalCarga = $InicioCarga->diff($FinCarga);
     echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
 	} 
 	else{
+    if (isset($_GET['SEDE'])){      
+      echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+    }
+    echo '<hr class="row align-items-start col-12">';
+
 		echo '
 		<form autocomplete="off" action="">
         <table style="width:100%;">
@@ -57,6 +69,9 @@
             <td align="right">
               <input id="fechaFin" name="fechaFin" type="date" required style="width:100%;">
             </td>
+            <input id="SEDE" name="SEDE" type="hidden" value="';
+            print_r($_GET['SEDE']);
+            echo'">
             <td align="right">
               <input type="submit" value="Buscar" class="btn btn-outline-success">
             </td>
