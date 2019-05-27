175db7982e2743efc997c57b4dc2ce17c3ac821c
mhenriquezm <manuelhm1993@gmail.com>
2019-05-27 10:14:22 -0400

    Esquema de la vista terminado
    Se realizaron las importaciones necesarias para dar formato a la vista del reporte

--- resources/views/pages/reporte/reporte9.blade.php
+++ resources/views/pages/reporte/reporte9.blade.php
@@ -1,4 +1,20 @@
-<?php
+@extends('layouts.model')
 
-	echo'Hola reporte 9';
-?>
+@section('title')
+    Reporte
+@endsection
+
+@section('content')
+	<h1 class="h5 text-info">
+		<i class="fas fa-file-invoice"></i>
+		Productos para surtir
+	</h1>
+	<hr class="row align-items-start col-12">
+
+	<?php
+		include(app_path().'\functions\config.php');
+		include(app_path().'\functions\querys.php');
+		include(app_path().'\functions\funciones.php');
+		include(app_path().'\functions\reportes.php');
+	?>
+@endsection
