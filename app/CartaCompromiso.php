8aad40cef68607b07c2ea4bbda932894a6654c35
mhenriquezm <manuelhm1993@gmail.com>
2019-07-02 16:31:50 -0400

    Modelo, controlador y migracion
    Se desarrollaron los objetos mencionados con una falla al momento de migrar

--- app/CartaCompromiso.php
+++ app/CartaCompromiso.php
@@ -5,5 +5,12 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class CartaCompromiso extends Model {
-    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'articulo', 'lote', 'fecha_de_vencimiento', 'proveedor', 'fecha_de_recepcion', 'fecha_de_tope', 'causa', 'nota', 'estatus', 'user'
+    ];
 }
