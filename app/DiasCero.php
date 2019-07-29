9e60689f032492b0f2bb58ae71ae5ee756a8e9b3
mhenriquezm <manuelhm1993@gmail.com>
2019-07-29 15:29:58 -0400

    Campos fillables
    Se definieron los campos fillables y que no pueden ser nulos

--- app/DiasCero.php
+++ app/DiasCero.php
@@ -5,5 +5,12 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class DiasCero extends Model {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'codigo_articulo', 'id_articulo', 'descripcion', 'existencia', 'fecha_captura', 'user'
+    ];
 }
