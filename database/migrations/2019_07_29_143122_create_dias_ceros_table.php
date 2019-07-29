5a16a38a9fa51150aa7f70eff53f2624c86d708c
mhenriquezm <manuelhm1993@gmail.com>
2019-07-29 15:22:45 -0400

    Migracion hecha
    Se ejecuto el comando para migrar la logica necesaria para crear la tabla en el gestor BBDD del cpharma

--- database/migrations/2019_07_29_143122_create_dias_ceros_table.php
+++ database/migrations/2019_07_29_143122_create_dias_ceros_table.php
@@ -13,6 +13,12 @@ class CreateDiasCerosTable extends Migration {
     public function up() {
         Schema::create('dias_ceros', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('codigo_articulo');
+            $table->integer('id_articulo');
+            $table->string('descripcion');
+            $table->decimal('existencia',16,4);
+            $table->date('fecha_captura');
+            $table->string('user');
             $table->timestamps();
         });
     }
