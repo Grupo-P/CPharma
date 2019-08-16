308f20cb61284e7f202e69e152f35a6593c0ed5b
mhenriquezm <manuelhm1993@gmail.com>
2019-08-16 17:07:29 -0400

    Validaciones del boton agregar

--- resources/views/pages/cartaCompromiso/index.blade.php
+++ resources/views/pages/cartaCompromiso/index.blade.php
@@ -86,7 +86,24 @@
 	<hr class="row align-items-start col-12">
 	<table style="width:100%;">
 	    <tr>
-	        <td style="width:10%;" align="center">	
+	    	<?php
+	    		if(!isset($_GET['SEDE'])) {
+	    			$_GET['SEDE'] = '';
+	    	?>
+	        <td>
+	        	<div class="input-group md-form form-sm form-1 pl-0">
+				  <div class="input-group-prepend">
+				    <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+				        aria-hidden="true"></i></span>
+				  </div>
+				  <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()">
+				</div>
+	        </td>
+	        <?php
+	        	}
+	    		else {
+	    	?>
+	    	<td style="width:10%;" align="center">	
 				<a href="/cartaCompromiso/create?SEDE=<?php print_r($_GET['SEDE']); ?>" role="button" class="btn btn-outline-info btn-sm" style="display:inline; text-align:left;">
 				<i class="fa fa-plus"></i>
 					Agregar		      		
@@ -102,6 +119,9 @@
 				  <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()">
 				</div>
 	        </td>
+	    	<?php
+	        	}
+	    	?>
 	    </tr>
 	</table>
 
