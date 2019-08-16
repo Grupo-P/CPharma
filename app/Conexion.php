f0524d146839d3fe79081a40bf6b9e6c68849774
CovaCode <covascode@gmail.com>
2019-08-16 10:15:32 -0400

    Views Update

--- app/Conexion.php
+++ app/Conexion.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conexion extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'instancia', 'base_datos', 'usuario', 'credencial', 'estatus', 'user'
+    ];
 }
