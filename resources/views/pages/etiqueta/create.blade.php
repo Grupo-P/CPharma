6be371c01876f8a7626ba1a8cce0c546257eae1a
CovaCode <covascode@gmail.com>
2019-09-09 17:00:23 -0400

    Validacion de productos para etiquetar

--- resources/views/pages/etiqueta/create.blade.php
+++ resources/views/pages/etiqueta/create.blade.php
@@ -1,9 +1,55 @@
 <?php
     include(app_path().'\functions\config.php');
-    include(app_path().'\functions\querys.php');
+    include(app_path().'\functions\Querys.php');
     include(app_path().'\functions\funciones.php');
     include(app_path().'\functions\reportes.php');
 
-    $EstoyEn = MiUbicacion();
-    echo'Hola estoy en: '.$EstoyEn;
+    $SedeConnection = 'FTN';
+
+    $conn = ConectarSmartpharma($SedeConnection);
+    $connCPharma = ConectarXampp();
+
+    $sql = QExistenciaActual();
+    $result = sqlsrv_query($conn,$sql);
+    
+    $contador = 0;
+
+    while(($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))&&($contador<3)) {
+        $IdArticulo = $row["IdArticulo"];
+        $CodigoInterno = $row["CodigoInterno"];
+        $Descripcion=$row["Descripcion"];
+        
+        $Existencia = intval($row["Existencia"]);
+        
+        echo'<br><br>*************************';
+        echo'<br>IdArticulo: '.$IdArticulo;
+        echo'<br>CodigoInterno: '.$CodigoInterno;
+        echo'<br>Descripcion: '.$Descripcion;
+        echo'<br>Existencia: '.$Existencia;
+        echo'<br>*************************';
+
+        $sqlCPharma = QEtiquetaArticulo($IdArticulo);
+
+        $ResultCPharma = mysqli_query($connCPharma,$sqlCPharma);
+        $RowCPharma = mysqli_fetch_assoc($ResultCPharma);
+        $IdArticuloCPharma = $RowCPharma['id_articulo'];
+
+        if(is_null($IdArticuloCPharma)){
+
+            $condicion = 'NO CLASIFICADO';
+            $clasificacion = 'ETIQUETABLES';
+            $estatus = 'ACTIVO';
+            $user = 'SYSTEM';
+            $date = new DateTime('now');
+            $date = $date->format("Y-m-d H:i:s");
+
+            $sqlCP = QGuardarEtiquetaArticulo($IdArticulo,$CodigoInterno,$Descripcion,$condicion,$clasificacion,$estatus,$user,$date);
+            mysqli_query($connCPharma,$sqlCP);
+        }
+
+        $contador++;
+    }
+
+    mysqli_close($connCPharma);
+    sqlsrv_close($conn);
 ?>
