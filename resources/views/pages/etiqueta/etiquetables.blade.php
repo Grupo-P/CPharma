16c8c212b2cad572e5955e5e4f4ac1b3970fc64c
CovaCode <covascode@gmail.com>
2019-09-12 18:30:44 -0400

    Modelado de etiqueta

--- resources/views/pages/etiqueta/etiquetables.blade.php
+++ resources/views/pages/etiqueta/etiquetables.blade.php
@@ -27,26 +27,107 @@
 		width: 5cm;
 		display: inline;
 	}
+	.centrado{
+		text-align: center;
+		text-transform: uppercase;
+	}
+	.derecha{
+		text-align: right;
+		text-transform: uppercase;
+	}
+	.izquierda{
+		text-align: left;
+		text-transform: uppercase;
+	}
+	thead {
+		border-bottom: 1px solid;
+	}
+	.descripcion{
+		padding: 2% 3%;
+	}
+	.titulo{
+		padding: 1.5% 3%;
+	}
+	.precio{
+		padding: 1.5% 5% 1.5% 1.5%;
+	}
 </style>
 
 	<div class="card-deck">
 
 		<div class="card border-dark mb-3 etiqueta">	  	
-	  		<div class="card-body text-left bg-white">
-	    		<p class="card-text text-dark">Prueba</p>
-	  		</div>
+	  		<table>
+	  			<thead>
+	  				<tr>
+	  					<td class="centrado" colspan="2">
+	  						Codigo Interno
+	  					</td>
+	  				</tr>	
+	  			</thead>
+	  			<tbody>
+	  				<tr rowspan="2">
+	  					<td class="centrado descripcion" colspan="2">
+	  						Arena para Gatos Cat Clean x 4 kg Imagroca
+	  					</td>
+	  				</tr>
+	  				<tr>
+	  					<td class="izquierda titulo">
+	  						PMVP Bs.
+	  					</td>
+  						<td class="derecha precio">
+	  						77878782.500,00
+	  					</td>
+	  				</tr>
+	  				<tr>
+	  					<td class="izquierda titulo">
+	  						IVA 16% Bs.
+	  					</td>
+  						<td class="derecha precio">
+	  						13.200,00
+	  					</td>
+	  				</tr>
+	  				<tr>
+	  					<td class="izquierda titulo">
+	  						<strong>Total a Pagar Bs.</strong>
+	  					</td>
+  						<td class="derecha precio">
+	  						<strong>13.200,00</strong>
+	  					</td>
+	  				</tr>
+	  				<tr>
+	  					<td class="izquierda dolarizado titulo">
+	  						<strong>*</strong>
+	  					</td>
+  						<td class="derecha precio">
+	  						12/09/2019
+	  					</td>
+	  				</tr>
+	  			</tbody>
+	  		</table>	
 		</div>
 		
 		<div class="card border-dark mb-3 etiqueta">	  	
-	  		<div class="card-body text-left bg-white">
-	    		<p class="card-text text-dark">Prueba</p>
-	  		</div>
+	  		<table>
+	  			<thead>
+	  				<tr>
+	  					<td class="centrado">
+	  						Codigo Interno
+	  					<td>
+	  				</tr>	
+	  			</thead>
+	  		</table>	
 		</div>
 
 		<div class="card border-dark mb-3 etiqueta">	  	
-	  		<div class="card-body text-left bg-white">
-	    		<p class="card-text text-dark">Prueba</p>
-	  		</div>
+	  		<table>
+	  			<thead>
+	  				<tr>
+	  					<td class="centrado">
+	  						Codigo Interno
+	  					<td>
+	  				</tr>	
+	  			</thead>
+	  		</table>	
 		</div>
 		
 	</div>
