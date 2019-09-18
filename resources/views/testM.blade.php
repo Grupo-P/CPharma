0a5d4983cc932ff0f9a4dc18d0db3852a09a1a1b
mhenriquezm <manuelhm1993@gmail.com>
2019-09-18 17:11:16 -0400

    Ultimas pruebas

--- resources/views/testM.blade.php
+++ resources/views/testM.blade.php
@@ -276,9 +276,7 @@
         saldoRestanteDs.val('-' + separarMiles(restanteDs, decimales));
       }
       else if(restanteBs != 0) {
-        resultado.val('Hay un vuelto pendiente de: Bs. ' + separarMiles(restanteBs, decimales)).removeClass('bg-danger text-white');
-        saldoRestanteBs.val('-' + separarMiles(restanteBs, decimales));
-        saldoRestanteDs.val('-' + separarMiles(restanteDs, decimales));
+        resultado.val('-').removeClass('bg-danger text-white');
       }
       
       formatearVariables()
