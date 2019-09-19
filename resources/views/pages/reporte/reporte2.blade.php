550e915decc6095c815d5c2aef0c35e3365f14c0
Sergio Cova <covascode@gmail.com>
2019-09-19 19:52:07 -0400

    Update reporte2.blade.php

--- resources/views/pages/reporte/reporte2.blade.php
+++ resources/views/pages/reporte/reporte2.blade.php
@@ -4,14 +4,81 @@
     Reporte
 @endsection
 
+@section('scriptsHead')
+    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">  
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"> 
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
+
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+    .autocomplete {
+      position: relative;
+      display: inline-block;
+    }
+    input {
+      border: 1px solid transparent;
+      background-color: #f1f1f1;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 16px;
+    }
+    input[type=text] {
+      background-color: #f1f1f1;
+      width: 100%;
+    }
+    .autocomplete-items {
+      position: absolute;
+      border: 1px solid #d4d4d4;
+      border-bottom: none;
+      border-top: none;
+      z-index: 99;
+      top: 100%;
+      left: 0;
+      right: 0;
+    }
+    .autocomplete-items div {
+      padding: 10px;
+      cursor: pointer;
+      background-color: #fff; 
+      border-bottom: 1px solid #d4d4d4; 
+    }
+    .autocomplete-items div:hover {
+      background-color: #e9e9e9; 
+    }
+    .autocomplete-active {
+      background-color: DodgerBlue !important; 
+      color: #ffffff; 
+    }
+  </style>
+@endsection
+
+@section('scriptsFoot')
+  <?php
+    if($ArtJson!=""){
+  ?>
+    <script type="text/javascript">
+      ArrJs = eval(<?php echo $ArtJson ?>);
+      autocompletado(document.getElementById("myInput"),document.getElementById("myId"), ArrJs);
+    </script> 
+  <?php
+    }
+  ?>  
+@endsection
+
 @section('content')
 	<h1 class="h5 text-info">
 		<i class="fas fa-file-invoice"></i>
 		Historico de productos
 	</h1>
 	<hr class="row align-items-start col-12">
-
-  <?php	
+<?php	
 	include(app_path().'\functions\config.php');
   include(app_path().'\functions\querys.php');
   include(app_path().'\functions\funciones.php');
@@ -66,82 +133,7 @@
 ?>
 @endsection
 
-@section('scriptsHead')
-    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">	
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}">	
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
-  	<script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
 
-  	<style>
-    * {
-      box-sizing: border-box;
-    }
 
-    /*the container must be positioned relative:*/
-    .autocomplete {
-      position: relative;
-      display: inline-block;
-    }
 
-    input {
-      border: 1px solid transparent;
-      background-color: #f1f1f1;
-      border-radius: 5px;
-      padding: 10px;
-      font-size: 16px;
-    }
-
-    input[type=text] {
-      background-color: #f1f1f1;
-      width: 100%;
-    }
-
-    .autocomplete-items {
-      position: absolute;
-      border: 1px solid #d4d4d4;
-      border-bottom: none;
-      border-top: none;
-      z-index: 99;
-      /*position the autocomplete items to be the same width as the container:*/
-      top: 100%;
-      left: 0;
-      right: 0;
-    }
-
-    .autocomplete-items div {
-      padding: 10px;
-      cursor: pointer;
-      background-color: #fff; 
-      border-bottom: 1px solid #d4d4d4; 
-    }
-
-    /*when hovering an item:*/
-    .autocomplete-items div:hover {
-      background-color: #e9e9e9; 
-    }
-
-    /*when navigating through the items using the arrow keys:*/
-    .autocomplete-active {
-      background-color: DodgerBlue !important; 
-      color: #ffffff; 
-    }
-    </style>
-@endsection
-
-@section('scriptsFoot')
-<?php
-  if($ArtJson!=""){
-?>
-    <script type="text/javascript">
-      ArrJs = eval(<?php echo $ArtJson ?>);
-      autocompletado(document.getElementById("myInput"),document.getElementById("myId"), ArrJs);
-    </script> 
-<?php
-  }
-?>	
-@endsection
 
