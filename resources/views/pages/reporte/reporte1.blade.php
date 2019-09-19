4542a20a21e986c843c780c06ac49c3cd7467fca
Sergio Cova <covascode@gmail.com>
2019-09-19 19:52:46 -0400

    Update reporte1.blade.php

--- resources/views/pages/reporte/reporte1.blade.php
+++ resources/views/pages/reporte/reporte1.blade.php
@@ -1,39 +1,44 @@
 @extends('layouts.model')
+
 @section('title')
     Reporte
 @endsection
+
 @section('content')
 	<h1 class="h5 text-info">
 		<i class="fas fa-file-invoice"></i>
 		Activacion de proveedores
 	</h1>
 	<hr class="row align-items-start col-12">
-<?php	
-	include(app_path().'\functions\config.php');
-	include(app_path().'\functions\querys.php');
-	include(app_path().'\functions\funciones.php');
 
-	$InicioCarga = new DateTime("now");
+	<?php	
+		include(app_path().'\functions\config.php');
+		include(app_path().'\functions\querys.php');
+		include(app_path().'\functions\funciones.php');
 
-	if (isset($_GET['SEDE'])){			
-		echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
-	}
-	echo '<hr class="row align-items-start col-12">';
+		$InicioCarga = new DateTime("now");
 
-	R1_Activacion_Proveedores($_GET['SEDE']);
-	GuardarAuditoria('CONSULTAR','REPORTE','Activacion de proveedores');
+		if (isset($_GET['SEDE'])){			
+			echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+		}
+		echo '<hr class="row align-items-start col-12">';
 
-	$FinCarga = new DateTime("now");
-    $IntervalCarga = $InicioCarga->diff($FinCarga);
-    echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
-?>
+		R1_Activacion_Proveedores($_GET['SEDE']);
+		GuardarAuditoria('CONSULTAR','REPORTE','Activacion de proveedores');
+
+		$FinCarga = new DateTime("now");
+	    $IntervalCarga = $InicioCarga->diff($FinCarga);
+	    echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
+	?>
 @endsection
+
 @section('scriptsHead')
     <script src="{{ asset('assets/js/sortTable.js') }}">	
     </script>
     <script src="{{ asset('assets/js/filter.js') }}">	
     </script>
 @endsection
+
 <?php
 	/*
 		TITULO: Reporte1_Activacion_Proveedores
