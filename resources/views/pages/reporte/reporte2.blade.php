56ddae824650b93696656377af6dad67bd17e274
Sergio Cova <covascode@gmail.com>
2019-09-19 20:01:21 -0400

    Update reporte2.blade.php

--- resources/views/pages/reporte/reporte2.blade.php
+++ resources/views/pages/reporte/reporte2.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('scriptsHead')
-    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">  
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"> 
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
+  <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
+  </script>
+  <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">  
+  </script>
+  <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"> 
+  </script>
+  <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
 
   <style>
     * {
@@ -56,7 +56,77 @@
       background-color: DodgerBlue !important; 
       color: #ffffff; 
     }
+    .barrido{
+      text-decoration: none;
+      transition: width 1s, height 1s, transform 1s;
+    }
+    .barrido:hover{
+      text-decoration: none;
+      transition: width 1s, height 1s, transform 1s;
+      transform: translate(20px,0px);
+    }
   </style>
+@endsection
+
+@section('content')
+	<h1 class="h5 text-info">
+		<i class="fas fa-file-invoice"></i>
+		Historico de productos
+	</h1>
+	<hr class="row align-items-start col-12">
+  <?php	
+  	include(app_path().'\functions\config.php');
+    include(app_path().'\functions\querys.php');
+    include(app_path().'\functions\funciones.php');
+    include(app_path().'\functions\reportes.php');
+
+    $ArtJson = "";
+  	
+  	if (isset($_GET['Id']))
+  	{
+      $InicioCarga = new DateTime("now");
+      
+      if (isset($_GET['SEDE'])){      
+        echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+      }
+      echo '<hr class="row align-items-start col-12">';
+
+      ReporteHistoricoProducto($_GET['SEDE'],$_GET['Id']);
+      GuardarAuditoria('CONSULTAR','REPORTE','Historico de productos');
+
+      $FinCarga = new DateTime("now");
+      $IntervalCarga = $InicioCarga->diff($FinCarga);
+      echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
+  	} 
+  	else{
+      $InicioCarga = new DateTime("now");
+
+      if (isset($_GET['SEDE'])){      
+        echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+      }
+      echo '<hr class="row align-items-start col-12">';
+      
+      $sql = QListaArticulos();
+      $ArtJson = armarJson($sql,$_GET['SEDE']);
+
+  		echo '
+  		<form autocomplete="off" action="" target="_blank">
+  	    <div class="autocomplete" style="width:90%;">
+  	      <input id="myInput" type="text" name="Descrip" placeholder="Ingrese el nombre del articulo " onkeyup="conteo()" required>
+  	      <input id="myId" name="Id" type="hidden">
+          <input id="SEDE" name="SEDE" type="hidden" value="';
+          print_r($_GET['SEDE']);
+          echo'">
+  	    </div>
+  	    <input type="submit" value="Buscar" class="btn btn-outline-success">
+    	</form>
+    	';
+
+      $FinCarga = new DateTime("now");
+      $IntervalCarga = $InicioCarga->diff($FinCarga);
+      echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
+  	} 
+  ?>
 @endsection
 
 @section('scriptsFoot')
@@ -71,69 +141,3 @@
     }
   ?>  
 @endsection
-
-@section('content')
-	<h1 class="h5 text-info">
-		<i class="fas fa-file-invoice"></i>
-		Historico de productos
-	</h1>
-	<hr class="row align-items-start col-12">
-<?php	
-	include(app_path().'\functions\config.php');
-  include(app_path().'\functions\querys.php');
-  include(app_path().'\functions\funciones.php');
-  include(app_path().'\functions\reportes.php');
-
-  $ArtJson = "";
-	
-	if (isset($_GET['Id']))
-	{
-    $InicioCarga = new DateTime("now");
-    
-    if (isset($_GET['SEDE'])){      
-      echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
-    }
-    echo '<hr class="row align-items-start col-12">';
-
-    ReporteHistoricoProducto($_GET['SEDE'],$_GET['Id']);
-    GuardarAuditoria('CONSULTAR','REPORTE','Historico de productos');
-
-    $FinCarga = new DateTime("now");
-    $IntervalCarga = $InicioCarga->diff($FinCarga);
-    echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
-	} 
-	else{
-    $InicioCarga = new DateTime("now");
-
-    if (isset($_GET['SEDE'])){      
-      echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
-    }
-    echo '<hr class="row align-items-start col-12">';
-    
-    $sql = QListaArticulos();
-    $ArtJson = armarJson($sql,$_GET['SEDE']);
-
-		echo '
-		<form autocomplete="off" action="" target="_blank">
-	    <div class="autocomplete" style="width:90%;">
-	      <input id="myInput" type="text" name="Descrip" placeholder="Ingrese el nombre del articulo " onkeyup="conteo()" required>
-	      <input id="myId" name="Id" type="hidden">
-        <input id="SEDE" name="SEDE" type="hidden" value="';
-        print_r($_GET['SEDE']);
-        echo'">
-	    </div>
-	    <input type="submit" value="Buscar" class="btn btn-outline-success">
-  	</form>
-  	';
-
-    $FinCarga = new DateTime("now");
-    $IntervalCarga = $InicioCarga->diff($FinCarga);
-    echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
-	} 
-?>
-@endsection
-
-
-
-
-
