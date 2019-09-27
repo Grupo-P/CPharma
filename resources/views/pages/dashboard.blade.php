3b9dfc1e464e73ea89dd4ce3ac7b86a775c12e61
Sergio Cova <covascode@gmail.com>
2019-09-26 20:04:59 -0400

    CPharma V4.2
    Se integro articulos devaluados

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -40,18 +40,14 @@
 		      </div>
 		      <div class="modal-body">
 		      	<label>Hola <b class="text-info">{{ Auth::user()->name }}</b>!!</label>
-		      	Estas usando<b class="text-info"> CPharma v.4.1</b>, esta version incluye las siguientes mejoras:</label>
+		      	Estas usando<b class="text-info"> CPharma v.4.2</b>, esta version incluye las siguientes mejoras:</label>
 
 		        <ul style="list-style:none">
 		        	<li class="card-text text-dark" style="display: inline;">
     					<br/>
 						<i class="far fa-check-circle text-info" style="display: inline;"></i>
 						Desde ya esta disponible el reporte: 
-						<b class="text-info">Productos en caida</b>!!
-						<br/><br/>
-						<i class="far fa-check-circle text-info" style="display: inline;"></i>
-						Desde ya esta disponible el modulo: 
-						<b class="text-info">Etiquetas</b>!!
+						<b class="text-info">Articulos devaluados</b>!!
     				</li>
 		        	
     				<!-- <li class="card-text text-dark" style="display: inline;">
