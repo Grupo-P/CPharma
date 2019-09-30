2f8cf15533a881df645c62145c22f085530cd9c8
mhenriquezm <manuelhm1993@gmail.com>
2019-09-30 09:31:25 -0400

    Inicio de la estandarizacion
    Se agregaron los identados y se comenzo a construir el nuevo esquema de los reportes

--- resources/views/testM.blade.php
+++ resources/views/testM.blade.php
@@ -13,17 +13,21 @@
     </script>
 
     <style>
+    	* {
+	    	box-sizing: border-box;
+	    }
+
         input {
-          border: 1px solid transparent;
-          background-color: #f1f1f1;
-          border-radius: 5px;
-          padding: 10px;
-          font-size: 16px;
+			border: 1px solid transparent;
+			background-color: #f1f1f1;
+			border-radius: 5px;
+			padding: 10px;
+			font-size: 16px;
         }
 
         input[type=text] {
-          background-color: #f1f1f1;
-          width: 100%;
+			background-color: #f1f1f1;
+			width: 100%;
         }
 
         .barrido {
@@ -50,23 +54,28 @@
         include(app_path().'\functions\config.php');
         include(app_path().'\functions\querys.php');
         include(app_path().'\functions\funciones.php');
-        include(app_path().'\functions\reportes.php');
 
         //--------- Borrar esta linea ---------//
         $_GET['SEDE'] = 'FTN';
         //--------- Borrar esta linea ---------//
 
         if(isset($_GET['fechaInicio'])) {
+
             $InicioCarga = new DateTime("now");
 
             if(isset($_GET['SEDE'])) {
-                echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+                
+                echo '
+                	<h1 class="h5 text-success" align="left">
+                		<i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE'])
+                	.'</h1>
+                ';
             }
 
             echo '<hr class="row align-items-start col-12">';
 
-            ReporteProductosParaSurtir($_GET['SEDE'],$_GET['fechaInicio'],$_GET['fechaFin']);
-        GuardarAuditoria('CONSULTAR','REPORTE','Productos para surtir');
+            R9_Productos_Surtir($_GET['SEDE'], $_GET['fechaInicio'], $_GET['fechaFin']);
+        	GuardarAuditoria('CONSULTAR', 'REPORTE', 'Productos para surtir');
 
             $FinCarga = new DateTime("now");
             $IntervalCarga = $InicioCarga->diff($FinCarga);
@@ -74,7 +83,12 @@
         }
         else {
             if(isset($_GET['SEDE'])) {
-                echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
+
+                echo '
+                	<h1 class="h5 text-success" align="left">
+                		<i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE'])
+                	.'</h1>
+                ';
             }
             echo '<hr class="row align-items-start col-12">';
 
@@ -82,28 +96,24 @@
                 <form autocomplete="off" action="" target="_blank">
                     <table style="width:100%;">
                         <tr>
-                            <td align="center">
-                                Fecha Inicio:
-                            </td>
+                            <td align="center">Fecha Inicio:</td>
                             <td>
-                <input id="fechaInicio" type="date" name="fechaInicio" required style="width:100%;">
-                </td>
-                <td align="center">
-                    Fecha Fin:
-                </td>
-                <td align="right">
-                    <input id="fechaFin" name="fechaFin" type="date" required style="width:100%;">
-                </td>
-                <input id="SEDE" name="SEDE" type="hidden" value="';
-                print_r($_GET['SEDE']);
-                echo'">
-                <td align="right">
-                    <input type="submit" value="Buscar" class="btn btn-outline-success">
-                </td>
-            </tr>
-                </table>
-            </form>
-        ';
+                            	<input id="fechaInicio" type="date" name="fechaInicio" required style="width:100%;">
+                            </td>
+                            <td align="center">Fecha Fin:</td>
+                            <td align="right">
+                            	<input id="fechaFin" name="fechaFin" type="date" required style="width:100%;">
+                            </td>
+                            <td align="right">
+                            	<input id="SEDE" name="SEDE" type="hidden" value="';
+	                            	print_r($_GET['SEDE']);
+	                            echo'">
+                            	<input type="submit" value="Buscar" class="btn btn-outline-success">
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            ';
         }
     ?>
 @endsection
