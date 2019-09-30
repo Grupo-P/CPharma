2910f4dc4b68882397d4edd01d2e82d667c89d75
mhenriquezm <manuelhm1993@gmail.com>
2019-09-30 11:26:41 -0400

    Reubicacion del head
    Siguiendo con el estandar se coloco el include del head a continuacion del layout model

--- resources/views/testM.blade.php
+++ resources/views/testM.blade.php
@@ -4,6 +4,59 @@
 	Reporte
 @endsection
 
+@section('scriptsHead')
+    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">    
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"> 
+    </script>
+    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
+
+    <style>
+        * {box-sizing:border-box;}
+
+        /*the container must be positioned relative:*/
+        .autocomplete {position:relative; display:inline-block;}
+
+        input {
+            border:1px solid transparent;
+            background-color:#f1f1f1;
+            border-radius:5px;
+            padding:10px;
+            font-size:16px;
+        }
+
+        input[type=text] {background-color:#f1f1f1; width:100%;}
+
+        .autocomplete-items {
+            position:absolute;
+            border:1px solid #d4d4d4;
+            border-bottom:none;
+            border-top:none;
+            z-index:99;
+            /*position the autocomplete items to be the same width as the container:*/
+            top:100%;
+            left:0;
+            right:0;
+        }
+
+        .autocomplete-items div {
+            padding:10px;
+            cursor:pointer;
+            background-color:#fff; 
+            border-bottom:1px solid #d4d4d4; 
+        }
+
+        /*when hovering an item:*/
+        .autocomplete-items div:hover {background-color:#e9e9e9;}
+
+        /*when navigating through the items using the arrow keys:*/
+        .autocomplete-active {background-color:DodgerBlue !important; color:#fff;}
+    </style>
+@endsection
+
 @section('content')
 	<h1 class="h5 text-info">
 		<i class="fas fa-file-invoice"></i>
@@ -74,59 +127,6 @@
 		    echo'Tiempo de carga: '.$IntervalCarga->format("%Y-%M-%D %H:%I:%S");
 		}
 	?>
-@endsection
-
-@section('scriptsHead')
-    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}">
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}">	
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}">	
-    </script>
-    <script type="text/javascript" src="{{ asset('assets/jquery/jquery-2.2.2.min.js') }}"></script>
-  	<script type="text/javascript" src="{{ asset('assets/jquery/jquery-ui.min.js') }}" ></script>
-
-  	<style>
-	    * {box-sizing:border-box;}
-
-	    /*the container must be positioned relative:*/
-	    .autocomplete {position:relative; display:inline-block;}
-
-	    input {
-	    	border:1px solid transparent;
-	    	background-color:#f1f1f1;
-	    	border-radius:5px;
-	    	padding:10px;
-	    	font-size:16px;
-	    }
-
-	    input[type=text] {background-color:#f1f1f1; width:100%;}
-
-	    .autocomplete-items {
-	    	position:absolute;
-	    	border:1px solid #d4d4d4;
-	    	border-bottom:none;
-	    	border-top:none;
-	    	z-index:99;
-	    	/*position the autocomplete items to be the same width as the container:*/
-	      	top:100%;
-	      	left:0;
-	      	right:0;
-	    }
-
-	    .autocomplete-items div {
-	    	padding:10px;
-		    cursor:pointer;
-		    background-color:#fff; 
-		    border-bottom:1px solid #d4d4d4; 
-	    }
-
-	    /*when hovering an item:*/
-	    .autocomplete-items div:hover {background-color:#e9e9e9;}
-
-	    /*when navigating through the items using the arrow keys:*/
-	    .autocomplete-active {background-color:DodgerBlue !important; color:#fff;}
-	</style>
 @endsection
 
 @section('scriptsFoot')
