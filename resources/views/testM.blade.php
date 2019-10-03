cc26f0cc19ecf5e3c370f5844256f0bd0d815709
mhenriquezm <manuelhm1993@gmail.com>
2019-10-03 10:20:56 -0400

    Limpiar el codigo de identado
    Se eliminaron identados y espacios sobrantes e innecesarios

--- resources/views/testM.blade.php
+++ resources/views/testM.blade.php
@@ -5,50 +5,60 @@
 @endsection
 
 @section('scriptsHead')
-    <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('assets/js/sortTable.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('assets/js/filter.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('assets/js/functions.js') }}"></script>
 
-    <style>
-      * {box-sizing:border-box;}
+  <style>
+    * {box-sizing:border-box;}
 
-      /*the container must be positioned relative:*/
-      .autocomplete {position:relative; display:inline-block;}
+    /*the container must be positioned relative:*/
+    .autocomplete {position:relative; display:inline-block;}
 
-      input {
-        border:1px solid transparent;
-        background-color:#f1f1f1;
-        border-radius:5px;
-        padding:10px;
-        font-size:16px;
-      }
+    input {
+      border:1px solid transparent;
+      background-color:#f1f1f1;
+      border-radius:5px;
+      padding:10px;
+      font-size:16px;
+    }
 
-      input[type=text] {background-color:#f1f1f1; width:100%;}
+    input[type=text] {background-color:#f1f1f1; width:100%;}
 
-      .autocomplete-items {
-        position:absolute;
-        border:1px solid #d4d4d4;
-        border-bottom:none;
-        border-top:none;
-        z-index:99;
-        /*position the autocomplete items to be the same width as the container:*/
-          top:100%;
-          left:0;
-          right:0;
-      }
+    .autocomplete-items {
+      position:absolute;
+      border:1px solid #d4d4d4;
+      border-bottom:none;
+      border-top:none;
+      z-index:99;
+      /*position the autocomplete items to be the same width as the container:*/
+        top:100%;
+        left:0;
+        right:0;
+    }
 
-      .autocomplete-items div {
-        padding:10px;
-        cursor:pointer;
-        background-color:#fff; 
-        border-bottom:1px solid #d4d4d4; 
-      }
+    .autocomplete-items div {
+      padding:10px;
+      cursor:pointer;
+      background-color:#fff; 
+      border-bottom:1px solid #d4d4d4; 
+    }
 
-      /*when hovering an item:*/
-      .autocomplete-items div:hover {background-color:#e9e9e9;}
+    /*when hovering an item:*/
+    .autocomplete-items div:hover {background-color:#e9e9e9;}
 
-      /*when navigating through the items using the arrow keys:*/
-      .autocomplete-active {background-color:DodgerBlue !important; color:#fff;}
+    /*when navigating through the items using the arrow keys:*/
+    .autocomplete-active {background-color:DodgerBlue !important; color:#fff;}
+
+    .barrido{
+      text-decoration: none;
+      transition: width 1s, height 1s, transform 1s;
+    }
+    .barrido:hover{
+      text-decoration: none;
+      transition: width 1s, height 1s, transform 1s;
+      transform: translate(20px,0px);
+    }
   </style>
 @endsection
 
@@ -75,9 +85,10 @@
 
       if(isset($_GET['SEDE'])) {
         echo '
-            <h1 class="h5 text-success"  align="left">
-              <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).
-            '</h1>';
+          <h1 class="h5 text-success"  align="left">
+            <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE'])
+          .'</h1>
+        ';
       }
       echo '<hr class="row align-items-start col-12">';
 
@@ -91,9 +102,10 @@
     else {
       if(isset($_GET['SEDE'])) {
         echo '
-            <h1 class="h5 text-success"  align="left">
-              <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).
-            '</h1>';
+          <h1 class="h5 text-success" align="left">
+            <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE'])
+          .'</h1>
+        ';
       }
       echo '<hr class="row align-items-start col-12">';
 
@@ -105,23 +117,14 @@
         <form id="form" autocomplete="off" action="" target="_blank">
           <table style="width:100%;">
             <tr>
-              <td align="center">
-                Fecha Inicio:
-              </td>
-
+              <td align="center">Fecha Inicio:</td>
               <td>
                 <input id="fechaInicio" type="date" name="fechaInicio" required style="width:100%;">
               </td>
 
-              <td align="center">
-                Fecha Fin:
-              </td>
-
+              <td align="center">Fecha Fin:</td>
               <td align="right">
                 <input id="fechaFin" name="fechaFin" type="date" required style="width:100%;">
-              </td>
-
-              <td>
                 <input id="SEDE" name="SEDE" type="hidden" value="'; print_r($_GET['SEDE']);
                 echo'">
               </td>
