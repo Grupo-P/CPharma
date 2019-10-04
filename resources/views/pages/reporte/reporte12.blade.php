3321ba31d2d78f3c011d1a40967056328055a46e
mhenriquezm <manuelhm1993@gmail.com>
2019-10-04 11:44:27 -0400

    Forzado de tierra negra borrado
    Se elimino la linea de forzar la base de datos de desarrollo

--- resources/views/pages/reporte/reporte12.blade.php
+++ resources/views/pages/reporte/reporte12.blade.php
@@ -75,10 +75,6 @@
     include(app_path().'\functions\funciones.php');
 
     $ArtJson="";
-
-    //--------- BORRAR ESTA LINEA ---------//
-    $_GET['SEDE'] = 'DBm';
-    //--------- BORRAR ESTA LINEA ---------//
     
     if(isset($_GET['Id'])) {
       $InicioCarga = new DateTime("now");
