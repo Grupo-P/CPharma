f4a37b9250c9c2bfa24a856a6ca8edbcd7b46e03
mhenriquezm <manuelhm1993@gmail.com>
2019-10-09 08:36:56 -0400

    Identado a 2 espacios encabezados

--- resources/views/pages/tasaVenta/edit.blade.php
+++ resources/views/pages/tasaVenta/edit.blade.php
@@ -1,70 +1,69 @@
 @extends('layouts.model')
 
 @section('title')
-    Tasa venta
+  Tasa venta
 @endsection
 
 @section('content')
-    <?php
-        include(app_path().'\functions\config.php');
-        include(app_path().'\functions\querys.php');
-        include(app_path().'\functions\funciones.php');
+  <?php
+    include(app_path().'\functions\config.php');
+    include(app_path().'\functions\querys.php');
+    include(app_path().'\functions\funciones.php');
 
-        $conCP = ConectarXampp();
-        
-        $sql1 = QG_RangoMinTasaVenta();
-        $result1 = mysqli_query($conCP, $sql1);
-        $row1 = mysqli_fetch_assoc($result1);
-        $minimo = floatval($row1['valor']);
+    $conCP = ConectarXampp();
+    
+    $sql1 = QG_RangoMinTasaVenta();
+    $result1 = mysqli_query($conCP, $sql1);
+    $row1 = mysqli_fetch_assoc($result1);
+    $minimo = floatval($row1['valor']);
 
-        $sql2 = QG_RangoMaxTasaVenta();
-        $result2 = mysqli_query($conCP, $sql2);
-        $row2 = mysqli_fetch_assoc($result2);
-        $maximo = floatval($row2['valor']);
+    $sql2 = QG_RangoMaxTasaVenta();
+    $result2 = mysqli_query($conCP, $sql2);
+    $row2 = mysqli_fetch_assoc($result2);
+    $maximo = floatval($row2['valor']);
 
-        mysqli_close($conCP);
-    ?>
+    mysqli_close($conCP);
+  ?>
 
-    <!-- Modal Guardar -->
-    @if(session('Error'))
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title text-danger" id="exampleModalCenterTitle">
-                  <i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <h4 class="h6">El registro no pudo ser almacenado, la fecha ya esta registrada</h4>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
-              </div>
-            </div>
+  <!-- Modal Guardar -->
+  @if(session('Error'))
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-danger" id="exampleModalCenterTitle">
+              <i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="h6">El registro no pudo ser almacenado, la fecha ya esta registrada</h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
           </div>
         </div>
-    @endif
+      </div>
+    </div>
+  @endif
 
-    <h1 class="h5 text-info">
-        <i class="fas fa-edit"></i>
-        Modificar tasa de venta
-    </h1>
+  <h1 class="h5 text-info">
+    <i class="fas fa-edit"></i>
+    Modificar tasa de venta
+  </h1>
 
-    <hr class="row align-items-start col-12">
+  <hr class="row align-items-start col-12">
 
-    <form action="/tasaVenta/" method="POST" style="display:inline;">  
-        @csrf                       
-        <button type="submit" name="Regresar" role="button" class="btn btn-outline-info btn-sm"data-placement="top">
-          <i class="fa fa-reply">&nbsp;Regresar</i>
-        </button>
-    </form>
+  <form action="/tasaVenta/" method="POST" style="display:inline;">  
+    @csrf                       
+    <button type="submit" name="Regresar" role="button" class="btn btn-outline-info btn-sm"data-placement="top">
+      <i class="fa fa-reply">&nbsp;Regresar</i>
+    </button>
+  </form>
 
-    <br>
-    <br>
+  <br><br>
 
     {!! Form::model($tasaVenta, ['route' => ['tasaVenta.update', $tasaVenta], 'method' => 'PUT']) !!}
     <fieldset>
