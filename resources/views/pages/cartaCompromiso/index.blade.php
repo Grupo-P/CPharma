bb793fb31f2e724b8a57886051238364b35fe4af
mhenriquezm <manuelhm1993@gmail.com>
2019-10-14 15:38:48 -0400

    Index modal 3 identado

--- resources/views/pages/cartaCompromiso/index.blade.php
+++ resources/views/pages/cartaCompromiso/index.blade.php
@@ -50,26 +50,26 @@
   @endif
 
   <!-- Modal Eliminar -->
-	@if(session('Deleted'))
-		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-		  <div class="modal-dialog modal-dialog-centered" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Deleted') }}</h5>
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		          <span aria-hidden="true">&times;</span>
-		        </button>
-		      </div>
-		      <div class="modal-body">
-		        <h4 class="h6">Compromiso actualizado con exito</h4>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>	
-		      </div>
-		    </div>
-		  </div>
-		</div>
-	@endif
+  @if(session('Deleted'))
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Deleted') }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="h6">Compromiso actualizado con exito</h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endif
 
 	<h1 class="h5 text-info">
 		<i class="fas fa-file-invoice"></i>
