d52ad786cf486e2cfbc5ab77bc256bb8c0b801d6
mhenriquezm <manuelhm1993@gmail.com>
2019-10-16 11:58:16 -0400

    Tabla creada
    Se creo la logica necesaria para la definicion de la tabla y se ejecuto dicha migracion

--- database/migrations/2019_10_16_115442_create_rh_candidatos_table.php
+++ database/migrations/2019_10_16_115442_create_rh_candidatos_table.php
@@ -4,17 +4,29 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRhCandidatosTable extends Migration
-{
+class CreateRhCandidatosTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('rh_candidatos', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('cedula')->unique();
+            $table->string('direccion');
+            $table->string('telefono_celular')->nullable();
+            $table->string('telefono_habitacion')->nullable();
+            $table->string('correo')->unique()->nullable();
+            $table->string('como_nos_contacto')->nullable();
+            $table->string('experiencia_laboral')->nullable();
+            $table->string('observaciones')->nullable();
+            $table->string('tipo_relacion');
+            $table->string('relaciones_laborales');
+            $table->string('estatus');
+            $table->string('vacante');
             $table->timestamps();
         });
     }
@@ -24,8 +36,7 @@ class CreateRhCandidatosTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('rh_candidatos');
     }
 }
