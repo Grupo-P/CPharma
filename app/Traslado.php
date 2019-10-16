e4cada99c185f2a6e83acf362752dc8e06636c89
covacode <covascode@gmail.com>
2019-10-16 09:07:55 -0400

    Update Traslado Model
    se completaron los fillable para el modelo del traslado

--- app/Traslado.php
+++ app/Traslado.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Traslado extends Model
 {
-    //
+  /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'id','numero_ajuste','fecha_ajuste','fecha_traslado','sede_emisora','sede_destino','operador_ajuste','operador_traslado','estatus'
+    ];
 }
