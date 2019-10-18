fbef37cbf9f387984e35a1fd800e7a269c6bc194
covacode <covascode@gmail.com>
2019-10-18 18:26:41 -0400

    Update NavBar
    se actualizo la vista del nav bar por completo

--- resources/views/includes/header_in.blade.php
+++ resources/views/includes/header_in.blade.php
@@ -1,15 +1,15 @@
 <!-- NavBar / Barra Navegacion -->
-<nav class="navbar navbar-expand-lg bg-white text-info" style="height:105px;">
+<nav class="navbar navbar-expand-lg bg-white text-info" style="height:110px;">
   <!-- Navbar brand -->
   <a class="navbar-brand text-info CP-title-NavBar bg-white" href="{{ url('/') }}"><b><i class="fas fa-syringe text-success"></i>CPharma</b>
   </a>
 <!-------------------------------------------------------------------------------->
   <!-- DASHBOARD -->
   <li class="navbar-brand">
-    <a class="navbar-brand textoN btn btn-outline-info" href="{{ url('/home') }}" role="button" data-toggle="tooltip" data-placement="top" title="DASHBOARD"> 
+    <a class="navbar-brand btn btn-outline-info textoN" href="{{ url('/home') }}" role="button" data-toggle="tooltip" data-placement="top" title="DASHBOARD"> 
       <span data-feather="home"></span>
       <i class="fas fa-columns"></i>
-      <span class="sr-only">(current)</span>
+      Dashboard<span class="sr-only">(current)</span>
     </a>
   </li>
   <!-- DASHBOARD -->
@@ -19,7 +19,7 @@
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-book"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu">
+    <div class="dropdown-menu">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/empresa') }}">
           <span data-feather="home"></span>
@@ -50,7 +50,7 @@
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-chart-bar"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu">
+    <div class="dropdown-menu">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
           <span data-feather="home"></span>
@@ -67,17 +67,17 @@
 <!-------------------------------------------------------------------------------->
 <?php
   if(
-    Auth::user()->departamento == 'SURTIDO'
+    Auth::user()->departamento == 'OPERACIONES'
     || Auth::user()->departamento == 'GERENCIA'
     || Auth::user()->departamento == 'TECNOLOGIA'
   ){
 ?>
-  <!-- SURTIDO -->
-  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="SURTIDO">
+  <!-- OPERACIONES -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="OPERACIONES">
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-dolly-flatbed"></i>
+      <i class="fas fa-tasks"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu">
+    <div class="dropdown-menu">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
           <span data-feather="home"></span>
@@ -85,16 +85,9 @@
           Reportes<span class="sr-only">(current)</span>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link CP-Links-Menu" href="{{ url('/etiqueta') }}">     
-          <span data-feather="home"></span>
-          <i class="fas fa-tag"></i>
-          Etiquetas<span class="sr-only">(current)</span>
-        </a>
-      </li>
     </div>
   </div>
-  <!-- SURTIDO -->
+  <!-- OPERACIONES -->
 <?php
   }
 ?>
@@ -111,7 +104,7 @@
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-warehouse"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu">
+    <div class="dropdown-menu">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
           <span data-feather="home"></span>
@@ -135,6 +128,67 @@
 <!-------------------------------------------------------------------------------->
 <?php
   if(
+    Auth::user()->departamento == 'DEVOLUCIONES'
+    || Auth::user()->departamento == 'GERENCIA'
+    || Auth::user()->departamento == 'TECNOLOGIA'
+  ){
+?>
+  <!-- DEVOLUCIONES -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="DEVOLUCIONES">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-history"></i>
+    </button>
+    <div class="dropdown-menu">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-file-invoice"></i>
+          Reportes<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
+  </div>
+  <!-- DEVOLUCIONES -->
+<?php
+  }
+?>
+<!-------------------------------------------------------------------------------->
+<?php
+  if(
+    Auth::user()->departamento == 'SURTIDO'
+    || Auth::user()->departamento == 'GERENCIA'
+    || Auth::user()->departamento == 'TECNOLOGIA'
+  ){
+?>
+  <!-- SURTIDO -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="SURTIDO">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-dolly-flatbed"></i>
+    </button>
+    <div class="dropdown-menu">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-file-invoice"></i>
+          Reportes<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/etiqueta') }}">     
+          <span data-feather="home"></span>
+          <i class="fas fa-tag"></i>
+          Etiquetas<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
+  </div>
+  <!-- SURTIDO -->
+<?php
+  }
+?>
+<!-------------------------------------------------------------------------------->
+<?php
+  if(
     Auth::user()->departamento == 'VENTAS'
     || Auth::user()->departamento == 'GERENCIA'
     || Auth::user()->departamento == 'TECNOLOGIA'
@@ -143,9 +197,9 @@
   <!-- VENTAS -->
   <div class="btn-group" data-toggle="tooltip" data-placement="top" title="VENTAS">
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-dollar-sign"></i>
+      <i class="fas fa-cash-register"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu">
+    <div class="dropdown-menu">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/tasaVenta') }}">
           <span data-feather="home"></span>
@@ -172,7 +226,7 @@
     <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <i class="fas fa-calculator"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu" style="width:200px;">
+    <div class="dropdown-menu" style="width:200px;">
       <li class="nav-item">
         <a class="nav-link CP-Links-Menu" href="{{ url('/dolar') }}">
           <span data-feather="home"></span>
@@ -207,50 +261,217 @@
 <?php
   }
 ?>
-
-
-
-
-
-
-
-
-
-  <div class="collapse navbar-collapse" id="basicExampleNav">
-    <ul class="navbar-nav ml-auto">
-      @guest
-        
-      @else
-        <li class="nav-item dropdown">
-          <a id="navbarDropdown" class="nav-link dropdown-toggle text-succees CP-Links-Menu" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-          <i class="fas fa-user text-succees"></i>
-          {{ Auth::user()->name }} <span class="caret text-succees"></span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right CP-divborder" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item CP-aborder" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                       document.getElementById('logout-form').submit();">
-            <i class="fas fa-sign-out-alt"></i>
-            {{ __('Cerrar Sesión') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-            </form>
-          </div>
-        </li>
-      @endguest
-    </ul>               
+<!-------------------------------------------------------------------------------->
+<?php
+  if(
+    Auth::user()->departamento == 'LÍDER DE TIENDA'
+    || Auth::user()->departamento == 'GERENCIA'
+    || Auth::user()->departamento == 'TECNOLOGIA'
+  ){
+?>
+  <!-- LÍDER DE TIENDA -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="LÍDER DE TIENDA">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-store"></i>
+    </button>
+    <div class="dropdown-menu">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-file-invoice"></i>
+          Reportes<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
   </div>
-
-  <!-- Collapse button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fas fa-bars text-success"></i>
-  </button>
-  <!-- Collapsible content -->
-
+  <!-- LÍDER DE TIENDA -->
+<?php
+  }
+?>
+<!-------------------------------------------------------------------------------->
+<?php
+  if(
+    Auth::user()->departamento == 'GERENCIA'
+    || Auth::user()->departamento == 'TECNOLOGIA'
+  ){
+?>
+  <!-- GERENCIA -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="GERENCIA">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-user-tie"></i>
+    </button>
+    <div class="dropdown-menu">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-file-invoice"></i>
+          Reportes<span class="sr-only">(current)</span>
+        </a>
+        <a class="nav-link CP-Links-Menu" href="{{ url('/usuario') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-user"></i>
+          Usuario<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
+  </div>
+  <!-- GERENCIA -->
+<?php
+  }
+?>
+<!------------------------------------------------------------------------------->
+<?php
+  if(
+    Auth::user()->departamento == 'TECNOLOGIA'
+  ){
+?>
+  <!-- TECNOLOGIA -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="TECNOLOGIA">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-user-cog"></i>
+    </button>
+    <div class="dropdown-menu" style="width:200px;">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sedes_reporte') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-file-invoice"></i>
+          Reportes<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/usuario') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-user"></i>
+          Usuario<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/rol') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-user-circle"></i>
+          Roles<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/departamento') }}">
+          <span data-feather="home"></span>
+          <i class="fab fa-buffer"></i>
+          Departamento<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/sede') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-store-alt"></i>
+          Sede<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/conexion') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-network-wired"></i>
+          Conexion<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/configuracion') }}">      
+          <span data-feather="home"></span>
+          <i class="fas fa-cogs"></i>
+          Configuracion<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/diascero') }}">      
+          <span data-feather="home"></span>
+          <i class="far fa-calendar"></i>
+          Dias en cero<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/productoscaida') }}">      
+          <span data-feather="home"></span>
+          <i class="fas fa-chart-line"></i>
+          Productos en Caida<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/CapturaEtiqueta') }}">      
+          <span data-feather="home"></span>
+          <i class="fas fa-tag"></i>
+          Captura Etiquetas<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/auditoria') }}">      
+          <span data-feather="home"></span>
+          <i class="fas fa-search"></i>
+          Auditoria<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
+  </div>
+  <!-- TECNOLOGIA -->
+<?php
+  }
+?>
+<!-------------------------------------------------------------------------------->
+<?php
+  if(
+    Auth::user()->departamento == 'TECNOLOGIA'
+    || Auth::user()->role == 'DEVELOPER'
+  ){
+?>
+  <!-- DEVELOPER -->
+  <div class="btn-group" data-toggle="tooltip" data-placement="top" title="DEVELOPER">
+    <button type="button" class="btn btn-outline-info dropdown-toggle textoI" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-user-secret"></i>
+    </button>
+    <div class="dropdown-menu">
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/testS') }}">     
+          <span data-feather="home"></span>
+          <i class="fas fa-box"></i>
+          Test Sergio<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/testM') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-box"></i>
+          Test Manuel<span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link CP-Links-Menu" href="{{ url('/testR') }}">
+          <span data-feather="home"></span>
+          <i class="fas fa-box"></i>
+          Test Rubmary<span class="sr-only">(current)</span>
+        </a>
+      </li>
+    </div>
+  </div>
+  <!-- DEVELOPER -->
+<?php
+  }
+?>
+<!-------------------------------------------------------------------------------->
+  <!-- SALIR -->
+  <li class="navbar-brand">
+    <a class="navbar-brand textoN btn btn-outline-info" href="{{ route('logout') }}" role="button" data-toggle="tooltip" data-placement="top" title="SALIR"onclick="event.preventDefault();
+                       document.getElementById('logout-form').submit();">
+      <span data-feather="home"></span>
+      <i class="fas fa-power-off"></i>
+      {{Auth::user()->name}}<span class="sr-only">(current)</span>
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+     @csrf
+    </form>
+  </li>
+  <!-- SALIR -->
+<!-------------------------------------------------------------------------------->
 </nav>
 <hr class="row align-items-start col-12"> 
+
 <script>
   $(document).ready(function(){
       $('[data-toggle="tooltip"]').tooltip();   
