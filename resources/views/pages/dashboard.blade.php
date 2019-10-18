8db9690bd703737dd663bde41f5e6548df90e1ad
covacode <covascode@gmail.com>
2019-10-17 21:19:12 -0400

    CPharma V4.4
    se versiono el sistema a CPharma v 4.4

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -32,7 +32,7 @@
 		      </div>
 		      <div class="modal-body">
 		      	<label>Hola <b class="text-info">{{ Auth::user()->name }}</b>!!</label>
-		      	Estas usando<b class="text-info"> CPharma v.4.3</b>, esta version incluye las siguientes mejoras:<br/><br/></label>
+		      	Estas usando<b class="text-info"> CPharma v.4.4</b>, esta version incluye las siguientes mejoras:<br/><br/></label>
 	        <ul style="list-style:none">
 	        	<li class="card-text text-dark" style="display: inline;">
 						<i class="far fa-check-circle text-info" style="display: inline;"></i>
@@ -45,6 +45,41 @@
 						<i class="far fa-check-circle text-info" style="display: inline;"></i>
 						Desde ya esta disponible el modulo: 
 						<b class="text-info">Traslado</b>!!
+    				</li>
+    			</ul>
+    			<ul style="list-style:none">
+	        	<li class="card-text text-dark" style="display: inline;">
+						<i class="far fa-check-circle text-info" style="display: inline;"></i>
+						Se actualizo el reporte: 
+						<b class="text-info">Productos mas vendidos</b>!!
+    				</li>
+    			</ul>
+    			<ul style="list-style:none">
+	        	<li class="card-text text-dark" style="display: inline;">
+						<i class="far fa-check-circle text-info" style="display: inline;"></i>
+						Se actualizo el reporte: 
+						<b class="text-info">Productos menos vendidos</b>!!
+    				</li>
+    			</ul>
+    			<ul style="list-style:none">
+	        	<li class="card-text text-dark" style="display: inline;">
+						<i class="far fa-check-circle text-info" style="display: inline;"></i>
+						Se actualizo el reporte: 
+						<b class="text-info">Pedido de productos</b>!!
+    				</li>
+    			</ul>
+    			<ul style="list-style:none">
+	        	<li class="card-text text-dark" style="display: inline;">
+						<i class="far fa-check-circle text-info" style="display: inline;"></i>
+						Se actualizo el reporte: 
+						<b class="text-info">Catalogo de proveedor</b>!!
+    				</li>
+    			</ul>
+    			<ul style="list-style:none">
+	        	<li class="card-text text-dark" style="display: inline;">
+						<i class="far fa-check-circle text-info" style="display: inline;"></i>
+						Se actualizo el reporte: 
+						<b class="text-info">Productos en Caida</b>!!
     				</li>
     			</ul>
 		      </div>
