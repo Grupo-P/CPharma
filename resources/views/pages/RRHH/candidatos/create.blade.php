68543a3c2e528a2f67cd8dc5cbbdd52c4a8d230e
mhenriquezm <manuelhm1993@gmail.com>
2019-10-23 16:35:33 -0400

    Modal de correo duplicado

--- resources/views/pages/RRHH/candidatos/create.blade.php
+++ resources/views/pages/RRHH/candidatos/create.blade.php
@@ -65,6 +65,33 @@
     </div>
   @endif
 
+  <!-- Modal Correo duplicado -->
+  @if(session('Error2'))
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-danger" id="exampleModalCenterTitle">
+              <i class="fas fa-exclamation-triangle text-danger"></i>
+              {{ session('Error') }}
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="h6">
+              El candidato no fue almacenado, el correo ya esta registrado
+            </h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endif
+
   <h1 class="h5 text-info"><i class="fas fa-plus"></i>&nbsp;Agregar candidato</h1>
   <hr class="row align-items-start col-12">
 
