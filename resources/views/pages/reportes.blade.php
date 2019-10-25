c661011f4d5a83903f3f87ac4f972a2705e4d0e8
covacode <covascode@gmail.com>
2019-10-25 15:58:41 -0400

    Update reportes
    se actualizaron las politicas de privacodad prar compras

--- resources/views/pages/reportes.blade.php
+++ resources/views/pages/reportes.blade.php
@@ -60,6 +60,23 @@
 				</form>
 	  	</div>
 		</div>
+		<div class="card border-info mb-3" style="width: 14rem;">	  	
+  		<div class="card-body text-left bg-info">
+    		<h5 class="card-title">
+	    		<span class="card-text text-white">
+	    			Productos mas vendidos
+	    		</span>
+    		</h5>	    	    
+  		</div>
+	  	<div class="card-footer bg-transparent border-info text-right">
+	  		<form action="/reporte3/" style="display: inline;">
+			    @csrf
+			    <input id="SEDE" name="SEDE" type="hidden" value="<?php print_r($_GET['SEDE']); ?>">				   
+			    <button type="submit" name="Reporte" role="button" class="btn btn-outline-info btn-sm"></i>Visualizar</button>
+				</form>
+	  	</div>
+		</div>
+	</div>
 <?php
   }
 ?>
