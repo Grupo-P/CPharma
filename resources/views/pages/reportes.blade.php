72af946da0c71db3d5a95f7c25b0c944daa50f9d
covacode <covascode@gmail.com>
2019-10-25 15:35:00 -0400

    Update reporte  compras

--- resources/views/pages/reportes.blade.php
+++ resources/views/pages/reportes.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.model')
 
 @section('title')
-    Reportes
+  Reportes
 @endsection
 
 @section('content')
@@ -20,10 +20,58 @@
 			echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.NombreSede($_GET['SEDE']).'</h1>';
 		}		
 	?>
-
 	<hr class="row align-items-start col-12">
+
+<!-------------------------------------------------------------------------------->
+<!-- COMPRAS -->
+<?php
+  if(Auth::user()->departamento == 'COMPRAS'){
+?>
+	<div class="card-deck">
+		<div class="card border-danger mb-3" style="width: 14rem;">	  	
+			<div class="card-body text-left bg-danger">
+	  		<h5 class="card-title">
+	    		<span class="card-text text-white">
+	    			Activacion de proveedores
+	    		</span>
+	  		</h5>	    	    
+			</div>
+	  	<div class="card-footer bg-transparent border-danger text-right">
+	  		<form action="/reporte1/" style="display: inline;">
+			    @csrf
+			    <input id="SEDE" name="SEDE" type="hidden" value="<?php print_r($_GET['SEDE']); ?>">
+			    <button type="submit" name="Reporte" role="button" class="btn btn-outline-danger btn-sm"></i>Visualizar</button>
+				</form>
+	  	</div>
+		</div>
+		<div class="card border-success mb-3" style="width: 14rem;">	  	
+  		<div class="card-body text-left bg-success">
+    		<h5 class="card-title">
+	    		<span class="card-text text-white">
+	    			Historico de productos
+	    		</span>
+    		</h5>	    	    
+  		</div>
+	  	<div class="card-footer bg-transparent border-success text-right">
+	  		<form action="/reporte2/" style="display: inline;">
+			    @csrf
+			    <input id="SEDE" name="SEDE" type="hidden" value="<?php print_r($_GET['SEDE']); ?>">				   				   
+			    <button type="submit" name="Reporte" role="button" class="btn btn-outline-success btn-sm"></i>Visualizar</button>
+				</form>
+	  	</div>
+		</div>
+<?php
+  }
+?>
+<!-- COMPRAS -->
+<!-------------------------------------------------------------------------------->
+
+
+<?php
+  if(Auth::user()->departamento == 'klk'){
+?>
    	<div class="card-deck">
-   		<!-- Reportes -->
+ 		<!-- Reportes -->
 		<div class="card border-danger mb-3" style="width: 14rem;">	  	
 	  		<div class="card-body text-left bg-danger">
 	    		<h5 class="card-title">
@@ -455,4 +503,7 @@
 		  	</div>
 		</div>
    	</div>
+<?php
+  }
+?>
 @endsection
