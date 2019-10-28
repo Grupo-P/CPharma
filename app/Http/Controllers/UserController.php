acc88ad65e4a1215aedd0a4c75a24d9b4a0fa55c
covacode <covascode@gmail.com>
2019-10-28 19:26:01 -0400

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,8 +28,17 @@ class UserController extends Controller
      */
     public function index()
     {
-        $usuarios =  User::all();
-        return view('pages.usuario.index', compact('usuarios'));
+        if(auth()->user()->role == 'DEVELOPER'){
+            $usuarios =  User::all();
+            return view('pages.usuario.index', compact('usuarios'));
+        }
+        else{
+            $usuarios =  
+            User::all()
+            ->where('role', '<>','DEVELOPER');
+            return view('pages.usuario.index', compact('usuarios'));
+        }
+        
     }
 
     /**
