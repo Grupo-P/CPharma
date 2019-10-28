f867b25f637adeaacb176a049040292f03fa83d6
mhenriquezm <manuelhm1993@gmail.com>
2019-10-28 12:43:04 -0400

    Plantilla edit creada

--- resources/views/pages/RRHH/candidatos/edit.blade.php
+++ resources/views/pages/RRHH/candidatos/edit.blade.php
@@ -1,1 +1,96 @@
 @extends('layouts.model')
+
+@section('title')
+  Candidato
+@endsection
+
+<!-- Modal Guardar -->
+@if(session('Error'))
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-danger" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h4 class="h6">El candidato no fue almacenado</h4>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+@endif
+
+@section('content')
+    <h1 class="h5 text-info">
+        <i class="fas fa-edit"></i>
+        Modificar candidato
+    </h1>
+
+    <hr class="row align-items-start col-12">
+
+    <form action="/candidatos/" method="POST" style="display: inline;">  
+        @csrf                       
+        <button type="submit" name="Regresar" role="button" class="btn btn-outline-info btn-sm"data-placement="top"><i class="fa fa-reply">&nbsp;Regresar</i></button>
+    </form>
+
+    <br>
+    <br>
+    {!! Form::model($candidatos, ['route' => ['candidatos.update', $candidatos], 'method' => 'PUT']) !!}
+    <fieldset>
+
+        <table class="table table-borderless table-striped">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="row"></th>
+                <th scope="row"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">{!! Form::label('nombre', 'Nombre') !!}</th>
+                <td>{!! Form::text('nombre', null, [ 'class' => 'form-control', 'placeholder' => 'Pedro', 'autofocus']) !!}</td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('apellido', 'Apellido') !!}</th>
+                <td>{!! Form::text('apellido', null, [ 'class' => 'form-control', 'placeholder' => 'Perez']) !!}</td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('telefono', 'Telefono') !!}</th>
+                <td>{!! Form::text('telefono', null, [ 'class' => 'form-control', 'placeholder' => '04XX-7988326']) !!}</td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('correo', 'Correo') !!}</th>
+                <td>{!! Form::text('correo', null, [ 'class' => 'form-control', 'placeholder' => 'pperez@empresa.com']) !!}</td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('cargo', 'Cargo') !!}</th>
+                <td>{!! Form::text('cargo', null, [ 'class' => 'form-control', 'placeholder' => 'candidatos']) !!}</td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('empresa', 'Empresa') !!}
+                </th>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">{!! Form::label('observacion', 'Observaciones') !!}</th>
+                <td>{!! Form::textarea('observacion', null, [ 'class' => 'form-control', 'placeholder' => 'Detalles importantes de la empresa', 'rows' => '3']) !!}</td>
+            </tr>
+        </tbody>
+        </table>
+        {!! Form::submit('Guardar', ['class' => 'btn btn-outline-success btn-md']) !!}
+    </fieldset>
+    {!! Form::close()!!}
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+        $('#exampleModalCenter').modal('show')
+    </script>
+@endsection
