8828886a27a19954e9e6559faf7f5e09b188138b
pvielmarubmary <56359288+pvielmarubmary@users.noreply.github.com>
2019-10-29 11:45:26 -0400

    Nombre de la tabla
    nombre especificado para el modelo

--- app/RH_Entrevista.php
+++ app/RH_Entrevista.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class RH_Entrevista extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_entrevistas';
 }
