41425d80ccfe4ab62b634317f316e17cf0f26acc
covacode <covascode@gmail.com>
2019-10-30 10:51:13 -0400

    Update 2019_10_30_095544_create_orden_compra_detalles_table.php
    Modelado de la base de datos para detalles de orden de compra

--- database/migrations/2019_10_30_095544_create_orden_compra_detalles_table.php
+++ database/migrations/2019_10_30_095544_create_orden_compra_detalles_table.php
@@ -9,12 +9,34 @@ class CreateOrdenCompraDetallesTable extends Migration
     /**
      * Run the migrations.
      *
+     *  $table->string('sede1'); -> FTN
+        $table->string('sede2'); -> FLL
+        $table->string('sede3'); -> FAU
+        $table->string('sede4'); -> MC
+     *
      * @return void
      */
     public function up()
     {
         Schema::create('orden_compra_detalles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('codigo_orden');
+            $table->integer('id_articulo');
+            $table->string('codigo_articulo');
+            $table->string('codigo_barra');
+            $table->string('descripcion');
+            $table->string('existencia_actual');
+            $table->string('sede1');
+            $table->string('sede2');
+            $table->string('sede3');
+            $table->string('sede4');
+            $table->string('total_unidades');
+            $table->string('costo_unitario');
+            $table->string('costo_total');
+            $table->string('existencia_rpt');
+            $table->string('dias_restantes_rpt');
+            $table->string('origen_rpt');
+            $table->string('rango_rpt');
             $table->timestamps();
         });
     }
