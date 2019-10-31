3a5abf6bd34982b33c164812606d6f6338aa8114
mhenriquezm <manuelhm1993@gmail.com>
2019-10-31 14:32:15 -0400

    Tabla asociada personalizada

--- app/RH_Vacante.php
+++ app/RH_Vacante.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RH_Vacante extends Model {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_vacantes';
 }
