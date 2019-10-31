a816204015aa9bfa8affdc6f19ced920ec4c7d9c
mhenriquezm <manuelhm1993@gmail.com>
2019-10-31 16:20:43 -0400

    Autenticacion agregada

--- app/Http/Controllers/RH_VacanteController.php
+++ app/Http/Controllers/RH_VacanteController.php
@@ -3,8 +3,19 @@
 namespace compras\Http\Controllers;
 
 use Illuminate\Http\Request;
+use compras\RH_Vacante;
+use compras\User;
 
 class RH_VacanteController extends Controller {
+    /**
+     * Create a new controller instance with auth.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
