b1c485f879d80625e31fa0feacdef6a04ecd5ef1
mhenriquezm <manuelhm1993@gmail.com>
2019-10-31 14:56:53 -0400

    Campos definidos

--- database/migrations/2019_10_31_143427_create_rh_vacantes_table.php
+++ database/migrations/2019_10_31_143427_create_rh_vacantes_table.php
@@ -13,6 +13,18 @@ class CreateRhVacantesTable extends Migration {
     public function up() {
         Schema::create('rh_vacantes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nombre_vacante');
+            $table->string('departamento');
+            $table->string('turno');
+            $table->string('dias_libres');
+            $table->string('sede');
+            $table->unsignedInteger('cantidad');
+            $table->date('fecha_solicitud');
+            $table->date('fecha_limite');
+            $table->string('nivel_urgencia');
+            $table->string('solicitante');
+            $table->text('comentarios');
+            $table->string('user');
             $table->timestamps();
         });
     }
