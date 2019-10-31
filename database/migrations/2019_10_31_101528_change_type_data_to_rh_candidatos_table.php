d54307c04473890c4642f2a886b178bf870c1e6e
mhenriquezm <manuelhm1993@gmail.com>
2019-10-31 10:34:05 -0400

    Migracion definida

--- database/migrations/2019_10_31_101528_change_type_data_to_rh_candidatos_table.php
+++ database/migrations/2019_10_31_101528_change_type_data_to_rh_candidatos_table.php
@@ -12,7 +12,17 @@ class ChangeTypeDataToRhCandidatosTable extends Migration {
      */
     public function up() {
         Schema::table('rh_candidatos', function (Blueprint $table) {
-            //
+            $table->text('direccion')->after('cedula')->change();
+
+            $table->text('experiencia_laboral')
+            ->after('como_nos_contacto')
+            ->nullable()
+            ->change();
+
+            $table->text('observaciones')
+            ->after('experiencia_laboral')
+            ->nullable()
+            ->change();
         });
     }
 
@@ -23,7 +33,17 @@ class ChangeTypeDataToRhCandidatosTable extends Migration {
      */
     public function down() {
         Schema::table('rh_candidatos', function (Blueprint $table) {
-            //
+            $table->string('direccion')->after('cedula')->change();
+
+            $table->string('experiencia_laboral')
+            ->after('como_nos_contacto')
+            ->nullable()
+            ->change();
+
+            $table->string('observaciones')
+            ->after('experiencia_laboral')
+            ->nullable()
+            ->change();
         });
     }
 }
