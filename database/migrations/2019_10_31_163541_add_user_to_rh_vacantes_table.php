85563652b051792c423386ce234cb150ddfa8e8a
mhenriquezm <manuelhm1993@gmail.com>
2019-10-31 16:39:11 -0400

    Migracion para estatus

---
+++ database/migrations/2019_10_31_163541_add_user_to_rh_vacantes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToRhVacantesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('rh_vacantes', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('rh_vacantes', function (Blueprint $table) {
+            //
+        });
+    }
+}
