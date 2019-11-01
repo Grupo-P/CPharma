4590db92bc677ef4d9f83c38f45eff10e8ca5e93
mhenriquezm <manuelhm1993@gmail.com>
2019-11-01 16:45:11 -0400

    Barrido de cabecera

--- resources/views/pages/RRHH/pruebas/edit.blade.php
+++ resources/views/pages/RRHH/pruebas/edit.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.model')
 
-@section('title')
-    pruebas
-@endsection
+@section('title', 'Modificar prueba')
 
 @section('content')
-<!-- Modal Guardar -->
-    @if (session('Error'))
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title text-danger" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <h4 class="h6"> Prueba no registrada </h4>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
-              </div>
-            </div>
+  <!-- Modal Guardar -->
+  @if(session('Error'))
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-danger" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="h6"> Prueba no registrada </h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
           </div>
         </div>
-    @endif
+      </div>
+    </div>
+  @endif
 
     <h1 class="h5 text-info">
         <i class="fas fa-edit"></i>
