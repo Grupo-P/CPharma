3f0b1255920ff4a10fa17b6ea29ede65b9137315
mhenriquezm <manuelhm1993@gmail.com>
2019-11-01 16:17:24 -0400

    Inicio de barrida de entrevista

--- resources/views/pages/RRHH/entrevistas/edit.blade.php
+++ resources/views/pages/RRHH/entrevistas/edit.blade.php
@@ -1,37 +1,34 @@
 @extends('layouts.model')
 
-@section('title')
-  Entrevistas
-@endsection
+@section('title', 'Modificar entrevista')
 
-<!-- Modal Guardar -->
-@if(session('Error'))
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title text-danger" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <h4 class="h6">La entrevista no fue almacenada</h4>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+@section('content')
+  <!-- Modal Guardar -->
+  @if(session('Error'))
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-danger" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>{{ session('Error') }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="h6">La entrevista no fue almacenada</h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-@endif
+  @endif
 
-@section('content')
   <h1 class="h5 text-info">
     <i class="fas fa-edit"></i>
     Modificar entrevista
   </h1>
-
   <hr class="row align-items-start col-12">
 
   <form action="/entrevistas/" method="POST" style="display: inline;">  
