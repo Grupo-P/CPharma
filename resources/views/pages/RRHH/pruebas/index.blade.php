05725f366667daa4f4e82e8b0e6db2ca0e6845ee
mhenriquezm <manuelhm1993@gmail.com>
2019-11-01 16:32:30 -0400

    Identado de cabecera

--- resources/views/pages/RRHH/pruebas/index.blade.php
+++ resources/views/pages/RRHH/pruebas/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.model')
 
-@section('title')
-  Pruebas
-@endsection
+@section('title', 'Pruebas')
 
 @section('content')
   <!-- Modal Guardar -->
@@ -19,7 +17,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6"> Prueba almacenada con exito</h4>
+            <h4 class="h6">Prueba almacenada con exito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -43,7 +41,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6"> Prueba modificada con exito</h4>
+            <h4 class="h6">Prueba modificada con exito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -67,7 +65,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6">Prueba Reincorporada con Éxito </h4>
+            <h4 class="h6">Prueba Reincorporada con Éxito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -89,7 +87,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6">Prueba Desincorporada con Éxito </h4>
+            <h4 class="h6">Prueba Desincorporada con Éxito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -102,7 +100,6 @@
   <h1 class="h5 text-info">
     <i class="fas fa-tasks"></i>&nbsp;Pruebas
   </h1>
-
   <hr class="row align-items-start col-12">
 
   <table style="width:100%;">
@@ -127,41 +124,39 @@
   </table>
 
   <br/>
- <table class="table table-striped table-borderless col-12 sortable" id="myTable">
-      <thead class="thead-dark">
-         <tr>
-          <th scope="col" class="CP-sticky">#</th>
-          <th scope="col" class="CP-sticky">Tipo</th>
-          <th scope="col" class="CP-sticky">Nombre</th>
-          <th scope="col" class="CP-sticky">Estatus</th>
-          <th scope="col" class="CP-sticky">Acciones</th>
+
+  <table class="table table-striped table-borderless col-12 sortable" id="myTable">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col" class="CP-sticky">#</th>
+        <th scope="col" class="CP-sticky">Tipo</th>
+        <th scope="col" class="CP-sticky">Nombre</th>
+        <th scope="col" class="CP-sticky">Estatus</th>
+        <th scope="col" class="CP-sticky">Acciones</th>
       </tr>
-      </thead>
+    </thead>
 
-<tbody>
+    <tbody>
     @foreach($pruebas as $prueba)
-        <tr>
-          <th>{{$prueba->id}}</th>
-          <td>{{$prueba->tipo_prueba}}</td>
-          <td>{{$prueba->nombre_prueba}}</td>
-          <td>{{$prueba->estatus}}</td>
-        <!-- Inicio Validacion de ROLES -->
-          <td style="width:140px;">
+      <tr>
+        <th>{{$prueba->id}}</th>
+        <td>{{$prueba->tipo_prueba}}</td>
+        <td>{{$prueba->nombre_prueba}}</td>
+        <td>{{$prueba->estatus}}</td>
         
+        <!-- Inicio Validacion de ROLES -->
+        <td style="width:140px;">
         <?php
-        if(Auth::user()->role == 'MASTER' || Auth::user()->role == 'DEVELOPER'){
+          if(Auth::user()->role == 'MASTER' || Auth::user()->role == 'DEVELOPER') {
+            if($prueba->estatus == 'ACTIVO') {
         ?>
+          <a href="/pruebas/{{$prueba->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+            <i class="far fa-eye"></i>
+          </a>
 
-          <?php
-          if($prueba->estatus == 'ACTIVO'){
-          ?>
-            <a href="/pruebas/{{$prueba->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
-                  <i class="far fa-eye"></i>                
-                </a>
-
-                <a href="/pruebas/{{$prueba->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
-                  <i class="fas fa-edit"></i>               
-                </a>
+          <a href="/pruebas/{{$prueba->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+            <i class="fas fa-edit"></i>
+          </a>
                     
                 <form action="/pruebas/{{$prueba->id}}" method="POST" style="display: inline;">
                 @method('DELETE')
