9f8b889b0011ad199b77cd0324ec7c48b37eee97
mhenriquezm <manuelhm1993@gmail.com>
2019-11-06 15:51:50 -0400

    Tabla personalizada

--- app/RH_Candidato_Examen.php
+++ app/RH_Candidato_Examen.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RH_Candidato_Examen extends Model {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_candidatos_examenes';
 }
