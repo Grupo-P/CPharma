e8948f02dce79d0094926bfb93e5c5e9d32c7bde
mhenriquezm <manuelhm1993@gmail.com>
2019-11-06 11:40:27 -0400

    Tabla personalizada

--- app/RH_EmpresaReferencia.php
+++ app/RH_EmpresaReferencia.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RH_EmpresaReferencia extends Model {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_empresa_referencias';
 }
