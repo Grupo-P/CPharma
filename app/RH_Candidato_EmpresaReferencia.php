3577d84174afa9bc2ed6ed04625335299016d4b4
mhenriquezm <manuelhm1993@gmail.com>
2019-11-07 17:23:09 -0400

    Intermedia candidatos empresas

---
+++ app/RH_Candidato_EmpresaReferencia.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace compras;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RH_Candidato_EmpresaReferencia extends Model {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rhi_candidatos_empresaref';
+}
---
+++ database/migrations/2019_11_07_171733_create_rh_candidatos_empresaref_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRhCandidatosEmpresarefTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('rhi_candidatos_empresaref', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('rh_candidatos_id');
+            $table->unsignedInteger('rh_empresaref_id');
+            $table->string('user');
+            $table->timestamps();
+
+            $table->foreign('rh_candidatos_id')
+            ->references('id')
+            ->on('rh_candidatos')
+            ->onDelete('cascade');
+
+            $table->foreign('rh_empresaref_id')
+            ->references('id')
+            ->on('rh_empresaref')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('rhi_candidatos_empresaref');
+    }
+}
