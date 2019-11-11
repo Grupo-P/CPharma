35333b592a7f835fb6bfdebb52816b40f2424e9b
covacode <covascode@gmail.com>
2019-11-11 13:31:30 -0400

    Update reporte14.blade.php

--- resources/views/pages/reporte/reporte14.blade.php
+++ resources/views/pages/reporte/reporte14.blade.php
@@ -101,7 +101,7 @@
     DESAROLLADO POR: SERGIO COVA
   */
   function R14_Productos_EnCaida($SedeConnection) {
-
+    $conn = FG_Conectar_Smartpharma('FTN');
     $connCPharma = FG_Conectar_CPharma();
 
   /*INCIO PARA CALCULOS CON DIAS EN CERO*/
@@ -191,7 +191,8 @@
               <th scope="col" class="CP-sticky">Dia 1</td>
               <th scope="col" class="CP-sticky">Unidades Vendidas</td>
               <th scope="col" class="CP-sticky">Dias Restantes</td> 
-              <th scope="col" class="CP-sticky bg-danger text-white">Dias restantes (Real)</th>                  
+              <th scope="col" class="CP-sticky bg-danger text-white">Dias restantes (Real)</th>
+              <th scope="col" class="CP-sticky">Acciones</th>                  
           </tr>
         </thead>
         <tbody>
@@ -216,6 +217,11 @@
       $Dia1 = $row['Dia1'];
       $UnidadesVendidas = $row['UnidadesVendidas'];
       $DiasRestantes = $row['DiasRestantes'];
+
+      $sql1 = QPC_Detalle_Articulo($IdArticulo);
+      $result1 = sqlsrv_query($conn,$sql1);
+      $row1 = sqlsrv_fetch_array($result1,SQLSRV_FETCH_ASSOC);
+      $CodigoBarra = $row1["CodigoBarra"];
 
       $sql2 = MySQL_Cuenta_Veces_Dias_Cero($IdArticulo,$FInicial,$FFinal);
       $result2 = mysqli_query($connCPharma,$sql2);
@@ -257,6 +263,45 @@
 
       echo '<td align="center">'.round($DiasRestantes,2).'</td>';
       echo '<td align="center" class="bg-danger text-white">'.round($DiasRestantesQuiebre,2).'</td>';
+
+      /*BOTON PARA AGREGAR A LA ORDEN DE COMPRA*/
+
+      $sqlDetalleOrden = "SELECT COUNT(*) AS CuentaOr FROM orden_compra_detalles WHERE id_articulo = '$IdArticulo'";
+      $resultDetalleOrden = mysqli_query($connCPharma,$sqlDetalleOrden);
+      $rowDetalleOrden = $resultDetalleOrden->fetch_assoc();
+      $cuentaArticuloOrden = $rowDetalleOrden['CuentaOr'];
+
+      echo'
+      <td style="width:140px;">
+        <form action="/ordenCompraDetalle/create" method="PRE" style="display: block; width:100%;" target="_blank">
+      ';
+      echo'<input type="hidden" name="id_articulo" value="'.$IdArticulo.'">';
+      echo'<input type="hidden" name="codigo_articulo" value="'.$CodigoArticulo.'">';
+      echo'<input type="hidden" name="codigo_barra" value="'.$CodigoBarra.'">';
+      echo'<input type="hidden" name="descripcion" value="'.$Descripcion.'">';
+      echo'<input type="hidden" name="existencia_rpt" value="'.$Existencia.'">';
+      echo'<input type="hidden" name="dias_restantes_rpt" value="'.$DiasRestantesQuiebre.'">';
+      echo'<input type="hidden" name="origen_rpt" value="Productos en Caida">';
+      echo'<input type="hidden" name="rango_rpt" value="Del: '.$FInicialImp.' Al: '.$FFinalImp.'">';
+      echo'
+          <button type="submit" name="Reporte" role="button" class="btn btn-outline-success btn-sm" value="SI" style="width:100%;">Agregar</button>
+        </form>
+      ';
+      if( $cuentaArticuloOrden!=0 ) {
+        echo'
+          <br/> 
+          <form action="/ordenCompraDetalle/0" method="PRE" style="display: block; width:100%;" target="_blank">';
+        echo'<input type="hidden" name="id_articulo" value="'.$IdArticulo.'">';
+        echo'
+          <button type="submit" role="button" class="btn btn-outline-danger btn-sm" style="width:100%;">En Transito</button>
+        </form>
+      ';
+      }
+      echo'
+      </td>
+      ';
+      /*BOTON PARA AGREGAR A LA ORDEN DE COMPRA*/
+
       echo '</tr>';
     $contador++;
     }
@@ -264,5 +309,161 @@
         </tbody>
     </table>';
     mysqli_close($connCPharma);
+  }
+?>
+
+<?php
+/**********************************************************************************/
+  /*
+    TITULO: QPC_Detalle_Articulo
+    FUNCION: Query que genera el detalle del articulo solicitado
+    RETORNO: Detalle del articulo
+    DESAROLLADO POR: SERGIO COVA
+  */
+  function QPC_Detalle_Articulo($IdArticulo) {
+    $sql = " 
+      SELECT
+    --Id Articulo
+      InvArticulo.Id AS IdArticulo,
+    --Codigo Interno
+      InvArticulo.CodigoArticulo AS CodigoInterno,
+    --Codigo de Barra
+      (SELECT CodigoBarra
+      FROM InvCodigoBarra 
+      WHERE InvCodigoBarra.InvArticuloId = InvArticulo.Id
+      AND InvCodigoBarra.EsPrincipal = 1) AS CodigoBarra,
+    --Descripcion
+      InvArticulo.Descripcion,
+    --Impuesto (1 SI aplica impuesto, 0 NO aplica impuesto)
+      (ISNULL(InvArticulo.FinConceptoImptoIdCompra,CAST(0 AS INT))) AS Impuesto,
+    --Utilidad (Utilidad del articulo, Utilidad es 1.00 NO considerar la utilidad para el calculo de precio)
+      ROUND(CAST(1-((ISNULL(ROUND(CAST((SELECT VenCondicionVenta.PorcentajeUtilidad
+          FROM VenCondicionVenta 
+          WHERE VenCondicionVenta.Id = (
+            SELECT VenCondicionVenta_VenCondicionVentaArticulo.Id
+            FROM VenCondicionVenta_VenCondicionVentaArticulo 
+            WHERE VenCondicionVenta_VenCondicionVentaArticulo.InvArticuloId = InvArticulo.Id)) AS DECIMAL(38,4)),2,0),CAST(0 AS INT)))/100)AS DECIMAL(38,2)),2,0) AS Utilidad,
+    --Precio Troquel Almacen 1
+      (ROUND(CAST((SELECT TOP 1
+      InvLote.M_PrecioTroquelado
+      FROM InvLoteAlmacen
+      INNER JOIN InvLote ON InvLote.Id = InvLoteAlmacen.InvLoteId
+      WHERE(InvLoteAlmacen.InvAlmacenId = '1')
+      AND (InvLoteAlmacen.InvArticuloId = InvArticulo.Id)
+      AND (InvLoteAlmacen.Existencia>0)
+      ORDER BY invlote.M_PrecioTroquelado DESC)AS DECIMAL(38,2)),2,0)) AS TroquelAlmacen1,
+    --Precio Troquel Almacen 2
+      (ROUND(CAST((SELECT TOP 1
+      InvLote.M_PrecioTroquelado
+      FROM InvLoteAlmacen
+      INNER JOIN InvLote ON InvLote.Id = InvLoteAlmacen.InvLoteId
+      WHERE(InvLoteAlmacen.InvAlmacenId = '2')
+      AND (InvLoteAlmacen.InvArticuloId = InvArticulo.Id)
+      AND (InvLoteAlmacen.Existencia>0)
+      ORDER BY invlote.M_PrecioTroquelado DESC)AS DECIMAL(38,2)),2,0)) AS TroquelAlmacen2,
+    --Precio Compra Bruto
+      (ROUND(CAST((SELECT TOP 1
+      InvLote.M_PrecioCompraBruto
+      FROM InvLoteAlmacen
+      INNER JOIN InvLote ON InvLote.Id = InvLoteAlmacen.InvLoteId
+      WHERE (InvLoteAlmacen.InvArticuloId = InvArticulo.Id)
+      AND (InvLoteAlmacen.Existencia>0)
+      ORDER BY invlote.M_PrecioCompraBruto DESC)AS DECIMAL(38,2)),2,0)) AS PrecioCompraBruto,
+    --Existencia (Segun el almacen del filtro)
+      (ROUND(CAST((SELECT SUM (InvLoteAlmacen.Existencia) As Existencia
+                  FROM InvLoteAlmacen
+                  WHERE(InvLoteAlmacen.InvAlmacenId = 1 OR InvLoteAlmacen.InvAlmacenId = 2)
+                  AND (InvLoteAlmacen.InvArticuloId = InvArticulo.Id)) AS DECIMAL(38,0)),2,0))  AS Existencia,
+    --Dolarizado (0 NO es dolarizado, Id Articulo SI es dolarizado)
+      (ISNULL((SELECT
+      InvArticuloAtributo.InvArticuloId
+      FROM InvArticuloAtributo 
+      WHERE InvArticuloAtributo.InvAtributoId = 
+        (SELECT InvAtributo.Id
+        FROM InvAtributo 
+        WHERE 
+        InvAtributo.Descripcion = 'Dolarizados'
+        OR  InvAtributo.Descripcion = 'Giordany'
+        OR  InvAtributo.Descripcion = 'giordany') 
+      AND InvArticuloAtributo.InvArticuloId = InvArticulo.Id),CAST(0 AS INT))) AS Dolarizado,
+    --Tipo Producto (0 Miscelaneos, Id Articulo Medicinas)
+      (ISNULL((SELECT
+      InvArticuloAtributo.InvArticuloId 
+      FROM InvArticuloAtributo 
+      WHERE InvArticuloAtributo.InvAtributoId = 
+        (SELECT InvAtributo.Id
+        FROM InvAtributo 
+        WHERE 
+        InvAtributo.Descripcion = 'Medicina') 
+      AND InvArticuloAtributo.InvArticuloId = InvArticulo.Id),CAST(0 AS INT))) AS Tipo,
+    --Articulo Estrella (0 NO es Articulo Estrella , Id SI es Articulo Estrella)
+      (ISNULL((SELECT
+      InvArticuloAtributo.InvArticuloId 
+      FROM InvArticuloAtributo 
+      WHERE InvArticuloAtributo.InvAtributoId = 
+        (SELECT InvAtributo.Id
+        FROM InvAtributo 
+        WHERE 
+        InvAtributo.Descripcion = 'Articulo Estrella') 
+      AND InvArticuloAtributo.InvArticuloId = InvArticulo.Id),CAST(0 AS INT))) AS ArticuloEstrella,
+    -- Ultima Venta (Fecha)
+      (SELECT TOP 1
+      CONVERT(DATE,VenFactura.FechaDocumento)
+      FROM VenFactura
+      INNER JOIN VenFacturaDetalle ON VenFacturaDetalle.VenFacturaId = VenFactura.Id
+      WHERE VenFacturaDetalle.InvArticuloId = InvArticulo.Id
+      ORDER BY FechaDocumento DESC) AS UltimaVenta,
+    --Tiempo sin Venta (En dias)
+      (SELECT TOP 1
+      DATEDIFF(DAY,CONVERT(DATE,VenFactura.FechaDocumento),GETDATE())
+      FROM VenFactura
+      INNER JOIN VenFacturaDetalle ON VenFacturaDetalle.VenFacturaId = VenFactura.Id
+      WHERE VenFacturaDetalle.InvArticuloId = InvArticulo.Id
+      ORDER BY FechaDocumento DESC) AS TiempoSinVenta,
+    --Ultimo Lote (Fecha)
+      (SELECT TOP 1
+      CONVERT(DATE,InvLote.FechaEntrada) AS UltimoLote
+      FROM InvLote
+      WHERE InvLote.InvArticuloId  = InvArticulo.Id
+      ORDER BY UltimoLote DESC) AS UltimoLote,
+    --Tiempo Tienda (En dias)
+      (SELECT TOP 1 
+      DATEDIFF(DAY,CONVERT(DATE,InvLote.FechaEntrada),GETDATE())
+      FROM InvLoteAlmacen 
+      INNER JOIN invlote on invlote.id = InvLoteAlmacen.InvLoteId
+      WHERE InvLotealmacen.InvArticuloId = InvArticulo.Id
+      ORDER BY InvLote.Auditoria_FechaCreacion DESC) AS TiempoTienda,
+    -- Ultimo Proveedor (Id Proveedor)
+      (SELECT TOP 1
+      ComProveedor.Id
+      FROM ComFacturaDetalle
+      INNER JOIN ComFactura ON ComFactura.Id = ComFacturaDetalle.ComFacturaId
+      INNER JOIN ComProveedor ON ComProveedor.Id = ComFactura.ComProveedorId
+      INNER JOIN GenPersona ON GenPersona.Id = ComProveedor.GenPersonaId
+      WHERE ComFacturaDetalle.InvArticuloId = InvArticulo.Id
+      ORDER BY ComFactura.FechaDocumento DESC) AS  UltimoProveedorID,
+    -- Ultimo Proveedor (Nombre Proveedor)
+      (SELECT TOP 1
+      GenPersona.Nombre
+      FROM ComFacturaDetalle
+      INNER JOIN ComFactura ON ComFactura.Id = ComFacturaDetalle.ComFacturaId
+      INNER JOIN ComProveedor ON ComProveedor.Id = ComFactura.ComProveedorId
+      INNER JOIN GenPersona ON GenPersona.Id = ComProveedor.GenPersonaId
+      WHERE ComFacturaDetalle.InvArticuloId = InvArticulo.Id
+      ORDER BY ComFactura.FechaDocumento DESC) AS  UltimoProveedorNombre
+    --Tabla principal
+      FROM InvArticulo
+    --Joins
+      LEFT JOIN InvLoteAlmacen ON InvLoteAlmacen.InvArticuloId = InvArticulo.Id
+      LEFT JOIN InvArticuloAtributo ON InvArticuloAtributo.InvArticuloId = InvArticulo.Id
+      LEFT JOIN InvAtributo ON InvAtributo.Id = InvArticuloAtributo.InvAtributoId 
+    --Condicionales
+      WHERE InvArticulo.Id = '$IdArticulo'
+    --Agrupamientos
+      GROUP BY InvArticulo.Id, InvArticulo.CodigoArticulo, InvArticulo.Descripcion, InvArticulo.FinConceptoImptoIdCompra
+    --Ordanamiento
+      ORDER BY InvArticulo.Id ASC 
+    ";
+    return $sql;
   }
 ?>
