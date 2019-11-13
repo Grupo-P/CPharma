5ce369a6d81ddb09fd9fd8f2c17ba1a757d10233
pvielmarubmary <56359288+pvielmarubmary@users.noreply.github.com>
2019-11-13 09:28:46 -0400

    Migracion definida
    Se crearon los campos para la base de datos y el metodo down para reversar la accion del up

--- database/migrations/2019_11_13_091545_add_telefonos_to_rh_laboratorio_table.php
+++ database/migrations/2019_11_13_091545_add_telefonos_to_rh_laboratorio_table.php
@@ -4,17 +4,16 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTelefonosToRhLaboratorioTable extends Migration
-{
+class AddTelefonosToRhLaboratorioTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('rh_laboratorio', function (Blueprint $table) {
-            //
+            $table->string('telefono_fijo')->after('direccion');
+            $table->string('telefono_celular')->after('telefono_fijo');
         });
     }
 
@@ -23,10 +22,10 @@ class AddTelefonosToRhLaboratorioTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('rh_laboratorio', function (Blueprint $table) {
-            //
+            $table->dropColumn('telefono_fijo');
+            $table->dropColumn('telefono_celular');
         });
     }
 }
