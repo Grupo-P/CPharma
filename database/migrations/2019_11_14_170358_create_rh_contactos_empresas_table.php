1bb3735d80255933d797f6b2182180757bedf17f
mhenriquezm <manuelhm1993@gmail.com>
2019-11-14 17:05:32 -0400

    Eliminar relacion temporalmente

--- database/migrations/2019_11_14_170358_create_rh_contactos_empresas_table.php
+++ database/migrations/2019_11_14_170358_create_rh_contactos_empresas_table.php
@@ -13,7 +13,6 @@ class CreateRhContactosEmpresasTable extends Migration {
     public function up() {
         Schema::create('rh_contactos_empresas', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('rh_empresaRef_id');
             $table->string('nombre');
             $table->string('apellido');
             $table->string('telefono');
@@ -22,11 +21,6 @@ class CreateRhContactosEmpresasTable extends Migration {
             $table->string('user');
             $table->string('estatus');
             $table->timestamps();
-
-            $table->foreign('rh_empresaRef_id')
-            ->references('id')
-            ->on('rh_empresaRef')
-            ->onDelete('cascade');
         });
     }
 
