04b6af90648e2daeff7695103f167f76e2894493
mhenriquezm <manuelhm1993@gmail.com>
2019-11-14 11:16:13 -0400

    Tabla personalizada

--- app/RHI_Examen_Laboratorio.php
+++ app/RHI_Examen_Laboratorio.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RHI_Examen_Laboratorio extends Model {
-    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_examenes_lab';
 }
