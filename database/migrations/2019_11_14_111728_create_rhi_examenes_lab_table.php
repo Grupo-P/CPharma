4f665973658d3e60b198635143309a002a506043
mhenriquezm <manuelhm1993@gmail.com>
2019-11-14 11:25:23 -0400

    Relaciones muchos a muchos

--- database/migrations/2019_11_14_111728_create_rhi_examenes_lab_table.php
+++ database/migrations/2019_11_14_111728_create_rhi_examenes_lab_table.php
@@ -13,7 +13,20 @@ class CreateRhiExamenesLabTable extends Migration {
     public function up() {
         Schema::create('rhi_examenes_lab', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('rh_examenes_id');
+            $table->unsignedInteger('rh_laboratorio_id');
+            $table->string('user');
             $table->timestamps();
+
+            $table->foreign('rh_examenes_id')
+            ->references('id')
+            ->on('rh_examenes')
+            ->onDelete('cascade');
+
+            $table->foreign('rh_laboratorio_id')
+            ->references('id')
+            ->on('rh_laboratorio')
+            ->onDelete('cascade');
         });
     }
 
