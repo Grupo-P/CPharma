d567143ac00531f99cd6357b40c5e6ff769f5ac5
mhenriquezm <manuelhm1993@gmail.com>
2019-11-15 12:21:56 -0400

    Plantilla adaptada

--- resources/views/pages/RRHH/contactos/show.blade.php
+++ resources/views/pages/RRHH/contactos/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.model')
 
-@section('title', 'Mostrar candidato')
+@section('title', 'Mostrar contacto')
 
 @section('content')
   <h1 class="h5 text-info">
     <i class="far fa-eye"></i>
-    Detalles del candidato
+    Detalles del contacto
   </h1>
   <hr class="row align-items-start col-12">
 
-  <form action="/candidatos/" method="POST" style="display: inline;">
+  <form action="/contactos/" method="POST" style="display: inline;">
     @csrf
     <button type="submit" name="Regresar" role="button" class="btn btn-outline-info btn-sm"data-placement="top">
       <i class="fa fa-reply">&nbsp;Regresar</i>
@@ -29,82 +29,82 @@
     <tbody>
       <tr>
         <th scope="row">Nombres</th>
-        <td>{{$candidatos->nombres}}</td>
+        <td>{{$contactos->nombres}}</td>
       </tr>
 
       <tr>
         <th scope="row">Apellidos</th>
-        <td>{{$candidatos->apellidos}}</td>
+        <td>{{$contactos->apellidos}}</td>
       </tr>
 
       <tr>
         <th scope="row">Cédula</th>
-        <td>{{$candidatos->cedula}}</td>
+        <td>{{$contactos->cedula}}</td>
       </tr>
 
       <tr>
         <th scope="row">Teléfono celular</th>
-        <td>{{$candidatos->telefono_celular}}</td>
+        <td>{{$contactos->telefono_celular}}</td>
       </tr>
 
       <tr>
         <th scope="row">Teléfono de habitación</th>
-        <td>{{$candidatos->telefono_habitacion}}</td>
+        <td>{{$contactos->telefono_habitacion}}</td>
       </tr>
 
       <tr>
         <th scope="row">Correo</th>
-        <td>{{$candidatos->correo}}</td>
+        <td>{{$contactos->correo}}</td>
       </tr>
 
       <tr>
         <th scope="row">Como nos contactó</th>
-        <td>{{$candidatos->como_nos_contacto}}</td>
+        <td>{{$contactos->como_nos_contacto}}</td>
       </tr>
 
       <tr>
         <th scope="row">Tipo de relación</th>
-        <td>{{$candidatos->tipo_relacion}}</td>
+        <td>{{$contactos->tipo_relacion}}</td>
       </tr>
 
       <tr>
         <th scope="row">Relaciones laborales</th>
-        <td>{{$candidatos->relaciones_laborales}}</td>
+        <td>{{$contactos->relaciones_laborales}}</td>
       </tr>
 
       <tr>
         <th scope="row">Dirección</th>
-        <td>{{$candidatos->direccion}}</td>
+        <td>{{$contactos->direccion}}</td>
       </tr>
 
       <tr>
         <th scope="row">Experiencia laboral</th>
-        <td>{{$candidatos->experiencia_laboral}}</td>
+        <td>{{$contactos->experiencia_laboral}}</td>
       </tr>
 
       <tr>
         <th scope="row">Observaciones</th>
-        <td>{{$candidatos->observaciones}}</td>
+        <td>{{$contactos->observaciones}}</td>
       </tr>
 
       <tr>
         <th scope="row">Estatus</th>
-        <td>{{$candidatos->estatus}}</td>
+        <td>{{$contactos->estatus}}</td>
       </tr>
 
       <tr>
         <th scope="row">Creado</th>
-        <td>{{$candidatos->created_at}}</td>
+        <td>{{$contactos->created_at}}</td>
       </tr>
 
       <tr>
         <th scope="row">Ultima Actualización</th>
-        <td>{{$candidatos->updated_at}}</td>
+        <td>{{$contactos->updated_at}}</td>
       </tr>
 
       <tr>
         <th scope="row">Actualizado por</th>
-        <td>{{$candidatos->user}}</td>
+        <td>{{$contactos->user}}</td>
       </tr>
     </tbody>
   </table>
