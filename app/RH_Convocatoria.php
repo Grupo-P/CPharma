4bc88110c55880da3c08f0c3b82b727b99a99b9c
pvielmarubmary <56359288+pvielmarubmary@users.noreply.github.com>
2019-11-19 11:19:09 -0400

    Datos fillable creados

--- app/RH_Convocatoria.php
+++ app/RH_Convocatoria.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class RH_Convocatoria extends Model
 {
-    //
+   /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_convocatoria';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'fecha', 'lugar', 'cargo_reclutar', 'user'
+    ];
+ 
 }
