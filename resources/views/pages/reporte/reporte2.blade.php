9d6e8edf5e67cf945e98062c77ccf3dfc97edfd8
covacode <covascode@gmail.com>
2019-11-26 15:13:07 -0400

    Update reporte2.blade.php
    se actualizo el reporte 2, barrido final

--- resources/views/pages/reporte/reporte2.blade.php
+++ resources/views/pages/reporte/reporte2.blade.php
@@ -188,14 +188,14 @@
     $PrecioCompraBrutoAlmacen2,$PrecioCompraBruto,$IsIVA,$CondicionExistencia);
 
     echo '
-    <div class="input-group md-form form-sm form-1 pl-0">
+    <div class="input-group md-form form-sm form-1 pl-0 CP-stickyBar">
       <div class="input-group-prepend">
         <span class="input-group-text purple lighten-3" id="basic-text1">
           <i class="fas fa-search text-white"
             aria-hidden="true"></i>
         </span>
       </div>
-      <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()">
+      <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()" autofocus="autofocus">
     </div>
     <br/>
 
