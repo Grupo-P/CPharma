a93c58ad4cc3c25efe36ff42e2b9bb3373d85102
covacode <covascode@gmail.com>
2019-11-28 19:37:05 -0400

    Update

--- resources/views/pages/caja/cuadreDivisa.blade.php
+++ resources/views/pages/caja/cuadreDivisa.blade.php
@@ -435,7 +435,7 @@
     include(app_path().'\functions\config.php');
     include(app_path().'\functions\querys.php');
     include(app_path().'\functions\funciones.php');
-    include(app_path().'\functions\reportes.php');
+    //include(app_path().'\functions\reportes.php');
 
     /*
         TITULO: ValidarFecha
