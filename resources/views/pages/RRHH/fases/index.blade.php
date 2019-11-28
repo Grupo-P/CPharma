143b824a33995e36eea8988150a38ba4eb536c69
mhenriquezm <manuelhm1993@gmail.com>
2019-11-28 11:35:18 -0400

    Variable empaquetada cambiada
    Se cambio la variable candidatos por fases y se elimino un modal innecesario

--- resources/views/pages/RRHH/fases/index.blade.php
+++ resources/views/pages/RRHH/fases/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.model')
 
-@section('title', 'Candidatos')
+@section('title', 'Fases')
 
 @section('scriptsHead')
   <style>
@@ -24,29 +24,6 @@
           </div>
           <div class="modal-body">
             <h4 class="h6">Candidato almacenado con éxito</h4>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  @endif
-
-  @if(session('Saved1'))
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-info" id="exampleModalCenterTitle">
-              <i class="fas fa-info text-info"></i>{{ session('Saved1') }}
-            </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <h4 class="h6">La entrevista fue almacenada con éxito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -128,14 +105,14 @@
   @endif
 
   <h1 class="h5 text-info">
-    <i class="fas fa-user-check"></i>&nbsp;Candidatos
+    <i class="fas fa-user-check"></i>&nbsp;Fases
   </h1>
   <hr class="row align-items-start col-12">
 
   <table style="width:100%;">
     <tr>
       <td style="width:10%;" align="center">
-        <a href="{{ url('/candidatos/create') }}" role="button" class="btn btn-outline-info btn-sm" style="display: inline; text-align: left;">
+        <a href="{{ url('/fases/create') }}" role="button" class="btn btn-outline-info btn-sm" style="display: inline; text-align: left;">
           <i class="fa fa-plus"></i>&nbsp;Agregar
         </a>
       </td>
@@ -170,7 +147,7 @@
     </thead>
 
     <tbody>
-      @foreach($candidatos as $candidato)
+      @foreach($fases as $candidato)
         <tr>
           <th>{{$candidato->id}}</th>
           <td>{{$candidato->nombres}}</td>
@@ -200,15 +177,15 @@
 
               if($candidato->estatus != 'RECHAZADO') {
           ?>
-            <a href="/candidatos/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+            <a href="/fases/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
               <i class="far fa-eye"></i>
             </a>
 
-            <a href="/candidatos/{{$candidato->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+            <a href="/fases/{{$candidato->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
               <i class="fas fa-edit"></i>
             </a>
 
-            <form action="/candidatos/{{$candidato->id}}" method="POST" style="display: inline;">
+            <form action="/fases/{{$candidato->id}}" method="POST" style="display: inline;">
               @method('DELETE')
               @csrf
               <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Desincorporar">
@@ -219,7 +196,7 @@
           <?php
             } else if($candidato->estatus == 'RECHAZADO') {
           ?>
-            <form action="/candidatos/{{$candidato->id}}" method="POST" style="display: inline;">
+            <form action="/fases/{{$candidato->id}}" method="POST" style="display: inline;">
               @method('DELETE')
               @csrf
               <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Reincorporar">
@@ -230,17 +207,17 @@
             }
           } else if(Auth::user()->role == 'ANALISTA') {
           ?>
-            <a href="/candidatos/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+            <a href="/fases/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
               <i class="far fa-eye"></i>
             </a>
 
-            <a href="/candidatos/{{$candidato->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+            <a href="/fases/{{$candidato->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
               <i class="fas fa-edit"></i>
             </a>
           <?php
             } else if(Auth::user()->role == 'USUARIO') {
           ?>
-            <a href="/candidatos/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+            <a href="/fases/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
               <i class="far fa-eye"></i>
             </a>
           <?php
