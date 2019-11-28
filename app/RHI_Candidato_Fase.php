f6272d814535f1d516e1c9b9e3f858189655e544
mhenriquezm <manuelhm1993@gmail.com>
2019-11-28 15:24:33 -0400

    Tabla personalizada

--- app/RHI_Candidato_Fase.php
+++ app/RHI_Candidato_Fase.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RHI_Candidato_Fase extends Model {
-    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rhi_candidatos_fases';
 }
