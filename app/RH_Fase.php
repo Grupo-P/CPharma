0fe515e3c857abc71465317422d34e95778f3c9a
mhenriquezm <manuelhm1993@gmail.com>
2019-11-28 10:50:23 -0400

    Tabla personalizada
    Se le dio un nombre especifico a la tabla asociada al modelo

--- app/RH_Fase.php
+++ app/RH_Fase.php
@@ -4,7 +4,11 @@ namespace compras;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RH_Fase extends Model
-{
-    //
+class RH_Fase extends Model {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_fases';
 }
