f83ad07e0f520197f20ea7aa450a5e693fa75d83
mhenriquezm <manuelhm1993@gmail.com>
2019-11-28 11:02:43 -0400

    Migracion creada
    Se creo la migracion para definir la tabla asosiada a la fase

---
+++ database/migrations/2019_11_28_105848_create_rh_fases_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRhFasesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('rh_fases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('rh_fases');
+    }
+}
