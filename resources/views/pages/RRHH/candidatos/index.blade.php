a0c3516bcc57a08a0583916160b22be7cdba2b55
mhenriquezm <manuelhm1993@gmail.com>
2019-12-02 09:54:05 -0400

    Permiso para eliminar en candidato
    Se agregaron los permisos totales al rol de analista

--- resources/views/pages/RRHH/candidatos/index.blade.php
+++ resources/views/pages/RRHH/candidatos/index.blade.php
@@ -228,7 +228,9 @@
             </form>
           <?php
             }
-          } else if(Auth::user()->role == 'ANALISTA') {
+          } 
+          else if(Auth::user()->role == 'ANALISTA') {
+            if($candidato->estatus != 'RECHAZADO') {
           ?>
             <a href="/candidatos/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
               <i class="far fa-eye"></i>
@@ -237,8 +239,28 @@
             <a href="/candidatos/{{$candidato->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
               <i class="fas fa-edit"></i>
             </a>
+
+            <form action="/candidatos/{{$candidato->id}}" method="POST" style="display: inline;">
+              @method('DELETE')
+              @csrf
+              <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Desincorporar">
+                <i class="fa fa-reply"></i>
+              </button>
+            </form>
           <?php
-            } else if(Auth::user()->role == 'USUARIO') {
+            } else if($candidato->estatus == 'RECHAZADO') {
+          ?>
+            <form action="/candidatos/{{$candidato->id}}" method="POST" style="display: inline;">
+              @method('DELETE')
+              @csrf
+              <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Reincorporar">
+                <i class="fa fa-share"></i>
+              </button>
+            </form>
+
+          <?php
+            }
+          } else if(Auth::user()->role == 'USUARIO') {
           ?>
             <a href="/candidatos/{{$candidato->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
               <i class="far fa-eye"></i>
