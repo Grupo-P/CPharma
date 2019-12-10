218321616eeeb0b0e7560d39a139f0d2a720dea2
mhenriquezm <manuelhm1993@gmail.com>
2019-12-10 08:37:52 -0400

    Tabla personalizada
    Se le dio un nombre especifico a la tabla que vamos a utilizar

--- app/RH_Practica.php
+++ app/RH_Practica.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class RH_Practica extends Model {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rh_practicas';
 }
