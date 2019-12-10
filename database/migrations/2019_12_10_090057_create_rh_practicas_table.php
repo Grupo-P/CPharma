b8143e067b960ffea4e18726032e4ee323c5d281
mhenriquezm <manuelhm1993@gmail.com>
2019-12-10 09:02:38 -0400

    Migracion ejecutada

---
+++ database/migrations/2019_12_10_090057_create_rh_practicas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRhPracticasTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('rh_practicas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('rh_candidatos_id');
+            $table->string('lider')->nullable();
+            $table->string('lugar')->nullable();
+            $table->double('duracion', 8, 2)->nullable();
+            $table->text('observaciones')->nullable();
+            $table->string('user')->nullable();
+            $table->string('estatus')->nullable();
+            $table->timestamps();
+
+            $table->foreign('rh_candidatos_id')
+            ->references('id')
+            ->on('rh_candidatos')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('rh_practicas');
+    }
+}
