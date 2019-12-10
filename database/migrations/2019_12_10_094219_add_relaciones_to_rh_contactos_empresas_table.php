55c6e5418519ac1e0983c1c6509ac6da84ce832d
mhenriquezm <manuelhm1993@gmail.com>
2019-12-10 09:46:10 -0400

    Contactos y empresa relacionadas

---
+++ database/migrations/2019_12_10_094219_add_relaciones_to_rh_contactos_empresas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelacionesToRhContactosEmpresasTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('rh_contactos_empresas', function (Blueprint $table) {
+            $table->unsignedInteger('rh_emprf_id')->after('id');
+
+            $table->foreign('rh_emprf_id')
+            ->references('id')
+            ->on('rh_empresaRef')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('rh_contactos_empresas', function (Blueprint $table) {
+            $table->dropColumn('rh_emprf_id');
+        });
+    }
+}
