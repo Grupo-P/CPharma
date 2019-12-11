39b0ae4bd63221c0765e897276886078d9244536
covacode <covascode@gmail.com>
2019-12-11 15:31:03 -0400

    Update functionConsultaPrecio.php

--- public/assets/functions/functionConsultaPrecio.php
+++ public/assets/functions/functionConsultaPrecio.php
@@ -1,5 +1,9 @@
 <?php 
+	include('C:\xampp\htdocs\CPharma\app\functions\config.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\functions.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\querys_mysql.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\querys_sqlserver.php');
+
   $IdArticulo = $_POST["IdArticulo"];
-  echo'Hola aqui busco de: '.$IdArticulo;
-  //echo''.FG_Precio_Consultor($IdArticulo);
+  echo''.FG_Precio_Consultor($IdArticulo);
 ?>
