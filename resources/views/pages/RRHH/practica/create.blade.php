393b11c1341e60a81f3eda8253432c2d4a26db77
mhenriquezm <manuelhm1993@gmail.com>
2019-12-12 11:24:25 -0400

    Adaptar template
    Se inicio la adaptacion del template a practicas

--- resources/views/pages/RRHH/practica/create.blade.php
+++ resources/views/pages/RRHH/practica/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.model')
 
-@section('title', 'Crear candidato')
+@section('title', 'Crear práctica')
 
 @section('content')
   <!-- Modal Guardar -->
@@ -19,61 +19,7 @@
           </div>
           <div class="modal-body">
             <h4 class="h6">
-              El candidato no fue almacenado
-            </h4>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  @endif
-
-  <!-- Modal Cedula duplicada -->
-  @if(session('Error1'))
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-danger" id="exampleModalCenterTitle">
-              <i class="fas fa-exclamation-triangle text-danger"></i>
-              {{ session('Error') }}
-            </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <h4 class="h6">
-              El candidato no fue almacenado, la cédula ya esta registrada
-            </h4>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  @endif
-
-  <!-- Modal Correo duplicado -->
-  @if(session('Error2'))
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-danger" id="exampleModalCenterTitle">
-              <i class="fas fa-exclamation-triangle text-danger"></i>
-              {{ session('Error') }}
-            </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <h4 class="h6">
-              El candidato no fue almacenado, el correo ya esta registrado
+              La práctica no fue almacenada
             </h4>
           </div>
           <div class="modal-footer">
@@ -85,11 +31,11 @@
   @endif
 
   <h1 class="h5 text-info">
-    <i class="fas fa-plus"></i>&nbsp;Agregar candidato
+    <i class="fas fa-plus"></i>&nbsp;Agregar práctica
   </h1>
   <hr class="row align-items-start col-12">
 
-  <form action="/candidatos/" method="POST" style="display: inline;">  
+  <form action="/practicas/" method="POST" style="display: inline;">  
     @csrf
     <button type="submit" name="Regresar" role="button" class="btn btn-outline-info btn-sm"data-placement="top">
       <i class="fa fa-reply">&nbsp;Regresar</i>
@@ -98,7 +44,7 @@
 
   <br/><br/>
 
-  {!! Form::open(['route' => 'candidatos.store', 'method' => 'POST', 'id' => 'crear_candidato', 'class' => 'form-group']) !!}
+  {!! Form::open(['route' => 'practicas.store', 'method' => 'POST', 'id' => 'crear_candidato', 'class' => 'form-group']) !!}
     <fieldset>
       <table class="table table-borderless table-striped">
         <thead class="thead-dark">
