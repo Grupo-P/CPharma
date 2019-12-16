e1b7738c3b6e9067d601201b4093714549ed44be
mhenriquezm <manuelhm1993@gmail.com>
2019-12-16 12:41:05 -0400

    Relacion entre candiadtos examenes
    Se desvinculo el representante de la empresa debido a que se colocara en la intermedia

--- database/migrations/2019_12_16_123059_add_llave_foranea_to_rh_examenes_table.php
+++ database/migrations/2019_12_16_123059_add_llave_foranea_to_rh_examenes_table.php
@@ -4,17 +4,22 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLlaveForaneaToRhExamenesTable extends Migration
-{
+class AddLlaveForaneaToRhExamenesTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('rh_examenes', function (Blueprint $table) {
-            //
+            $table->dropColumn('representante');
+            $table->unsignedInteger('rh_candidatos_id')->after('id');
+
+            $table->foreign('rh_candidatos_id')
+            ->references('id')
+            ->on('rh_candidatos')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
         });
     }
 
@@ -23,10 +28,11 @@ class AddLlaveForaneaToRhExamenesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('rh_examenes', function (Blueprint $table) {
-            //
+            $table->string('representante')->after('empresa');
+            $table->dropForeign('rh_examenes_rh_candidatos_id_foreign');
+            $table->dropColumn('rh_candidatos_id');
         });
     }
 }
