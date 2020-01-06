de78e479fe779ab17743ec9852fb85fd6bb4f9b5
mhenriquezm <manuelhm1993@gmail.com>
2020-01-06 16:34:51 -0400

    Personalizar la tabla del modelo
    Se dio un nombre personalizado a la tabla correspondiente

--- app/TS_Movimiento.php
+++ app/TS_Movimiento.php
@@ -5,5 +5,10 @@ namespace compras;
 use Illuminate\Database\Eloquent\Model;
 
 class TS_Movimiento extends Model {
-    
+	/**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ts_movimientos';
 }
