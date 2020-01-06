8dc515ebd6e47a175de7fa2de3e63fdcfe8a6811
mhenriquezm <manuelhm1993@gmail.com>
2020-01-06 16:37:02 -0400

    Migracion de movimientos
    Se creo la migracion para la tabla de movimientos

---
+++ database/migrations/2020_01_06_163610_create_ts_movimientos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTsMovimientosTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('ts_movimientos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('ts_movimientos');
+    }
+}
