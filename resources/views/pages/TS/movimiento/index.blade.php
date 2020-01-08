c44cc6dc8f927900c4450a2faa52ec518ebc4943
mhenriquezm <manuelhm1993@gmail.com>
2020-01-08 14:25:07 -0400

    Limpiar modales y cabeceras

--- resources/views/pages/TS/movimiento/index.blade.php
+++ resources/views/pages/TS/movimiento/index.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.model')
 
-@section('title')
-    Empresa
-@endsection
+@section('title', 'Movimientos')
+
 
 @section('content')
 
@@ -18,7 +17,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6">Empresa almacenada con exito</h4>
+            <h4 class="h6">Movimiento almacenado con éxito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -40,7 +39,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <h4 class="h6">Empresa modificada con exito</h4>
+            <h4 class="h6">Movimiento modificado con éxito</h4>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button>
@@ -50,38 +49,16 @@
     </div>
   @endif
 
-  <!-- Modal Eliminar -->
-  @if (session('Deleted'))
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-info" id="exampleModalCenterTitle"><i class="fas fa-info text-info"></i>{{ session('Deleted') }}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <h4 class="h6">Empresa actualizada con exito</h4>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-success" data-dismiss="modal">Aceptar</button> 
-          </div>
-        </div>
-      </div>
-    </div>
-  @endif
-
   <h1 class="h5 text-info">
     <i class="fas fa-industry"></i>
-    Empresa
+    Movimientos
   </h1>
 
   <hr class="row align-items-start col-12">
   <table style="width:100%;" class="CP-stickyBar">
       <tr>
           <td style="width:10%;" align="center">  
-        <a href="{{ url('/empresa/create') }}" role="button" class="btn btn-outline-info btn-sm" 
+        <a href="{{ url('/movimientos/create') }}" role="button" class="btn btn-outline-info btn-sm" 
         style="display: inline; text-align: left;">
         <i class="fa fa-plus"></i>
           Agregar
@@ -112,13 +89,13 @@
         </tr>
       </thead>
       <tbody>
-    @foreach($empresas as $empresa)
+    @foreach($movimientos as $movimiento)
         <tr>
-          <th>{{$empresa->id}}</th>
-          <td>{{$empresa->nombre}}</td>
-          <td>{{$empresa->rif}}</td>
-          <td>{{$empresa->telefono}}</td>         
-          <td>{{$empresa->estatus}}</td>
+          <th>{{$movimiento->id}}</th>
+          <td>{{$movimiento->nombre}}</td>
+          <td>{{$movimiento->rif}}</td>
+          <td>{{$movimiento->telefono}}</td>         
+          <td>{{$movimiento->estatus}}</td>
           
         <!-- Inicio Validacion de ROLES -->
           <td style="width:140px;">
@@ -128,26 +105,26 @@
         ?>
 
           <?php
-          if($empresa->estatus == 'ACTIVO'){
+          if($movimiento->estatus == 'ACTIVO'){
           ?>
-            <a href="/empresa/{{$empresa->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+            <a href="/movimientos/{{$movimiento->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
                   <i class="far fa-eye"></i>                
                 </a>
 
-                <a href="/empresa/{{$empresa->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+                <a href="/movimientos/{{$movimiento->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
                   <i class="fas fa-edit"></i>               
                 </a>
                     
-                <form action="/empresa/{{$empresa->id}}" method="POST" style="display: inline;">
+                <form action="/movimientos/{{$movimiento->id}}" method="POST" style="display: inline;">
                 @method('DELETE')
                 @csrf             
                 <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Desincorporar"><i class="fa fa-reply"></i></button>
             </form>
           <?php
           }
-          else if($empresa->estatus == 'INACTIVO'){
+          else if($movimiento->estatus == 'INACTIVO'){
           ?>    
-              <form action="/empresa/{{$empresa->id}}" method="POST" style="display: inline;">
+              <form action="/movimientos/{{$movimiento->id}}" method="POST" style="display: inline;">
               @method('DELETE')
               @csrf             
               <button type="submit" name="Eliminar" role="button" class="btn btn-outline-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Reincorporar"><i class="fa fa-share"></i></button>
@@ -158,17 +135,17 @@
         <?php 
         } else if(Auth::user()->role == 'SUPERVISOR' || Auth::user()->role == 'ADMINISTRADOR' || Auth::user()->role == 'SUPERVISOR CAJA'){ 
         ?>
-          <a href="/empresa/{{$empresa->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+          <a href="/movimientos/{{$movimiento->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
                 <i class="far fa-eye"></i>                
               </a>
 
-              <a href="/empresa/{{$empresa->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
+              <a href="/movimientos/{{$movimiento->id}}/edit" role="button" class="btn btn-outline-info btn-sm" data-toggle="tooltip" data-placement="top" title="Modificar">
                 <i class="fas fa-edit"></i>
               </a>
         <?php
         } else if(Auth::user()->role == 'USUARIO'){
         ?>
-          <a href="/empresa/{{$empresa->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
+          <a href="/movimientos/{{$movimiento->id}}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Detalle">
                 <i class="far fa-eye"></i>                
               </a>    
         <?php
