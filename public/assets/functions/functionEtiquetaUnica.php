40bfe21f8c900a452df9593565bac3e4638f3bc1
covacode <covascode@gmail.com>
2020-01-09 09:49:49 -0400

    Actualizacion Logica
    ya se escane el producto y se retorna el codigo, se armo la estructura necesaria para la consulta al server

---
+++ public/assets/functions/functionEtiquetaUnica.php
@@ -0,0 +1,11 @@
+<?php 
+	include('C:\xampp\htdocs\CPharma\app\functions\config.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\functions.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\querys_mysql.php');
+  include('C:\xampp\htdocs\CPharma\app\functions\querys_sqlserver.php');
+
+  $IdArticulo = $_POST["IdArticulo"];
+  //$precio = FG_Precio_Consultor($IdArticulo);
+  //FG_Registro_Datos($IdArticulo,$precio);
+  echo''.$IdArticulo;
+?>
--- resources/views/pages/etiqueta/Generar_Etiqueta_Unica.blade.php
+++ resources/views/pages/etiqueta/Generar_Etiqueta_Unica.blade.php
@@ -1,1 +1,250 @@
-Hola
+@extends('layouts.modelUser')
+
+@section('title')
+  Etiqueta
+@endsection
+
+<style>
+	* {
+      box-sizing: border-box;
+    }
+    .autocomplete {
+      position: relative;
+      display: inline-block;
+    }
+    input {
+      border: 1px solid transparent;
+      background-color: #fff;
+      border-radius: 5px;
+      padding: 10px;
+      font-size: 16px;
+    }
+    input[type=text] {
+    	text-align: center;
+    	font-size: 1.6em;
+      background-color: #fff;
+      width: 100%;
+      height: 50px;
+    }
+    input[type=text]:focus {
+		 	outline: 0;
+    }
+    .autocomplete-items {
+      position: absolute;
+      border: 1px solid #d4d4d4;
+      border-bottom: none;
+      border-top: none;
+      z-index: 99;
+      top: 100%;
+      left: 0;
+      right: 0;
+    }
+    .autocomplete-items div {
+      padding: 10px;
+      cursor: pointer;
+      background-color: #fff; 
+      border-bottom: 1px solid #d4d4d4; 
+    }
+    .autocomplete-items div:hover {
+      background-color: #e9e9e9; 
+    }
+    .autocomplete-active {
+      background-color: #17a2b8 !important; 
+      color: #ffffff; 
+    }
+		.center th {
+	    vertical-align: middle;
+	    text-align: center;
+	  }
+	  .aum-icon-lup {
+    	text-align: center;
+    	font-size: 1em;
+    }
+    .aum-icon-cod {
+      text-align: center;
+      font-size: 1.2em;
+    }
+</style>
+
+@section('content')
+
+	<?php 
+	  include(app_path().'\functions\config.php');
+	  include(app_path().'\functions\functions.php');
+	  include(app_path().'\functions\querys_mysql.php');
+	  include(app_path().'\functions\querys_sqlserver.php');
+
+    $SedeConnection = 'FTN';//FG_Mi_Ubicacion();
+    $RutaUrl = FG_Mi_Ubicacion();
+
+	  $CodJson = '';
+    $ArtJson = '';
+
+		$sql1 = RCPQ_Lista_Articulos_CodBarra();
+    $CodJson = FG_Armar_Json($sql1,$SedeConnection);
+
+    $sql1 = RCPQ_Lista_Articulos_Descripcion();
+    $ArtJson = FG_Armar_Json($sql1,$SedeConnection);
+	?>
+    <table class="table table-borderless col-12">
+      <thead class="center">
+        <tr>
+          <th scope="col" colspan="3">
+            <h1 class="text-info">GENERAR ETIQUETA UNICA</h1>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white" style="border: 4px solid #17a2b8;">
+          <td align="center" class="text-success"><h1><i class="fas fa-tag aum-icon-cod"></i></h1></td>
+          <td align="center" style="border: 4px solid #17a2b8;">
+            <input id="inputCodBar" type="text" name="CodBar" autofocus="autofocus">
+          </td>
+          <td align="center" class="text-success"><h1><i class="fas fa-search aum-icon-lup"></i></h1></td>
+        </tr>
+      </tbody>
+    </table>
+    
+    <table class="table table-borderless col-12" id="tablaError">
+      <thead class="center">
+        <th class="bg-white text-danger border border-white">
+          <h3 id="MsnError"></h3></th>
+      </thead>
+    </table>
+
+    <div id="DivEtiquetas"></div>
+
+@endsection
+
+@section('scriptsPie')
+  <script type="text/javascript">
+    const SedeConnectionJs = '<?php echo $RutaUrl;?>'
+  </script>
+
+  <script>
+    /************************************************************************/
+    function dominio(SedeConnectionJs){
+      var dominio = '';
+      switch(SedeConnectionJs) {
+        case 'FTN':
+          dominio = 'http://cpharmaftn.com/';
+          return dominio;
+        break;
+        case 'FLL':
+          dominio = 'http://cpharmafll.com/';
+          return dominio;
+        break;
+        case 'FAU':
+          dominio = 'http://cpharmafau.com/';
+          return dominio;
+        break;
+        case 'GP':
+          dominio = 'http://cpharmade.com/';
+          return dominio;
+        break;
+      }
+    }
+  </script>
+
+	<script>
+    var dominio = dominio(SedeConnectionJs);
+    const URLEtiquetaUnica = ''+dominio+'assets/functions/functionEtiquetaUnica.php';
+
+		$('#inputCodBar').attr("placeholder", "Haga scan o escriba el codigo de barra");
+		$('#inputCodBar').attr("onblur", "this.placeholder = 'Haga scan o escriba el codigo de barra'");
+		$('#inputCodBar').attr("onfocus", "this.placeholder = ''");
+
+		$('#inputCodBar').keyup(function(e){
+	    if(e.keyCode == 13) {
+
+    	 	$("#MsnError").html('');
+    	 
+        var CodBarrScan = $('#inputCodBar').val();
+        var indiceCodBarScan = ArrJsCB.indexOf(CodBarrScan);
+        var indiceIdScan = indiceCodBarScan+1;
+
+        var indiceIdScanDesc = ArrJs.indexOf(ArrJsCB[indiceIdScan]);
+        var indiceScanDesc = indiceIdScanDesc-1;
+
+        $('#inputCodBar').val('');
+        
+        if( (indiceCodBarScan>0) && (indiceScanDesc)>0 ) {
+
+          var parametro = {
+          //"IdArticulo":ArrJsCB[indiceIdScan]
+          "IdArticulo":CodBarrScan
+          };
+
+          //Incio Armado tablaResuldado
+          $.ajax({
+            data: parametro,
+            url: URLEtiquetaUnica,
+            type: "POST",
+            success: function(data) {
+              var respuesta = data;
+              $("#MsnError").html(respuesta);
+            }
+           });
+          //Fin Armado tablaResuldado
+        }
+        else {
+          
+        }
+      }   
+    });
+	</script>
+     
+  <?php
+    if($CodJson!=""){
+  ?>
+    <script type="text/javascript">
+      const ArrJsCB = eval(<?php echo $CodJson ?>);
+    </script> 
+  <?php
+    }
+  ?>  
+   <?php
+    if($ArtJson!=""){
+  ?>
+    <script type="text/javascript">
+      const ArrJs = eval(<?php echo $ArtJson ?>);
+    </script> 
+  <?php
+    }
+  ?>
+@endsection
+
+<?php
+/**********************************************************************************/
+  /*
+    TITULO: RCPQ_Lista_Articulos_CodBarra
+    FUNCION: Armar una lista de articulos con descripcion e id
+    RETORNO: Lista de articulos con descripcion e id
+    DESAROLLADO POR: SERGIO COVA
+  */
+  function RCPQ_Lista_Articulos_CodBarra() {
+    $sql = "
+      SELECT CodigoBarra,
+      InvArticulo.Id
+      FROM InvCodigoBarra
+      INNER JOIN InvArticulo ON InvArticulo.Id = InvCodigoBarra.InvArticuloId
+    ";
+    return $sql;
+  }
+  /**********************************************************************************/
+  /*
+    TITULO: RCPQ_Lista_Articulos_Descripcion
+    FUNCION: Armar una lista de articulos con descripcion e id
+    RETORNO: Lista de articulos con descripcion e id
+    DESAROLLADO POR: SERGIO COVA
+  */
+  function RCPQ_Lista_Articulos_Descripcion() {
+    $sql = "
+      SELECT
+      InvArticulo.Descripcion,
+      InvArticulo.Id
+      FROM InvArticulo
+    ";
+    return $sql;
+  }
+?>
