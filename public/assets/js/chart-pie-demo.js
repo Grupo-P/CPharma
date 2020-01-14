ea79929bec27d6328bb5d20c61fa3275bc05c113
covacode <covascode@gmail.com>
2020-01-14 17:57:29 -0400

    Graficos Terminados
    se completo la funcionalidad de los graficos con el dashboard del charma para rrhh integrado con galac nomina

--- public/assets/js/chart-pie-demo.js
+++ public/assets/js/chart-pie-demo.js
@@ -9,49 +9,197 @@ var CuentaActivo = 0;
 var URL = 'http://cpharmade.com/assets/functions/functionRHDash.php';
 
 //Incio Armado del chart de FTN
-var paramFTN = {
-  "sede":'FTN'
-};
-$.ajax({
-  data: paramFTN,
-  url: URL,
-  type: "POST",
-  success: function(data) {
-    var respuesta = JSON.parse(data);
-    CuentaIngreso = respuesta['CuentaIngreso'];
-    CuentaEgreso = respuesta['CuentaEgreso'];
-    CuentaActivo = respuesta['CuentaActivo'];
-    
-    // Pie Chart Example myPieChart
-    var ctx = document.getElementById("ChartFTN");
-    var ChartFTN = new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ["Activos", "Ingresos", "Egresos"],
-        datasets: [{
-          data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
-          backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
-          hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
-          hoverBorderColor: "rgba(234, 236, 244, 1)",
-        }],
-      },
-      options: {
-        maintainAspectRatio: false,
-        tooltips: {
-          backgroundColor: "rgb(255,255,255)",
-          bodyFontColor: "#858796",
-          borderColor: '#dddfeb',
-          borderWidth: 1,
-          xPadding: 15,
-          yPadding: 15,
-          displayColors: false,
-          caretPadding: 10,
+  var paramFTN = {
+    "sede":'FTN'
+  };
+  $.ajax({
+    data: paramFTN,
+    url: URL,
+    type: "POST",
+    success: function(data) {
+      var respuesta = JSON.parse(data);
+      CuentaIngreso = respuesta['CuentaIngreso'];
+      CuentaEgreso = respuesta['CuentaEgreso'];
+      CuentaActivo = respuesta['CuentaActivo'];
+      
+      // Pie Chart Example myPieChart
+      var ctx = document.getElementById("ChartFTN");
+      var ChartFTN = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Activos", "Ingresos", "Egresos"],
+          datasets: [{
+            data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
+            backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
+            hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+          }],
         },
-        legend: {
-          display: false
+        options: {
+          maintainAspectRatio: false,
+          tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+          },
+          legend: {
+            display: false
+          },
+          cutoutPercentage: 80,
         },
-        cutoutPercentage: 80,
-      },
-    });
-  }
-});
+      });
+    }
+  });
+//FIN Armado del chart de FTN
+
+//Incio Armado del chart de FLL
+  var paramFLL = {
+    "sede":'FLL'
+  };
+  $.ajax({
+    data: paramFLL,
+    url: URL,
+    type: "POST",
+    success: function(data) {
+      var respuesta = JSON.parse(data);
+      CuentaIngreso = respuesta['CuentaIngreso'];
+      CuentaEgreso = respuesta['CuentaEgreso'];
+      CuentaActivo = respuesta['CuentaActivo'];
+      
+      // Pie Chart Example myPieChart
+      var ctx = document.getElementById("ChartFLL");
+      var ChartFLL = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Activos", "Ingresos", "Egresos"],
+          datasets: [{
+            data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
+            backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
+            hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+          }],
+        },
+        options: {
+          maintainAspectRatio: false,
+          tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+          },
+          legend: {
+            display: false
+          },
+          cutoutPercentage: 80,
+        },
+      });
+    }
+  });
+//FIN Armado del chart de FLL
+
+//Incio Armado del chart de FAU
+  var paramFAU = {
+    "sede":'FAU'
+  };
+  $.ajax({
+    data: paramFAU,
+    url: URL,
+    type: "POST",
+    success: function(data) {
+      var respuesta = JSON.parse(data);
+      CuentaIngreso = respuesta['CuentaIngreso'];
+      CuentaEgreso = respuesta['CuentaEgreso'];
+      CuentaActivo = respuesta['CuentaActivo'];
+      
+      // Pie Chart Example myPieChart
+      var ctx = document.getElementById("ChartFAU");
+      var ChartFAU = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Activos", "Ingresos", "Egresos"],
+          datasets: [{
+            data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
+            backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
+            hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+          }],
+        },
+        options: {
+          maintainAspectRatio: false,
+          tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+          },
+          legend: {
+            display: false
+          },
+          cutoutPercentage: 80,
+        },
+      });
+    }
+  });
+//FIN Armado del chart de FAU
+
+//Incio Armado del chart de MC
+  var paramMC = {
+    "sede":'MC'
+  };
+  $.ajax({
+    data: paramMC,
+    url: URL,
+    type: "POST",
+    success: function(data) {
+      var respuesta = JSON.parse(data);
+      CuentaIngreso = respuesta['CuentaIngreso'];
+      CuentaEgreso = respuesta['CuentaEgreso'];
+      CuentaActivo = respuesta['CuentaActivo'];
+      
+      // Pie Chart Example myPieChart
+      var ctx = document.getElementById("ChartMC");
+      var ChartMC = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ["Activos", "Ingresos", "Egresos"],
+          datasets: [{
+            data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
+            backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
+            hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+          }],
+        },
+        options: {
+          maintainAspectRatio: false,
+          tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+          },
+          legend: {
+            display: false
+          },
+          cutoutPercentage: 80,
+        },
+      });
+    }
+  });
+//FIN Armado del chart de MC
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -972,16 +972,94 @@
 	<!-- Dashboard Metricas-->
 	<div class="card-deck">
 		<!-- Pie Chart FTN-->
-    <div class="col-xl-4 col-lg-5">
+    <div class="col-xl-3 col-lg-5">
       <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-info">FARMACIA TIERRA NEGRA, C.A.</h6>
+          <h6 class="m-0 font-weight-bold text-info">FARMACIA TIERRA NEGRA, C.A</h6>
         </div>
         <!-- Card Body -->
         <div class="card-body">
           <div class="chart-pie pt-4 pb-2">
             <canvas id="ChartFTN"></canvas>
+          </div>
+          <div class="mt-4 text-center small">
+            <span class="mr-2">
+              <i class="fas fa-circle text-primary"></i> Activos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-success"></i> Ingresos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-danger"></i> Egresos
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Pie Chart FLL-->
+    <div class="col-xl-3 col-lg-5">
+      <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <h6 class="m-0 font-weight-bold text-info">FARMACIA LA LAGO, C.A</h6>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+          <div class="chart-pie pt-4 pb-2">
+            <canvas id="ChartFLL"></canvas>
+          </div>
+          <div class="mt-4 text-center small">
+            <span class="mr-2">
+              <i class="fas fa-circle text-primary"></i> Activos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-success"></i> Ingresos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-danger"></i> Egresos
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Pie Chart FAU-->
+    <div class="col-xl-3 col-lg-5">
+      <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <h6 class="m-0 font-weight-bold text-info">FARMACIA AV. UNIVERSIDAD, C.A</h6>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+          <div class="chart-pie pt-4 pb-2">
+            <canvas id="ChartFAU"></canvas>
+          </div>
+          <div class="mt-4 text-center small">
+            <span class="mr-2">
+              <i class="fas fa-circle text-primary"></i> Activos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-success"></i> Ingresos
+            </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-danger"></i> Egresos
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Pie Chart MC-->
+    <div class="col-xl-3 col-lg-5">
+      <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <h6 class="m-0 font-weight-bold text-info">MULATA CAFE, C.A.</h6>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+          <div class="chart-pie pt-4 pb-2">
+            <canvas id="ChartMC"></canvas>
           </div>
           <div class="mt-4 text-center small">
             <span class="mr-2">
