eacda58fa1438056d3ef5f00e11f170d5714fc20
covacode <covascode@gmail.com>
2020-01-14 17:38:54 -0400

    Update chart-pie-demo.js
    Se completo la primera grafica

--- public/assets/js/chart-pie-demo.js
+++ public/assets/js/chart-pie-demo.js
@@ -18,41 +18,40 @@ $.ajax({
   type: "POST",
   success: function(data) {
     var respuesta = JSON.parse(data);
-    console.log(data);
     CuentaIngreso = respuesta['CuentaIngreso'];
     CuentaEgreso = respuesta['CuentaEgreso'];
     CuentaActivo = respuesta['CuentaActivo'];
+    
+    // Pie Chart Example myPieChart
+    var ctx = document.getElementById("ChartFTN");
+    var ChartFTN = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Activos", "Ingresos", "Egresos"],
+        datasets: [{
+          data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
+          backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
+          hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
+          hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          caretPadding: 10,
+        },
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+      },
+    });
   }
- });
-
-// Pie Chart Example myPieChart
-var ctx = document.getElementById("ChartFTN");
-var ChartFTN = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Activos", "Ingresos", "Egresos"],
-    datasets: [{
-      data: [CuentaActivo,CuentaIngreso,CuentaEgreso],
-      backgroundColor: ['#4e73df', '#1cc88a', '#dc3545'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#c62f3e'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
 });
