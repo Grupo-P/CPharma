cdf2ac8cc663bd8032b65b8c612528b350ea6b85
mhenriquezm <manuelhm1993@gmail.com>
2020-01-29 16:44:01 -0400

    Saldo real en dolares

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -906,12 +906,9 @@
               ->orderBy('id', 'desc')
               ->first();
 
-              if(empty($saldo_actualDs)) {
-              	echo 'Saldo actual: ' . number_format(0, 2, ',', '.') . " " . SigDolar;
-              }
-              else {
-              	echo 'Saldo actual: ' . number_format($saldo_actualDs->saldo_actual, 2, ',', '.') . " " . SigDolar;
-              }
+              echo 'Saldo actual: ' . number_format(DB::table('configuracions')
+            ->where('id', 8)
+            ->value('valor'), 2, ',', '.') . " " . SigDolar;
           ?>            
           </span>
         </h3>
