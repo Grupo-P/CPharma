698a74962a0498264c74b1ba821c1c351611fe21
covacode <covascode@gmail.com>
2020-08-04 22:14:16 -0300

    Inventario update
    combo de seleccion multiple

--- resources/views/pages/inventario/por_aleatorio.blade.php
+++ resources/views/pages/inventario/por_aleatorio.blade.php
@@ -231,7 +231,7 @@
             <th scope="col" class="CP-sticky">Codigo de Barra</th>
             <th scope="col" class="CP-sticky">Descripcion</th>
             <th scope="col" class="CP-sticky">Existencia</th>
-            <th scope="col" class="CP-sticky">Seleccion</th>
+            <th scope="col" class="CP-sticky">Seleccion <input type="checkbox" onclick="marcarTodos(this)"/></th>
           </tr>
         </thead>
         <tbody>
@@ -334,3 +334,17 @@
     return $sql;
   }
 ?>
+
+<script>
+  function marcarTodos(source) 
+  {
+    checkboxes=document.getElementsByTagName('input'); //obtenemos todos los controles del tipo Input
+    for(i=0;i<checkboxes.length;i++) //recoremos todos los controles
+    {
+      if(checkboxes[i].type == "checkbox") //solo si es un checkbox entramos
+      {
+        checkboxes[i].checked=source.checked; //si es un checkbox le damos el valor del checkbox que lo llamó (Marcar/Desmarcar Todos)
+      }
+    }
+  }
+</script>
