18def7143907735c9934bd3d28ac59026b18b636
covacode <covascode@gmail.com>
2020-09-13 12:06:22 -0300

    Model Categorias

---
+++ app/Categoria.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace compras;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+   /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'codigo_categoria', 'nombre', 'estatus', 'user'
+    ];
+}
