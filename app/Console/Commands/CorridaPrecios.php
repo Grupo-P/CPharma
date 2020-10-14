3aaf3a7ed7be265cbb3e90381a31e850159c1fb2
covacode <covascode@gmail.com>
2020-10-13 21:14:36 -0300

    corrida de precios

---
+++ app/Console/Commands/CorridaPrecios.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace compras\Console\Commands;
+
+use Illuminate\Console\Command;
+use compras\Auditoria;
+
+class CorridaPrecios extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Capturar:CorridaPrecios';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Este comando ejecuta la corrida de precios automatica';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        include(app_path().'\functions\config.php');
+        include(app_path().'\functions\functions.php');
+        include(app_path().'\functions\querys_mysql.php');
+        include(app_path().'\functions\querys_sqlserver.php');
+
+        //Aqui debe ir el llamado a la funcion de corrida de precios
+
+        $Auditoria = new Auditoria();
+        $Auditoria->accion = 'EJECUTAR';
+        $Auditoria->tabla = 'CORRIDA DE PRECIOS';
+        $Auditoria->registro = 'CPHARMA';
+        $Auditoria->user = 'SYSTEM';
+        $Auditoria->save();
+
+        $this->info('La corrida de precios fue ejecutada satisfactoriamente!');
+    }
+}
