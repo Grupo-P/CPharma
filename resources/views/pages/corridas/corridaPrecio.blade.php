9302f83d9293ac872a7eebae3aa90ca87ad84f56
covacode <covascode@gmail.com>
2020-10-13 22:29:10 -0300

    Update corridaPrecio.blade.php

--- resources/views/pages/corridas/corridaPrecio.blade.php
+++ resources/views/pages/corridas/corridaPrecio.blade.php
@@ -58,22 +58,63 @@
     <hr class="row align-items-start col-12">
 
 <?php
+  use compras\Configuracion;
 	include(app_path().'\functions\config.php');
   include(app_path().'\functions\functions.php');
   include(app_path().'\functions\querys_mysql.php');
   include(app_path().'\functions\querys_sqlserver.php');
 
+  $_GET['SEDE'] = FG_Mi_Ubicacion();
+
 	if( (Auth::user()->departamento == 'GERENCIA') 
    	&& 
-   	((Auth::user()->email == 'giordany@farmacia72.com')      
+   	((Auth::user()->email == 'giordany@farmacia72.com')     
    	|| (Auth::user()->email == 'giancarlos@farmacia72.com'))
 	)  {
 
-	
-		echo("Aqui se ve la corrida de precios");
+    $configuracion = Configuracion::where('variable','DolarCalculo')->get();
 
+    if (isset($_GET['tipoCorrida'])) {
+
+      echo($_GET['tipoCorrida']);
+
+    }else{
+
+  		echo '
+        <form id="form" autocomplete="off" action="" style="margin-bottom:50px;">
+          <table style="width:100%;">
+            <tr>
+              <td align="center">Tasa de calculo:</td>
+              <td>
+                <input id="tasaCalculo" type="text" name="tasaCalculo" disabled style="width:auto;" value="'.number_format($configuracion[0]->valor,2,"," ,"." ).'">
+              </td>
+
+              <td align="center">Fecha:</td>
+              <td>
+                <input id="fecha" name="fecha" value="'.$configuracion[0]->updated_at->format("d/m/Y").'" type="text" disabled style="width:auto;">                          
+              </td>
+
+              <td align="center">Tipo de Corrida:</td>
+              <td>
+                <input type="radio" id="subida" name="tipoCorrida" value="subida" checked>
+                <label for="subida">Subida</label><br>
+                <input type="radio" id="subida/bajada" name="tipoCorrida" value="bajada">
+                <label for="subida/bajada">Subida/Bajada</label><br>
+              </td>
+
+              <td>
+                <input id="SEDE" name="SEDE" type="hidden" value="';echo($_GET['SEDE']);echo'">
+              <input type="submit" value="Ejecutar" class="btn btn-outline-success" style="width:80%;">
+              </td>
+            </tr>          
+          </table>                    
+        </form>
+      ';
+    }
+        
 	}else{
-		echo("Fuera de aqui");
+		echo '
+      <h6 align="center" style="margin-top:50px; margin-bottom:150px;">Ussted NO tiene permisos para acceder a este modulo</h6>';
 	}
 ?>
 @endsection
