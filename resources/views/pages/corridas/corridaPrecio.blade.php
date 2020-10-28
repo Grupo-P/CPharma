b40cf1f4dd5102f19b4d97eb04cf2a06a1cace1b
covacode <covascode@gmail.com>
2020-10-27 21:47:09 -0300

    updates

--- resources/views/pages/corridas/corridaPrecio.blade.php
+++ resources/views/pages/corridas/corridaPrecio.blade.php
@@ -109,12 +109,62 @@
         FG_Corrida_Precio($_GET['tipoCorrida'],$configuracion[0]->valor);
 
         $connCPharma = FG_Conectar_CPharma();
-        $sql = "SELECT * FROM auditoria_corridas ORDER BY FECHA DESC LIMIT 1";
+        $sql = "SELECT * FROM auditoria_corridas ORDER BY id DESC LIMIT 1";
         $result = mysqli_query($connCPharma,$sql);
-        $row = mysqli_fetch_assoc($result);
-        $evaluados = $row['evaluados'];
+        $row = mysqli_fetch_assoc($result);        
         
-        
+        echo'
+        <hr class="row align-items-start col-12">
+        <table style="width:100%;" class="CP-stickyBar">
+            <tr>
+                <td style="width:90%;">
+                  <div class="input-group md-form form-sm form-1 pl-0">
+                <div class="input-group-prepend">
+                  <span class="input-group-text purple lighten-3" id="basic-text1"><i class="fas fa-search text-white"
+                      aria-hidden="true"></i></span>
+                </div>
+                <input class="form-control my-0 py-1" type="text" placeholder="Buscar..." aria-label="Search" id="myInput" onkeyup="FilterAllTable()" autofocus="autofocus">
+              </div>
+                </td>
+            </tr>
+        </table>
+        <br/>
+        ';
+
+        echo '
+        <table class="table table-striped table-borderless col-12 sortable" id="myTable">
+          <thead class="thead-dark">
+            <tr>
+                <th scope="col" class="CP-sticky">#</th>
+                <th scope="col" class="CP-sticky">Tipo de corrida</th>
+                <th scope="col" class="CP-sticky">Operador</th>
+                <th scope="col" class="CP-sticky">Fecha</th>
+                <th scope="col" class="CP-sticky">Hora</th>
+                <th scope="col" class="CP-sticky">Nota</th>
+                <th scope="col" class="CP-sticky">Articulos evaluados</th>
+                <th scope="col" class="CP-sticky">Articulos procesados exito</th>
+                <th scope="col" class="CP-sticky">Articulos con fallas</th>
+                <th scope="col" class="CP-sticky">Articulos que cambiaron</th>
+                <th scope="col" class="CP-sticky">Articulos sin cambios</th>
+            </tr>
+          </thead>
+          <tbody>
+          ';
+            echo '<td align="center"><strong>'.$row['id'].'</strong></td>';
+            echo '<td align="center">'.$row['tipocorrida'].'</td>';
+            echo '<td align="center">'.$row['operador'].'</td>';
+            echo '<td align="center">'.$row['fecha'].'</td>';
+            echo '<td align="center">'.$row['hora'].'</td>';
+            echo '<td align="center">'.$row['observacion'].'</td>';
+            echo '<td align="center">'.$row['evaluados'].'</td>';
+            echo '<td align="center">'.$row['exitos'].'</td>';
+            echo '<td align="center">'.$row['fallas'].'</td>';
+            echo '<td align="center">'.$row['cambios'].'</td>';
+            echo '<td align="center">'.$row['nocambio'].'</td>';            
+          echo'
+        </tbody>
+      </table>
+        ';
 
         $FinCarga = new DateTime("now");
         $IntervalCarga = $InicioCarga->diff($FinCarga);
