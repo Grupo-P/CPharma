e2f7a58342c98bac68d2a3896411729641ce0e61
covacode <covascode@gmail.com>
2020-11-18 20:54:46 -0300

    migrate db orden detalle

---
+++ database/migrations/2020_11_18_195057_add_four_colums.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFourColums extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orden_compra_detalles', function (Blueprint $table) {
+            $table->string('unidades_vendidas')->nullable()->after('updated_at');
+            $table->string('venta_diaria_real')->nullable()->after('unidades_vendidas');
+            $table->string('pedir_real')->nullable()->after('venta_diaria_real');
+            $table->string('dias_pedir')->nullable()->after('pedir_real');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orden_compra_detalles', function (Blueprint $table) {
+            $table->dropColumn('unidades_vendidas');
+            $table->dropColumn('venta_diaria_real');
+            $table->dropColumn('pedir_real');
+            $table->dropColumn('dias_pedir');
+        });
+    }
+}
