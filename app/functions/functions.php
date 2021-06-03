af3e2c98db280e3f5d07f7c4a101aaf078136d80
Sergio Cova <covascode@gmail.com>
2021-06-03 18:50:22 -0300

    Update funcion conexion

--- app/functions/functions.php
+++ app/functions/functions.php
@@ -134,7 +134,7 @@
         //FIN BLOQUE DE FLL
         //INICIO BLOQUE DE FAU
             case '12':
-                return 'DBs';
+                return 'FAU';
                 //return 'DBs';
             break;
         //FIN BLOQUE DE FAU
