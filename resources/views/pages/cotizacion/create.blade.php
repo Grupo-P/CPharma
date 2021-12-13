17f5a0a525b548228b763883771fca53ffbb74e5
Nisa Delgado <nisadelgado@gmail.com>
2021-12-13 11:40:29 -0400

    13/12/2021

--- resources/views/pages/cotizacion/create.blade.php
+++ resources/views/pages/cotizacion/create.blade.php
@@ -4,97 +4,6 @@
     Cotizaciones
 @endsection
 
-@section('scriptsFoot')
-    <link href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-    <script>
-        $('[name=nombre_cliente]').autocomplete({
-            source: function (request, response) {
-                $.ajax({
-                    url: '/cotizacion/create',
-                    type: 'GET',
-                    dataType: 'json',
-                    data: {
-                        buscar: request.term,
-                        tipo: 'cliente'
-                    },
-                    success: function (data) {
-                        response($.map(data, function (item) {
-                            return {
-                                label: item.nombre + ' | ' + item.ci_rif,
-                                value: item.nombre,
-                                direccion: item.direccion,
-                                ci_rif: item.ci_rif
-                            }
-                        }));
-                    }
-                });
-            },
-            select: function (event, ui) {
-                $('[name=ci_cliente]').val(ui.item.ci_rif);
-                $('[name=direccion_cliente]').val(ui.item.direccion);
-            },
-            autoFocus: true
-        });
-
-        $('[name=ci_cliente]').autocomplete({
-            source: function (request, response) {
-                $.ajax({
-                    url: '/cotizacion/create',
-                    type: 'GET',
-                    dataType: 'json',
-                    data: {
-                        buscar: request.term,
-                        tipo: 'cliente'
-                    },
-                    success: function (data) {
-                        response($.map(data, function (item) {
-                            return {
-                                label: item.nombre + ' | ' + item.ci_rif,
-                                value: item.ci_rif,
-                                direccion: item.direccion,
-                                nombre: item.nombre
-                            }
-                        }));
-                    }
-                });
-            },
-            select: function (event, ui) {
-                $('[name=nombre_cliente]').val(ui.item.nombre);
-                $('[name=direccion_cliente]').val(ui.item.direccion);
-            },
-            autoFocus: true
-        });
-
-        $('[name=buscar_articulo]').autocomplete({
-            source: function (request, response) {
-                $.ajax({
-                    url: '/cotizacion/create',
-                    type: 'GET',
-                    dataType: 'json',
-                    data: {
-                        buscar: request.term,
-                        tipo: 'articulo'
-                    },
-                    success: function (data) {
-                        response($.map(data, function (item) {
-                            return {
-                                label: item.codigo_interno + ' | ' + item.codigo_barra + ' | ' + item.descripcion,
-                                value: item.codigo_interno + ' | ' + item.codigo_barra + ' | ' + item.descripcion
-                            }
-                        }));
-                    }
-                });
-            },
-            select: function (event, ui) {
-
-            },
-            autoFocus: true
-        });
-    </script>
-@endsection
-
 @section('content')
     <h1 class="h5 text-info">
         <i class="fas fa-file"></i>
@@ -114,12 +23,12 @@
                     <div class="row">
                         <div class="col">
                             <label for="nombre_cliente">Nombre del cliente</label>
-                            <input type="text" class="form-control" name="nombre_cliente">
+                            <input type="text" autofocus class="form-control" name="nombre_cliente" required>
                         </div>
 
                         <div class="col">
                             <label for="ci_cliente">CI/RIF del cliente</label>
-                            <input type="text" class="form-control" name="ci_cliente">
+                            <input type="text" class="form-control" name="ci_cliente" required>
                         </div>
 
                         <div class="col">
@@ -137,7 +46,19 @@
                     <div class="row">
                         <div class="col">
                             <label for="buscar_articulo">Buscar articulo:</label>
-                            <input type="text" class="form-control" name="buscar_articulo">
+
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="buscar_articulo">
+
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-info add">
+                                        <i class="fa fa-plus"></i>
+                                        Agregar
+                                    </button>
+                                </div>
+                            </div>
+
+                            <input type="hidden" name="id_articulo">
                         </div>
                     </div>
 
@@ -154,13 +75,86 @@
                                         <th>Precio {{ SigDolar }}</th>
                                     </tr>
                                 </thead>
+
+                                <tbody class="articulos"></tbody>
                             </table>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <button type="submit" class="mt-3 btn btn-primary btn-lg bg-dark border-dark">Crear cotización</button>
+            <div class="row mt-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary bg-dark border-dark">
+                        <i class="fa fa-check"></i>
+                        Crear cotización
+                    </button>
+
+                    <a href="/home" class="btn btn-danger">
+                        <i class="fa fa-reply"></i>
+                        Atrás
+                    </a>
+                </div>
+            </div>
         </form>
     </div>
+@endsection
+
+@section('scriptsFoot')
+    <link href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <script>
+        clientes = {!! $clientes !!};
+
+        $('[name=nombre_cliente]').autocomplete({
+            source: clientes,
+            autoFocus: true,
+            select: function (event, ui) {
+                $('[name=ci_cliente]').val(ui.item.ci_rif);
+                $('[name=direccion_cliente]').val(ui.item.direccion);
+            }
+        });
+
+        articulos = {!! $articulos !!};
+
+        $('[name=buscar_articulo]').autocomplete({
+            source: articulos,
+            autoFocus: true,
+            select: function (event, ui) {
+                $('[name=id_articulo]').val(ui.item.id_articulo);
+
+                articulo = {
+                    codigo_articulo: ui.item.codigo_interno,
+                    codigo_barra: ui.item.codigo_barra,
+                    descripcion: ui.item.descripcion,
+                    componente: ui.item.componente,
+                    precio_bs: ui.item.precio_bs,
+                    precio_ds: ui.item.precio_ds,
+                }
+            }
+        });
+
+        $('.add').click(function () {
+            id_articulo = $('[name=id_articulo]').val();
+
+            if (id_articulo) {
+                $('[name=buscar_articulo]').val('');
+                $('[name=id_articulo]').val('');
+
+                $('.articulos').append(`
+                    <tr>
+                        <td>${articulo.codigo_articulo}</td>
+                        <td>${articulo.codigo_barra}</td>
+                        <td>${articulo.descripcion}</td>
+                        <td>${articulo.componente}</td>
+                        <td>${articulo.precio_bs}</td>
+                        <td>${articulo.precio_ds}</td>
+                    </tr>
+                `);
+            }
+
+            $('[name=buscar_articulo]').focus();
+        });
+    </script>
 @endsection
