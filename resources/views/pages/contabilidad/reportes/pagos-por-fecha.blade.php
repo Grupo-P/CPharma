a871597df6f5943cc672b5a9d8959f9f981a19da
Nisa Delgado <nisadelgado@gmail.com>
2022-03-21 16:08:13 -0400

    Mostrar/ocultar columnas en pagos por fecha

--- resources/views/pages/contabilidad/reportes/pagos-por-fecha.blade.php
+++ resources/views/pages/contabilidad/reportes/pagos-por-fecha.blade.php
@@ -6,7 +6,118 @@
 @endsection
 
 
+@section('scriptsHead')
+    <script>
+        function mostrar_ocultar(that, elemento) {
+            if (that.checked) {
+                return $('.' + elemento).show();
+            }
+
+            return $('.' + elemento).hide();
+        }
+
+        campos = ['nro_recibo', 'fecha_hora', 'tipo', 'emisor', 'proveedor', 'monto_pago', 'monto_proveedor_base', 'monto_proveedor_iva', 'comentario', 'plan_cuentas', 'estado', 'conciliado', 'operador'];
+
+        function mostrar_todas(that) {
+            if (that.checked) {
+                for (var i = campos.length - 1; i >= 0; i--) {
+                    $('.' + campos[i]).show();
+                    $('[name='+campos[i]+']').prop('checked', true);
+                }
+            } else {
+                for (var i = campos.length - 1; i >= 0; i--) {
+                    $('.' + campos[i]).hide();
+                    $('[name='+campos[i]+']').prop('checked', false);
+                }
+            }
+        }
+    </script>
+@endsection
+
+
 @section('content')
+    <div class="modal fade" id="ver_campos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Mostrar u ocultar columnas</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'nro_recibo')" name="nro_recibo" checked>
+                # de recibo
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'fecha_hora')" name="fecha_hora" checked>
+                Fecha y hora
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'tipo')" name="tipo" checked>
+                Tipo
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'emisor')" name="emisor" checked>
+                Emisor
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'proveedor')" name="proveedor" checked>
+                Proveedor
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'monto_pago')" name="monto_pago" checked>
+                Monto del pago
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'monto_proveedor_base')" name="monto_proveedor_base" checked>
+                Monto proveedor base
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'monto_proveedor_iva')" name="monto_proveedor_iva" checked>
+                Monto proveedor IVA
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'comentario')" name="comentario" checked>
+                Comentario
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'plan_cuentas')" name="plan_cuentas" checked>
+                Plan de cuentas
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'estado')" name="estado" checked>
+                Estado
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_ocultar(this, 'conciliado')" name="conciliado" checked>
+                Conciliado
+            </div>
+
+            <div class="form-group">
+                <input type="checkbox" onclick="mostrar_todas(this)" name="Marcar todas" checked>
+                Marcar todas
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h1 class="h5 text-info">
         <i class="fas fa-file-invoice">
         </i>
@@ -28,23 +139,25 @@
 
         <h6 align="center">Periodo desde el {{ $fechaInicio }} al {{ $fechaFin }}</h6>
 
+        <h6 align="center"><a href="" data-toggle="modal" data-target="#ver_campos"><i class="fa fa-eye"></i> Mostrar u ocultar campos<a></h6>
+
         <table class="table table-striped table-bordered col-12 sortable" id="myTable">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col" class="CP-sticky">#</th>
-                    <th scope="col" class="CP-sticky"># de recibo</th>
-                    <th scope="col" class="CP-sticky">Fecha y hora</th>
-                    <th scope="col" class="CP-sticky">Tipo</th>
-                    <th scope="col" class="CP-sticky">Emisor</th>
-                    <th scope="col" class="CP-sticky">Proveedor</th>
-                    <th scope="col" class="CP-sticky">Monto del pago</th>
-                    <th scope="col" class="CP-sticky">Monto proveedor base</th>
-                    <th scope="col" class="CP-sticky">Monto proveedor IVA</th>
-                    <th scope="col" class="CP-sticky">Comentario</th>
-                    <th scope="col" class="CP-sticky">Plan de cuentas</th>
-                    <th scope="col" class="CP-sticky">Estado</th>
-                    <th scope="col" class="CP-sticky">Conciliado</th>
-                    <th scope="col" class="CP-sticky">Operador</th>
+                    <th scope="col" class="nro_recibo CP-sticky"># de recibo</th>
+                    <th scope="col" class="fecha_hora CP-sticky">Fecha y hora</th>
+                    <th scope="col" class="tipo CP-sticky">Tipo</th>
+                    <th scope="col" class="emisor CP-sticky">Emisor</th>
+                    <th scope="col" class="proveedor CP-sticky">Proveedor</th>
+                    <th scope="col" class="monto_pago CP-sticky">Monto del pago</th>
+                    <th scope="col" class="monto_proveedor_base CP-sticky">Monto proveedor base</th>
+                    <th scope="col" class="monto_proveedor_iva CP-sticky">Monto proveedor IVA</th>
+                    <th scope="col" class="comentario CP-sticky">Comentario</th>
+                    <th scope="col" class="plan_cuentas CP-sticky">Plan de cuentas</th>
+                    <th scope="col" class="estado CP-sticky">Estado</th>
+                    <th scope="col" class="conciliado CP-sticky">Conciliado</th>
+                    <th scope="col" class="operador CP-sticky">Operador</th>
                     <th scope="col" class="CP-sticky"></th>
                 </tr>
             </thead>
@@ -59,10 +172,10 @@
                 @foreach($pagos as $pago)
                     <tr>
                         <td class="text-center">{{ $loop->iteration }}</td>
-                        <td class="text-center">{{ str_pad($pago->id, 5, 0, STR_PAD_LEFT) }}</td>
-                        <td class="text-center">{{ date_format($pago->created_at, 'd/m/Y h:i A') }}</td>
-                        <td class="text-center">{{ (get_class($pago) == 'compras\ContPagoBancario') ? 'Banco' : 'Efectivo' }}</td>
-                        <td class="text-center">
+                        <td class="text-center nro_recibo">{{ str_pad($pago->id, 5, 0, STR_PAD_LEFT) }}</td>
+                        <td class="text-center fecha_hora">{{ date_format($pago->created_at, 'd/m/Y h:i A') }}</td>
+                        <td class="text-center tipo">{{ (get_class($pago) == 'compras\ContPagoBancario') ? 'Banco' : 'Efectivo' }}</td>
+                        <td class="text-center emisor">
                             @if(get_class($pago) == 'compras\ContPagoBancario' && $pago->banco)
                                 {{ $pago->banco->alias_cuenta }}
                             @endif
@@ -108,7 +221,7 @@
                             @endif
                         </td>
 
-                        <td align="center" class="CP-barrido">
+                        <td align="center" class="CP-barrido proveedor">
                             @if(isset($pago->proveedor))
                                 @php
                                     $fechaInicio = date_create();
@@ -124,7 +237,7 @@
                             @endif
                         </td>
 
-                        <td class="text-center">
+                        <td class="text-center monto_pago">
                             @if($pago->monto)
                                 @php
                                     $monto_banco = monto_banco($pago->monto, $pago->iva, $pago->retencion_deuda_1, $pago->retencion_deuda_2, $pago->retencion_iva);
@@ -174,9 +287,9 @@
                             }
                         @endphp
 
-                        <td class="text-center">{{ number_format($monto_proveedor_base, 2, ',', '.') }}</td>
-                        <td class="text-center">{{ ($pago->iva) ? number_format($pago->iva, 2, ',', '.') : '' }}</td>
-                        <td class="text-center">{!! ($pago->comentario) ? $pago->comentario : $pago->concepto !!}</td>
+                        <td class="text-center monto_proveedor_base">{{ number_format($monto_proveedor_base, 2, ',', '.') }}</td>
+                        <td class="text-center monto_proveedor_iva">{{ ($pago->iva) ? number_format($pago->iva, 2, ',', '.') : '' }}</td>
+                        <td class="text-center comentario">{!! ($pago->comentario) ? $pago->comentario : $pago->concepto !!}</td>
 
                         @php
                             if ($pago->cuenta) {
@@ -188,10 +301,10 @@
                             }
                         @endphp
 
-                        <td class="text-center">{{ $plan_cuentas }}</td>
-                        <td class="text-center">{{ ($pago->estatus == 'Reversado') ? 'Reversado' : 'Pagado' }}</td>
-                        <td class="text-center">{{ ($pago->fecha_conciliado) ? 'Si' : 'No' }}</td>
-                        <td class="text-center">{{ ($pago->user) ? $pago->user : $pago->operador }}</td>
+                        <td class="text-center plan_cuentas">{{ $plan_cuentas }}</td>
+                        <td class="text-center estado">{{ ($pago->estatus == 'Reversado') ? 'Reversado' : 'Pagado' }}</td>
+                        <td class="text-center conciliado">{{ ($pago->fecha_conciliado) ? 'Si' : 'No' }}</td>
+                        <td class="text-center operador">{{ ($pago->user) ? $pago->user : $pago->operador }}</td>
                         <td class="text-center">
                             <a target="_blank" href="{{ $url }}" role="button" class="btn btn-outline-success btn-sm" data-toggle="tooltip" data-placement="top" title="Ver soporte">
                                 <i class="fas fa-eye"></i>
