228491def6e4b0ec2e856f5db14ab21b76fc8d30
Sergio Cova <covascode@gmail.com>
2022-04-21 20:48:00 -0300

    Update Reporte 49

--- resources/views/pages/reporte/reporte49.blade.php
+++ resources/views/pages/reporte/reporte49.blade.php
@@ -1,12 +1,14 @@
 <?php    
     $SedeConnection = FG_Mi_Ubicacion();
 
+	/*
 	echo'<pre>';
 	print_r($arrayGlobal);
 	echo'<br><br>';
 	print_r($arrayArticulos);
 	echo'</pre>';
 	die;
+	*/
 ?>
 
 @extends('layouts.model')
@@ -23,15 +25,15 @@
 	<hr class="row align-items-start col-12">
 
 	<?php					
-		if (isset($SedeConnection)){			
-			echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.FG_Nombre_Sede($SedeConnection).'</h1>';
+		if (isset($arrayGlobal['SedeConnection'])){			
+			echo '<h1 class="h5 text-success"  align="left"> <i class="fas fa-prescription"></i> '.FG_Nombre_Sede($arrayGlobal['SedeConnection']).'</h1>';
 		}
 		echo '<hr class="row align-items-start col-12">';
 
-		R49_Reposicion_Inventario($ArrayData);
+		R49_Reposicion_Inventario($arrayGlobal,$arrayArticulos);
 		FG_Guardar_Auditoria('CONSULTAR','REPORTE','Reposicion de Inventario');
 		
-        echo'Tiempo de carga: '.$Tiempo;
+        echo'Tiempo de carga: '.$arrayGlobal['Tiempo'];
 	?>
 @endsection
 
@@ -43,7 +45,7 @@
 		RETORNO: Lista de activacio de proveedores
 		DESAROLLADO POR: SERGIO COVA
  	*/
-	function R49_Reposicion_Inventario($ArrayData) {
+	function R49_Reposicion_Inventario($arrayGlobal,$arrayArticulos) {
 		
         $contador = 1;
 		
@@ -59,8 +61,8 @@
 		</div>
 		<br/>
         
-        <h6 align="left">Rango Anterior: '.$FInicial_RangoAnterior.' al '.$FInicial_RangoUltimo_menos.' </h6>
-        <h6 align="left">Rango Ultimo: '.$FInicial_RangoUltimo.' al '.$Hoy.' </h6>
+        <h6 align="left">Rango Anterior: '.$arrayGlobal['FInicialRangoAnterior'].' al '.$arrayGlobal['FInicialRangoUltimoMenos'].' </h6>
+        <h6 align="left">Rango Ultimo: '.$arrayGlobal['FInicialRangoUltimo'].' al '.$arrayGlobal['Hoy'].' </h6>
 
 		<table class="table table-striped table-bordered col-12 sortable" id="myTable">
 	  	<thead class="thead-dark">
@@ -72,30 +74,30 @@
                 <th scope="col" class="CP-sticky">Existencia</th>
                 <th scope="col" class="CP-sticky">Ultima Venta</th>
                 <th scope="col" class="CP-sticky">Ultima Compra</th>
-                <th scope="col" class="CP-sticky">Dias Restantes (Rango Anterior) ('.$FInicial_RangoAnterior.' al '.$FInicial_RangoUltimo_menos.')</th>                
-                <th scope="col" class="CP-sticky">Dias Restantes (Rango Ultimo) ('.$FInicial_RangoUltimo.' al '.$Hoy.')</th>
+                <th scope="col" class="CP-sticky">Dias Restantes (Rango Anterior) ('.$arrayGlobal['FInicialRangoAnterior'].' al '.$arrayGlobal['FInicialRangoUltimoMenos'].')</th>                
+                <th scope="col" class="CP-sticky">Dias Restantes (Rango Ultimo) ('.$arrayGlobal['FInicialRangoUltimo'].' al '.$arrayGlobal['Hoy'].')</th>
 		    </tr>
 	  	</thead>
   	<tbody>
 		';
-		foreach ($ArrayData as $Data) {                    						
+		foreach ($arrayArticulos as $articulo) {
 			echo '<tr>';
 			echo '<td align="center"><strong>'.intval($contador).'</strong></td>';
-            echo '<td align="center">'.$Data['CodigoInterno'].'</td>';
-            echo '<td align="center">'.$Data['CodigoBarra'].'</td>';
+            echo '<td align="center">'.$articulo['CodigoInterno'].'</td>';
+            echo '<td align="center">'.$articulo['CodigoBarra'].'</td>';
 
             echo
 			'<td align="left" class="CP-barrido">
-			<a href="/reporte2?Id='.$Data['IdArticulo'].'&SEDE='.$Data['SedeConnection'].'" style="text-decoration: none; color: black;" target="_blank">'
-				.FG_Limpiar_Texto($Data['Descripcion']).
+			<a href="/reporte2?Id='.$articulo['IdArticulo'].'&SEDE='.$arrayGlobal['SedeConnection'].'" style="text-decoration: none; color: black;" target="_blank">'
+				.FG_Limpiar_Texto($articulo['Descripcion']).
 			'</a>
 			</td>';
 			
-            echo '<td align="center">'.intval($Data['Existencia']).'</td>';
-			echo '<td align="center">'.$Data['UltimaVenta'].'</td>';
-            echo '<td align="center">'.$Data['UltimaCompra'].'</td>';
-            echo '<td align="center">'.$Data['DiasRestantesQuiebre_RangoAnterior'].'</td>';			
-            echo '<td align="center">'.$Data['DiasRestantesQuiebre_RangoUltimo'].'</td>';			
+            echo '<td align="center">'.intval($articulo['Existencia']).'</td>';
+			echo '<td align="center">'.$articulo['UltimaVenta'].'</td>';
+            echo '<td align="center">'.$articulo['UltimaCompra'].'</td>';
+            echo '<td align="center">'.$articulo['DiasRestantesAnterior'].'</td>';
+            echo '<td align="center">'.$articulo['DiasRestantesUltimo'].'</td>';
 			echo '</tr>';
 			$contador++;
   	    }
