6130b59c59d5e4b1934099a71fd3c34fb658283f
Sergio Cova <covascode@gmail.com>
2022-04-21 22:10:38 -0300

    Update Reporte 49

--- resources/views/pages/reporte/reporte49.blade.php
+++ resources/views/pages/reporte/reporte49.blade.php
@@ -1,15 +1,3 @@
-<?php    
-    $SedeConnection = FG_Mi_Ubicacion();
-	/*
-	echo'<pre>';
-	print_r($arrayGlobal);
-	echo'<br><br>';
-	print_r($arrayArticulos);
-	echo'</pre>';
-	die;	
-	*/
-?>
-
 @extends('layouts.model')
 
 @section('title')
