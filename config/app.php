d0803fcc78c50b414ccba72eacd725f3375b159c
Sergio Cova <covascode@gmail.com>
2022-08-18 20:38:18 -0300

    laravel-permission

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'PBase'),
 
     /*
     |--------------------------------------------------------------------------
@@ -197,6 +197,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
