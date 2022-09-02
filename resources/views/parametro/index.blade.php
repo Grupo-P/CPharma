b9d29a2a77d2b26125a2b2e45798dbf6a8079dc3
Sergio Cova <covascode@gmail.com>
2022-09-02 12:36:00 -0300

    Busqueda por columnas

--- resources/views/parametro/index.blade.php
+++ resources/views/parametro/index.blade.php
@@ -6,7 +6,15 @@
     <!-- Footer theme -->
 @stop
 
-@section('css') 
+@section('css')
+    <style>
+        tfoot input {
+            width: 100%;
+            padding: 3px;
+            box-sizing: border-box;
+            border: none;
+        }
+    </style>
 @stop
 
 @section('js')   
@@ -16,7 +24,24 @@
         })
 
         $(document).ready(function(){
-            $('#parametros').DataTable({
+            $('#parametros tfoot th.isSearchable').each(function () {
+                var title = $(this).text();
+                $(this).html('<input type="text" placeholder="Buscar ' + title + '..." />');
+            });
+
+            var table = $('#parametros').DataTable({
+                initComplete: function () {
+                this.api()
+                    .columns()
+                    .every(function () {
+                        var that = this;
+                        $('input', this.footer()).on('keyup change clear', function () {                            
+                            if (that.search() !== this.value) {
+                                that.search(this.value).draw();
+                            }
+                        });
+                    });
+                },
                 dom: 'Bfrtip',
                 responisve: true,
                 autoWidth: false,                
@@ -88,7 +113,7 @@
     </nav>
 @stop
 
-@section('content')
+@section('content')    
     <div class="card">
         <div class="card-header">            
             <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="right" title="Crear Parametro"><i class="fas fa-plus"></i></button>
@@ -101,9 +126,10 @@
                         <th>Variable</th>
                         <th>Descripción</th>
                         <th>Valor</th>
-                        <th>Auxiliar</th>                        
+                        <th>Auxiliar</th>
                         <th>Estado</th>
                         <th>Status</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -127,12 +153,40 @@
                                 @if($parametro->deleted_at=='')
                                     <span class="text-success" data-toggle="tooltip" data-placement="left" title="OK"><i class="fas fa-check"></i></span>
                                 @else
-                                    <span class="text-danger" data-toggle="tooltip" data-placement="left" title="Borrada"><i class="fas fa-ban"></i></span>
+                                    <span class="text-danger" data-toggle="tooltip" data-placement="left" title="Borrada"><i class="fas fa-trash-alt"></i></span>
+                                @endif
+                            </td>
+
+                            <td class="text-center">                                
+                                <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="left" title="Editar"><i class="fas fa-edit"></i></button>                                
+
+                                @if($parametro->activa==1)
+                                    <button type="button" class="btn btn-warning text-white" data-toggle="tooltip" data-placement="left" title="Inactivar"><i class="fas fa-ban"></i></button>
+                                @elseif($parametro->activa==0)
+                                    <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="left" title="Activar"><i class="fas fa-undo"></i></button>
+                                @endif
+
+                                @if($parametro->deleted_at=='')
+                                    <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="left" title="Borrar"><i class="fas fa-trash-alt"></i></button>
+                                @elseif($parametro->deleted_at!='')
+                                    <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="left" title="Restaurar"><i class="fas fa-trash-restore-alt"></i></button>
                                 @endif
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <th class="isSearchable">nro</th>
+                        <th class="isSearchable">variable</th>
+                        <th class="isSearchable">descripción</th>
+                        <th class="isSearchable">valor</th>
+                        <th class="isSearchable">auxiliar</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
