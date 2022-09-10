028142b401d5b99d65de5ecb388293ff66c47e2e
Sergio Cova <covascode@gmail.com>
2022-09-09 21:16:26 -0300

    Reorder Directories

--- app/Http/Controllers/Core/ParametroController.php
+++ app/Http/Controllers/Core/ParametroController.php
@@ -10,7 +10,7 @@ class ParametroController extends Controller
 {
     public function __invoke(){        
         $parametros = Parametro::all();
-        return view('parametro.index', [
+        return view('core.parametro.index', [
             'parametros' => $parametros
         ]);
     }   
