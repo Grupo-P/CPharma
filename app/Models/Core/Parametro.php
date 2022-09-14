7c317a692ffd0e898f5826675f28c154e2416e6b
Sergio Cova <covascode@gmail.com>
2022-09-14 19:35:58 -0300

    Activity Log

--- app/Models/Core/Parametro.php
+++ app/Models/Core/Parametro.php
@@ -5,10 +5,13 @@ namespace App\Models\Core;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Parametro extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $table = "core_parametros";
 
@@ -20,5 +23,10 @@ class Parametro extends Model
     public function users()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll();
     }
 }
