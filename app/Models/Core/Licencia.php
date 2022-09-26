b98f85c2de57b3f390149c4377aac2c389ddbbf6
Sergio Cova <covascode@gmail.com>
2022-09-26 20:58:51 -0300

    Licencia Update

--- app/Models/Core/Licencia.php
+++ app/Models/Core/Licencia.php
@@ -5,6 +5,7 @@ namespace App\Models\Core;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
 
@@ -28,5 +29,49 @@ class Licencia extends Model
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()->useLogName(Licencia::class)->logAll();
+    }
+
+    public static function validate_licence(){
+        $validate_licence = true;
+        $mensaje = 'Licencia valida';
+
+        $licencia = Licencia::find(1);
+        $razon_social = Parametro::find(1);
+        $rif = Parametro::find(2);
+        $fecha_limite = Parametro::find(3);
+        $tipo_licencia = Parametro::find(4);
+
+        if(! Hash::check($razon_social->valor,$licencia->hash1) ){
+            $validate_licence = false;
+            $mensaje = 'La información de la razon social es incorrecta';
+        }
+
+        if(! Hash::check($rif->valor,$licencia->hash2) ){
+            $validate_licence = false;
+            $mensaje = 'La información del RIF es incorrecta';
+        }
+
+        if(! Hash::check($fecha_limite->valor,$licencia->hash3) ){
+            $validate_licence = false;
+            $mensaje = 'La información de la fecha limite es incorrecta';
+        }
+
+        if( date('Y-m-d') > $fecha_limite->valor){
+            $validate_licence = false;
+            $mensaje = 'La fecha limite expiro';
+        }
+
+        if(! Hash::check($tipo_licencia->valor,$licencia->hash4) ){
+            $validate_licence = false;
+            $mensaje = 'La información del tipo de licencia es incorrecta';
+        }
+
+        $resultado = array(
+            'validate_licence' => $validate_licence,
+            'mensaje' => $mensaje,
+            'fecha' => $fecha_limite->valor,
+        );
+
+        return $resultado;
     }
 }
