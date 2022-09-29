4ac994a9e4899a3b404bee4ff15d68226d26d9ec
Sergio Cova <covascode@gmail.com>
2022-09-29 08:52:53 -0300

    Updates Base

--- database/seeders/Core/LiceciaSeeder.php
+++ database/seeders/Core/LiceciaSeeder.php
@@ -15,6 +15,16 @@ class LiceciaSeeder extends Seeder
      */
     public function run()
     {
-        Licencia::factory(1)->create();
+        Licencia::create([
+            'hash1' => '$2y$10$Rdbbvw7igV1yIIu63FzIdu5mHwwzFhotLC2pFmeQ4zYGJhyboAq2m',
+            'hash2' => '$2y$10$/Ll4SNEC2uat7eqPzBiPgeCrSJHvp2zdABnTCIZCIxAt1ScnqMbCq',
+            'hash3' => '$2y$10$T31PXlS7OlsZBJCaBFFV3OSZgFODk9R.gHcv.q7QOwGwSv3jSN9q6',
+            'hash4' => '$2y$10$nIhVZeNz51N7JsflbHRt.uDL8USlhgS.LVR/IDWNHOt6SO1hwC3Dq',
+            'activo' => 1,
+            'borrado' => 0,
+            'user_created_at' => 1,
+        ]);
+        
+        //Licencia::factory(1)->create();
     }
 }
--- database/seeders/Core/ParametroSeeder.php
+++ database/seeders/Core/ParametroSeeder.php
@@ -15,6 +15,42 @@ class ParametroSeeder extends Seeder
      */
     public function run()
     {
-        Parametro::factory(250)->create();
+        Parametro::create([
+            'variable' => 'razon_social',
+            'valor' => 'Razón Social C.A',
+            'descripcion' => 'Razón social',
+            'activo' => 1,
+            'borrado' => 0,
+            'user_created_at' => 1,
+        ]);
+
+        Parametro::create([
+            'variable' => 'rif',
+            'valor' => 'J-19968352-0',
+            'descripcion' => 'RIF del comercio',
+            'activo' => 1,
+            'borrado' => 0,
+            'user_created_at' => 1,
+        ]);
+
+        Parametro::create([
+            'variable' => 'fecha_limite',
+            'valor' => '2022-11-01',
+            'descripcion' => 'Fecha limite de la licencia',
+            'activo' => 1,
+            'borrado' => 0,
+            'user_created_at' => 1,
+        ]);
+
+        Parametro::create([
+            'variable' => 'tipo_licencia',
+            'valor' => 'basica',
+            'descripcion' => 'Tipo de licencia',
+            'activo' => 1,
+            'borrado' => 0,
+            'user_created_at' => 1,
+        ]);
+        
+        Parametro::factory(51)->create();
     }
 }
