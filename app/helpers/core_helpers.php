b65d149249e5df3ad116b8d5997d21d57d23a94b
Sergio Cova <covascode@gmail.com>
2022-10-06 08:37:33 -0300

    Helpers Update

--- app/helpers/core_helpers.php
+++ app/helpers/core_helpers.php
@@ -5,4 +5,11 @@ if (! function_exists('last_version')) {
         return 'v.1.0.0.';
     }
 }
+
+if (! function_exists('release_changes')) {
+    function release_changes()
+    {
+        return 'Hola cambio';
+    }
+}
 ?>
