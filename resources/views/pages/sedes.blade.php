803a3296cfd5dc84cb2e03324fd38e2fe424c57c
Sergio Cova <covascode@gmail.com>
2023-03-16 21:07:10 -0300

    Configuracion 3

--- resources/views/pages/sedes.blade.php
+++ resources/views/pages/sedes.blade.php
@@ -749,6 +749,36 @@
     }
 ?>
 <!-- CASO KDI-->
+<!-- CASO FEC-->
+<?php
+}
+    if($SedeConnection == 'FEC'){
+?>
+    <?php
+        if(Auth::user()->sede == 'FARMACIA EL CALLEJON, C.A.' || Auth::user()->sede == 'GRUPO P, C.A'){
+    ?>
+    <div class="card-deck">
+        <div class="card border-danger mb-3" style="width: 14rem;">
+        <div class="card-body text-left bg-danger">
+            <h5 class="card-title">
+                <span class="card-text text-white">
+                    <?php echo "".SedeFEC; ?>
+                </span>
+            </h5>
+        </div>
+        <div class="card-footer bg-transparent border-danger text-right">
+                <form action="/reporte/" style="display: inline;">
+                @csrf
+                <input id="SEDE" name="SEDE" type="hidden" value="FEC">
+                <button type="submit" name="Reporte" role="button" class="btn btn-outline-danger btn-sm"></i>Ver reportes</button>
+                </form>
+        </div>
+        </div>
+    </div>
+    <?php
+    }
+?>
+<!-- CASO FEC-->
 <!-------------------------------------------------------------------------------->
 <!-------------------------------------------------------------------------------->
 <!-- CASO FSM -->
