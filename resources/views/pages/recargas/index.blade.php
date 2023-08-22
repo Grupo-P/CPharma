e922c6429a580d4be0a0286f4f1ff808e8ab2a3b
Juan Vera <jartigas1992@gmail.com>
2023-08-22 09:01:40 -0400

    fix de rutas de recargas

--- resources/views/pages/recargas/index.blade.php
+++ resources/views/pages/recargas/index.blade.php
@@ -114,7 +114,7 @@
             </div>
         @endif
                         
-        <form action="{{route("filtrofecha")}}" method="POST">
+        <form action="{{route("recargas2")}}" method="POST">
             @csrf
             <div class="row">
                 <div class="col-4">
