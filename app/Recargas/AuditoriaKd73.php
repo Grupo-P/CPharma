5e33f6bc5a38c47dc989ac5b6c3d0e9c0125efb3
Juan Vera <jartigas1992@gmail.com>
2023-08-24 10:30:33 -0400

    fix de ordenes sin transacciones tomados en cuenta en reporte

--- app/Recargas/AuditoriaKd73.php
+++ app/Recargas/AuditoriaKd73.php
@@ -38,70 +38,92 @@ class AuditoriaKd73 extends Model
     {
         return $this->hasOne(CajaKd73::class,'id','id_caja');
     }
+
     public static function totalDivisas($cajero,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_user','=',$cajero)->get();
-        if($operaciones->SUM('total_divisas')>0){
-            $resultado=$operaciones->SUM('total_divisas');
-        }
-        else{
-            $resultado=0;
-        }
+
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_divisas > 0){
+                    $resultado= $resultado + $operacion->total_divisas;
+                }
+            }
+        }        
         return $resultado;
     }
 
     public static function totalBolivares($cajero,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_user','=',$cajero)->get();
-        
-        if($operaciones->SUM('total_bolivares')>0){
-            $resultado=$operaciones->SUM('total_bolivares');
-        }
-        else{
-            $resultado=0;
+
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_bolivares > 0){
+                    $resultado= $resultado + $operacion->total_bolivares;
+                }
+            }
         }
         return  $resultado;
     }
 
     public static function totalVueltos($cajero,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_user','=',$cajero)->get();
-        
-        if($operaciones->SUM('total_vuelto')>0){
-            $resultado=$operaciones->SUM('total_vuelto');
-        }
-        else{
-            $resultado=0;
+
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_vuelto > 0){
+                    $resultado= $resultado + $operacion->total_vuelto;
+                }
+            }
         }
         return  $resultado;
     }
 
     public static function totalDivisasCaja($caja,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_caja','=',$caja)->get();
-        if($operaciones->SUM('total_divisas')>0){
-            $resultado=$operaciones->SUM('total_divisas');
-        }
-        else{
-            $resultado=0;
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_divisas > 0){
+                    $resultado= $resultado + $operacion->total_divisas;
+                }
+            }
         }
         return $resultado;
     }
 
     public static function totalBolivaresCaja($caja,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_caja','=',$caja)->get();
-        if($operaciones->SUM('total_bolivares')>0){
-            $resultado=$operaciones->SUM('total_bolivares');
-        }
-        else{
-            $resultado=0;
+
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_bolivares > 0){
+                    $resultado= $resultado + $operacion->total_bolivares;
+                }
+            }
         }
         return  $resultado;
     }
 
     public static function totalVueltosCaja($caja,$fini,$ffin){
         $operaciones = OperacionKd73::whereBetween('fecha',[ $fini,$ffin])->where('id_caja','=',$caja)->get();
-        if($operaciones->SUM('total_vuelto')>0){
-            $resultado=$operaciones->SUM('total_vuelto');
-        }
-        else{
-            $resultado=0;
+
+        $resultado = 0;
+        foreach($operaciones as $operacion){
+            $auditoria = AuditoriaKd73::where('id_operacion','=',$operacion->id)->first();
+            if($auditoria != null){
+                if($operacion->total_vuelto > 0){
+                    $resultado= $resultado + $operacion->total_vuelto;
+                }
+            }
         }
         return  $resultado;
     }
