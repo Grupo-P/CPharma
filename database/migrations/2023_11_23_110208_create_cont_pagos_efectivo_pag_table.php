3dd7ed96d8104241b5916cb256824d88b01e29ca
Jonathan Urdaneta <johnmub.dev@gmail.com>
2023-11-23 11:37:48 -0400

    Add: Agregar estructura correcta de migraciones para FLF y PAG

--- database/migrations/2023_11_23_110208_create_cont_pagos_efectivo_pag_table.php
+++ database/migrations/2023_11_23_110208_create_cont_pagos_efectivo_pag_table.php
@@ -15,7 +15,34 @@ class CreateContPagosEfectivoPagTable extends Migration
     {
         Schema::create('cont_pagos_efectivo_pag', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('id_proveedor')->nullable();
+            $table->string('id_cuenta')->nullable();
+            $table->string('ingresos')->nullable();
+            $table->string('egresos')->nullable();
+            $table->string('diferido')->nullable();
+            $table->string('saldo_anterior');
+            $table->string('saldo_actual');
+            $table->string('diferido_anterior')->nullable();
+            $table->string('diferido_actual')->nullable();
+            $table->string('concepto');
+            $table->string('user');
+            $table->string('user_up')->nullable();
+            $table->string('estatus')->nullable();
+            $table->string('sede');
+            $table->softDeletes();
             $table->timestamps();
+            $table->string('fecha_conciliado')->nullable();
+            $table->string('usuario_conciliado')->nullable();
+            $table->string('tasa')->nullable();
+            $table->string('autorizado_por')->nullable();
+            $table->string('estatus_conciliaciones')->nullable();
+            $table->string('titular_pago')->nullable();
+            $table->string('monto_proveedor')->default(0);
+            $table->string('iva')->nullable();
+            $table->string('retencion_deuda_1')->nullable();
+            $table->string('retencion_deuda_2')->nullable();
+            $table->string('retencion_iva')->nullable();
+            $table->string('monto_banco')->default(0);
         });
     }
 
