06b510af0da5a4faa23a1be8cb799a66ff1a6ef0
Jonathan Urdaneta <johnmub.dev@gmail.com>
2023-12-14 14:47:36 -0400

    Change: Corregir dominio de etiquetas

--- resources/views/pages/etiqueta/ofertero/Generar_Etiqueta_Ofertero.blade.php
+++ resources/views/pages/etiqueta/ofertero/Generar_Etiqueta_Ofertero.blade.php
@@ -596,8 +596,8 @@
   </script>
 
 	<script>
-    // var dominio = dominio(SedeConnectionJs);
-    var dominio = "http://cpharmagpde.com/"
+    var dominio = dominio(SedeConnectionJs);
+    // var dominio = "http://cpharmagpde.com/"
     const URLEtiquetaUnica = ''+dominio+'assets/functions/ofertero/functionEtiquetaOfertero.php';
 
 		$('#inputCodBar').attr("placeholder", "Haga scan o escriba el codigo de barra");
