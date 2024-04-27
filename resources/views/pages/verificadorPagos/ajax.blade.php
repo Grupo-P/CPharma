eeb233dbd13ae1212b606e45b4faafa113e9126b
Jonathan Urdaneta <johnmub.dev@gmail.com>
2024-04-27 10:29:46 -0400

    cambiar bancos de los zelle

--- resources/views/pages/verificadorPagos/ajax.blade.php
+++ resources/views/pages/verificadorPagos/ajax.blade.php
@@ -544,8 +544,46 @@
                 }
 
                 // PNC
+                if ($item->from == 'PNC Alerts <pncalerts@pnc.com>' && strpos($item->subject, 'sent you'))
+                {
+                    $body = imap_fetchbody($conn, $email, 1);
 
-                if (strpos($asunto, 'sent you a Zelle® payment.') && $item->from == 'PNC Alerts <pncalerts@pnc.com>') {
+                    $array = explode(' sent you ', $item->subject);
+                    $enviadoPor = $array[0];
+                    $enviadoPor = strtoupper($array[0]);
+
+                    $monto = $array[1];
+
+                    $inicioMonto = strpos($body, 'Amount:');
+                    $finMonto = strpos($body, 'Note:');
+                    $monto = substr($body, $inicioMonto, $finMonto-$inicioMonto);
+                    $monto = strip_tags($monto);
+                    $monto = str_replace('Amount:', '', $monto);
+
+                    $inicioComentario = strpos($body, 'Note:');
+                    $finComentario = strpos($body, 'Date:');
+                    $comentario = substr($body, $inicioComentario, $finComentario-$inicioComentario);
+                    $comentario = strip_tags($comentario);
+                    $comentario = str_replace('Note:', '', $comentario);
+
+                    $inicioReferencia = strpos($body, 'Transaction ID:');
+                    $finReferencia = strpos($body, 'The money will ');
+                    $referencia = substr($body, $inicioReferencia, $finReferencia-$inicioReferencia);
+                    $referencia = strip_tags($referencia);
+                    $referencia = str_replace('Transaction ID:', '', $referencia);
+                    $comentario = $comentario . ' Referencia: ' . $referencia;
+
+                    $pagos[$i]['tipo'] = 'Zelle PNC';
+                    $pagos[$i]['enviado_por'] = $enviadoPor;
+                    $pagos[$i]['monto'] = $monto;
+                    $pagos[$i]['fecha'] = $fecha;
+                    $pagos[$i]['fechaSinFormato'] = $fechaSinFormato;
+                    $pagos[$i]['comentario'] = $comentario;
+                    $pagos[$i]['hash'] = rand(100, 999) . $fechaSinFormato . rand(100, 999) ;
+                    $pagos[$i]['referencia'] = $i;
+
+                        $i++;
+                } else if (strpos($asunto, 'sent you a Zelle® payment.') && $item->from == 'PNC Alerts <pncalerts@pnc.com>') {
 
                     $body = @imap_qprint(@imap_body($conn, $email));
 
@@ -583,7 +621,6 @@
                 }
 
                 // Truist
-
                 if ($item->subject == 'Money was sent to you with Zelle' && $item->from == 'Truist Alerts <alertnotifications@message.truist.com>') {
 
                     $body = imap_fetchbody($conn, $email, 1);
@@ -741,7 +778,7 @@
 
             foreach ($overview as $item) {
 
-                    // PNC
+                // PNC
                 if ($item->from == 'PNC Alerts <pncalerts@pnc.com>' && strpos($item->subject, 'sent you'))
                 {
                     $body = imap_fetchbody($conn, $email, 1);
@@ -783,7 +820,7 @@
                         $i++;
                 } 
                 // BANESCO USA
-                else if ($item->from == 'Banesco USA <customerservice@banescousa.com>' && strpos($item->subject, 'deposited your payment'))
+                if ($item->from == 'Banesco USA <customerservice@banescousa.com>' && strpos($item->subject, 'deposited your payment'))
                 {
                     $body = imap_fetchbody($conn, $email, 1);
 
@@ -815,6 +852,62 @@
                     $pagos[$i]['fechaSinFormato'] = $fechaSinFormato;
                     $pagos[$i]['comentario'] = 'Referencia: '.$referencia;
                     $pagos[$i]['hash'] = rand(100, 999) . $fechaSinFormato . rand(100, 999) ;
+                    $pagos[$i]['referencia'] = $i;
+
+                    $i++;
+                } 
+                 // TRUIST
+                if ($item->subject == 'Money was sent to you with Zelle' && $item->from == 'Truist Alerts <alertnotifications@message.truist.com>') {
+                    $body = imap_fetchbody($conn, $email, 1);
+                    $body = imap_base64($body);
+
+                    $inicioEnviadoPor = strpos($body, 'Sent by:');
+                    $finEnviadoPor = strpos($body, 'Amount');
+                    $enviadoPor = substr($body, $inicioEnviadoPor, $finEnviadoPor-$inicioEnviadoPor);
+                    $enviadoPor = strip_tags($enviadoPor);
+                    $enviadoPor = str_replace(['Sent by:', '&nbsp;'], '', $enviadoPor);
+                    $enviadoPor = trim($enviadoPor);
+
+                    $inicioMonto = strpos($body, 'Amount:');
+
+                    $finMonto = strpos($body, 'Memo:') === false ? strpos($body, 'The money will') : strpos($body, 'Memo:');
+
+                    if(!$finMonto) {
+                        $finMonto = strpos($body, 'Memo:') === false ? strpos($body, 'This was de') : strpos($body, 'Memo:');
+                    }
+
+                    $monto = substr($body, $inicioMonto, $finMonto-$inicioMonto);
+                    $monto = strip_tags($monto);
+                    $monto = str_replace(['Amount:', '&nbsp;'], '', $monto);
+                    $monto = trim($monto);
+
+                    if (strpos($body, 'Memo:') === false) {
+                        $comentario = '';
+                    } else {
+                        $inicioComentario = strpos($body, 'Memo:');
+                        $finComentario = strpos($body, 'The money will');
+                        
+                        if(!$finComentario) {
+                            $finComentario = strpos($body, 'This was de');
+                        }
+
+                        $comentario = substr($body, $inicioComentario, $finComentario-$inicioComentario);
+                        $comentario = strip_tags($comentario);
+                        $comentario = str_replace(['Memo:', '&nbsp;'], '', $comentario);
+                        $comentario = trim($comentario);
+                    }
+
+
+                    $decimales = explode('.', (string) $monto);
+                    $decimales = $decimales[1] ?? $monto;
+
+                    $pagos[$i]['tipo'] = 'Zelle Truist';
+                    $pagos[$i]['enviado_por'] = $enviadoPor;
+                    $pagos[$i]['monto'] = $monto;
+                    $pagos[$i]['fecha'] = $fecha;
+                    $pagos[$i]['fechaSinFormato'] = $fechaSinFormato;
+                    $pagos[$i]['comentario'] = $comentario;
+                    $pagos[$i]['hash'] = rand(100, 999) . substr($enviadoPor[0], 0, 1) . rand(100, 999) . $decimales;
                     $pagos[$i]['referencia'] = $i;
 
                     $i++;
